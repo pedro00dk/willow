f2c6571e105e97a9ad6eaa63ed12b2e8ca70ce8b
Pedro Henrique <pedro00dk@gmail.com>
2018-12-20 11:14:35 -0300

    Add comments and types to trace.py

--- tracerPython/src/tracer.py
+++ tracerPython/src/tracer.py
@@ -1,24 +1,27 @@
 import sys
+import types
 
 
 class Tracer:
     """
-    Trace python code and analyses its state after every instruction
+    Trace python code and analyses its state after every instruction.
     """
 
-    def __init__(self, script):
+    def __init__(self, script: str):
         """
-        Initialize the tracer with the python script
+        Initialize the tracer with the python script.
+
+            :param script: python script
         """
         self.script = script
         self.lines = script.splitlines() if len(script) > 0 else ['']
 
     def start(self):
         """
-        Start to trace the script
+        Start to trace the script.
         """
         try:
-            sys.settrace(self._dispatch)
+            sys.settrace(self._trace)
             exec(compile(self.script, '<script>', 'exec'), {}, {})
             print('done')
         except Exception as e:
@@ -26,6 +29,13 @@ class Tracer:
         finally:
             sys.settrace(None)
 
-    def _dispatch(self, frame, event, args):
+    def _trace(self, frame: types.FrameType, event: str, args):
+        """
+        The script trace function.
+
+            :param frame: current frame stack
+            :param event: current code event, one of: 'call', 'line', 'return', 'exception' or 'opcode'
+            :param args: context arguments in some code states
+        """
         print(self.lines[frame.f_lineno - 1])
-        return self._dispatch
+        return self._trace
