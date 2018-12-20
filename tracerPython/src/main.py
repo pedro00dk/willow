a8af86c4d1e60d146a29cfc215f4bc0fcc019251
Pedro Henrique <pedro00dk@gmail.com>
2018-12-20 15:26:00 -0300

    Expose name and sandbox in the CLI

--- tracerPython/src/main.py
+++ tracerPython/src/main.py
@@ -5,11 +5,12 @@ import tracer
 
 def main():
     parser = argparse.ArgumentParser(description='Tracer CLI parser')
+    parser.add_argument('--name', default='<script>', help='The script name')
+    parser.add_argument('--sandbox', default=False, action='store_true', help='Run in a restricted scope')
     parser.add_argument('script', help='The python script to parse')
     arguments = parser.parse_args()
-    
-    tracer.Tracer('<script>', arguments.script, True).start()
 
+    tracer.Tracer(arguments.name, arguments.script, arguments.sandbox).start()
 
 
 if __name__ == '__main__':
