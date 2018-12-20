653800f0e929285bb4f553d6cbbb7dad2788508d
Pedro Henrique <pedro00dk@gmail.com>
2018-12-20 16:22:23 -0300

    Create inspector script and move/rename TracerControler to it

---
+++ tracerPython/src/inspector.py
@@ -0,0 +1,61 @@
+import sys
+import types
+
+
+class Inspector:
+    """
+    Control the script tracing progression by receiving commands.
+    """
+
+    TRACEABLE_EVENTS = {'call', 'line', 'exception', 'return'}
+
+    def __init__(self, name: str, lines: list):
+        """
+        Initialize the tracer controller with the script name and lines.
+
+            :param name: script name
+            :param line: script lines
+        """
+        self._name = name
+        self._lines = lines
+
+    def code_line(self, frame: types.FrameType):
+        return self._lines[frame.f_lineno - 1] if self.is_base_file(frame) else None
+
+    def is_base_file(self, frame: types.FrameType):
+        return frame.f_code.co_filename == self._name
+
+    def is_traceable_event(self, event: str):
+        return event in Inspector.TRACEABLE_EVENTS
+
+    def trace(self, frame: types.FrameType, event: str, args):
+        """
+        The script trace function.
+
+            :param frame: current frame stack
+            :param event: current code event, one of: 'call', 'line', 'return', 'exception' or 'opcode'
+            :param args: context arguments in some code states
+        """
+        if not self.is_base_file(frame) or not self.is_traceable_event(event):
+            return self.trace
+
+        print(self.inspect_state(frame, event, args))
+
+        return self.trace
+
+    # inspection methods
+
+    def inspect_state(self, frame: types.FrameType, event: str, args):
+        line = frame.f_lineno
+        text = self.code_line(frame)
+        return {
+            'event': event,
+            'line': line,
+            'text': text,
+        }
+
+    def inspect_stack(self, frame: types.FrameType):
+        pass
+
+    def inspect_heap(self, frame: types.FrameType):
+        pass
--- tracerPython/src/tracer.py
+++ tracerPython/src/tracer.py
@@ -2,6 +2,7 @@ import sys
 import types
 
 import scope
+import inspector
 
 
 class Tracer:
@@ -21,7 +22,7 @@ class Tracer:
         self._script = script
         self._sandbox = sandbox
         self._lines = self._script.splitlines() if len(script) > 0 else ['']
-        self._controller = TraceController(self._name, self._lines)
+        self._controller = inspector.Inspector(self._name, self._lines)
 
     def start(self):
         """
@@ -37,62 +38,3 @@ class Tracer:
             print(str(e))
         finally:
             sys.settrace(None)
-
-
-class TraceController:
-    """
-    Control the script tracing progression by receiving commands.
-    """
-
-    TRACEABLE_EVENTS = {'call', 'line', 'exception', 'return'}
-
-    def __init__(self, name: str, lines: list):
-        """
-        Initialize the tracer controller with the script name and lines.
-
-            :param name: script name
-            :param line: script lines
-        """
-        self._name = name
-        self._lines = lines
-
-    def code_line(self, frame: types.FrameType):
-        return self._lines[frame.f_lineno - 1] if self.is_base_file(frame) else None
-
-    def is_base_file(self, frame: types.FrameType):
-        return frame.f_code.co_filename == self._name
-
-    def is_traceable_event(self, event: str):
-        return event in TraceController.TRACEABLE_EVENTS
-
-    def trace(self, frame: types.FrameType, event: str, args):
-        """
-        The script trace function.
-
-            :param frame: current frame stack
-            :param event: current code event, one of: 'call', 'line', 'return', 'exception' or 'opcode'
-            :param args: context arguments in some code states
-        """
-        if not self.is_base_file(frame) or not self.is_traceable_event(event):
-            return self.trace
-
-        print(self.inspect_state(frame, event, args))
-
-        return self.trace
-
-    # inspection methods
-
-    def inspect_state(self, frame: types.FrameType, event: str, args):
-        line = frame.f_lineno
-        text = self.code_line(frame)
-        return {
-            'event': event,
-            'line': line,
-            'text': text,
-        }
-
-    def inspect_stack(self, frame: types.FrameType):
-        pass
-
-    def inspect_heap(self, frame: types.FrameType):
-        pass
