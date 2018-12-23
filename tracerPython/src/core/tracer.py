6529b9cc9668931d4f013f1b22cadd569c2deb66
Pedro Henrique <pedro00dk@gmail.com>
2018-12-23 14:12:06 -0300

    Move frame processing functionality from Inspector to FrameProcessor

--- tracerPython/src/core/tracer.py
+++ tracerPython/src/core/tracer.py
@@ -32,18 +32,107 @@ class Tracer:
 
     def run(self):
         """
-        Configures the scope and runs the tracer, giving the tracing control to the inspector.
+        Configures the scope and runs the tracer, giving the tracing control to the frame processor.
         """
         script_scope = scope.default_scope(self._name) if not self._sandbox else scope.sandbox_scope(self._name)
-        script_inspector = inspector.Inspector(self._name, self._lines, self._action_queue, self._result_queue)
+        frame_processor = FrameProcessor(self._name, self._lines, self._action_queue, self._result_queue)
 
         try:
             action = self._action_queue.get()
             compiled = compile(self._script, script_scope[scope.Globals.FILE], 'exec')
             self._result_queue.put(events.Event(events.Results.STARTED))
-            sys.settrace(script_inspector.trace)
+            sys.settrace(frame_processor.trace)
             exec(compiled, script_scope)
         except Exception as e:
             self._result_queue.put(events.Event(events.Results.ERROR, str(e)))
         finally:
             sys.settrace(None)
+
+
+class FrameProcessor:
+    """
+    Read action queue waiting for actions, process the frames and write the results in the queue. 
+    """
+
+    def __init__(self, name: str, lines: list, action_queue: mp.Queue, result_queue: mp.Queue):
+        """
+        Initializes the frame processor with the script name, lines and queues.
+        """
+        self._name = name
+        self._lines = lines
+        self._action_queue = action_queue
+        self._result_queue = result_queue
+
+        # frame common info
+        self.exec_call_frame = None
+        self.inspected_frame_count = 0
+    
+    # list of traceable events
+    TRACEABLE_EVENTS = {'call', 'line', 'exception', 'return'}
+
+    @staticmethod
+    def is_file(frame: types.FrameType, name: str):
+        """
+        Returns true if the frame is from the received file name.
+        """
+        return frame.f_code.co_filename == name
+
+    @staticmethod
+    def is_traceable(event: str):
+        """
+        Returns true if the frame event is traceable.
+        """
+        return event in FrameProcessor.TRACEABLE_EVENTS
+
+    def trace(self, frame: types.FrameType, event: str, args):
+        """
+        The script trace function.
+        """
+        if not FrameProcessor.is_file(frame, self._name) or not FrameProcessor.is_traceable(event):
+            return self.trace
+
+        self.exec_call_frame = frame.f_back if self.inspected_frame_count == 0 else self.exec_call_frame
+        self.inspected_frame_count += 1
+
+        while True:
+            action = self._action_queue.get()
+
+            # hold actions
+            if action.name == events.Actions.EVAL:
+                expression = action.value['expression']
+                inspect = action.value['inspect']
+
+                # evaluate first and then inspect state
+                product = self.evaluate_expression(frame, expression)
+                inspection = {}  # self.inspect_state(frame, event, args) if inspect else {}
+
+                self._result_queue.put(events.Event(events.Results.PRODUCT, {**inspection, **product}))
+                continue
+
+            # progressive actions
+            if action.name == events.Actions.STEP:
+                self._result_queue.put(events.Event(events.Results.DATA, {'finish': True}))
+            elif action.name == events.Actions.QUIT:
+                self._result_queue.put(events.Event(events.Results.DATA, {}))
+            break
+
+        return self.trace
+
+    # evaluation methods
+
+    def evaluate_expression(self, frame: types.FrameType, expression: str):
+        """
+        Evaluates expressions against the frame scope, process possible exceptions if any is thrown.
+        The expression is able to mutate the script state.
+        """
+        try:
+            product = eval(expression, frame.f_globals, frame.f_locals)
+        except Exception as e:
+            product = {
+                'type': type(e).__name__,
+                'value': e.args,
+                'traceback': traceback.format_exception(type(e), e, e.__traceback__)
+            }
+            pass
+        finally:
+            return {'product': product}
