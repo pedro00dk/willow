f53227352289e00473bebb7439da7afec1e40e3e
Pedro Henrique <pedro00dk@gmail.com>
2018-12-24 14:56:19 -0300

    Fix format flag in main

--- tracerPython/src/main.py
+++ tracerPython/src/main.py
@@ -13,31 +13,32 @@ def main():
                         help='Run without stopping and print all results')
     parser.add_argument('--omit-help', default=False, action='store_true', help='Omit help messages')
     parser.add_argument('script', help='The python script to parse')
+
     arguments = parser.parse_args()
 
     trace_broker = broker.TracerBroker(arguments.name, arguments.script, arguments.sandbox)
+
     if arguments.uncontrolled:
-        run_uncontrolled(trace_broker, arguments.omit_help)
+        run_uncontrolled(trace_broker, arguments.formated, arguments.omit_help)
     else:
-        run_controlled(trace_broker, arguments.omit_help)
-        pass
+        run_controlled(trace_broker, arguments.formated, arguments.omit_help)
 
 
-def run_uncontrolled(trace_broker: broker.TracerBroker, omit_help: bool):
+def run_uncontrolled(trace_broker: broker.TracerBroker, formated: bool, omit_help: bool):
     if not omit_help:
         print('## Running in uncontrolled mode')
         print('## Output format: <event result>\\n<event value>')
+        print()
 
-    print_results(trace_broker.start())
-    try:
-        while True:
-            print_results(trace_broker.step())
+    print_results(trace_broker.start(), formated)
+    while True:
+        try:
+            print_results(trace_broker.step(), formated)
+        except:
+            break
 
-    except:
-        pass
 
-
-def run_controlled(trace_broker: broker.TracerBroker, omit_help: bool):
+def run_controlled(trace_broker: broker.TracerBroker, formated: bool, omit_help: bool):
     if not omit_help:
         print('## Running in controlled mode')
         print('## Output format: <event result>\\n<event value>')
@@ -50,31 +51,30 @@ def run_controlled(trace_broker: broker.TracerBroker, omit_help: bool):
         print()
 
     while True:
-        action_data = input('>').split()
+        action_data = input('>>> ').split()
         try:
             if action_data[0] == 'start':
-                print_results(trace_broker.start())
+                print_results(trace_broker.start(), formated)
             elif action_data[0] == 'step':
-                print_results(trace_broker.step(1))
+                print_results(trace_broker.step(1), formated)
             elif action_data[0] == 'eval':
-                print_results(trace_broker.eval(action_data[1]))
+                print_results(trace_broker.eval(action_data[1]), formated)
             elif action_data[0] == 'input':
-                trace_broker.input(action_data[1])
+                trace_broker.input(action_data[1], formated)
             elif action_data[0] == 'stop':
                 trace_broker.stop()
                 break
             else:
                 print('action not found, try again!')
         except Exception as e:
-            # raise e
             print(e)
-            print('continuing')
 
 
-def print_results(results: list):
+def print_results(results: list, formated: bool):
+    print_function = print if not formated else pprint.pprint
     for result in results:
         print(result.name)
-        pprint.pprint(result.value)
+        print_function(result.value)
         print()
 
 
