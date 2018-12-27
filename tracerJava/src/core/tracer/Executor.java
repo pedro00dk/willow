583785a71dc7869f6bea6aae4ff52b41f69ca760
Pedro Henrique <pedro00dk@gmail.com>
2018-12-27 18:09:59 -0300

    Implement method to stop execution on main

--- tracerJava/src/core/tracer/Executor.java
+++ tracerJava/src/core/tracer/Executor.java
@@ -3,6 +3,7 @@ package core.tracer;
 import com.sun.jdi.VirtualMachine;
 import com.sun.jdi.connect.IllegalConnectorArgumentsException;
 import com.sun.jdi.connect.VMStartException;
+import com.sun.jdi.event.MethodEntryEvent;
 import com.sun.jdi.request.EventRequest;
 import com.sun.tools.jdi.VirtualMachineManagerImpl;
 
@@ -52,19 +53,45 @@ public class Executor {
         vm = connector.launch(connectorArguments);
     }
 
-    public void execute() throws VMStartException, IllegalConnectorArgumentsException, IOException, InterruptedException {
-        startVirtualMachine();
-
-        var methodEntryRequest =  vm.eventRequestManager().createMethodEntryRequest();
-        methodEntryRequest.setSuspendPolicy(EventRequest.SUSPEND_NONE);
-        methodEntryRequest.enable();
-
-        while(true) {
-            var eventSet = vm.eventQueue().remove();
-            eventSet.forEach(e -> System.out.println(e.getClass().getName()));
-            eventSet.resume();
+    /**
+     * Stops the execution when the program reaches the main method.
+     */
+    private MethodEntryEvent stopOnMain() throws InterruptedException {
+        var manager = vm.eventRequestManager();
+        var request = manager.createMethodEntryRequest();
+        request.addClassFilter(project.getMainClass());
+        request.setSuspendPolicy(EventRequest.SUSPEND_EVENT_THREAD);
+        request.enable();
+        vm.resume();
+        while (true) for (var event : vm.eventQueue().remove()) {
+            if (event instanceof MethodEntryEvent) {
+                var methodEvent = (MethodEntryEvent) event;
+                var method = methodEvent.method();
+                if (method.isPublic() && method.isStatic() && method.name().equals("main") &&
+                        method.signature().equals("([Ljava/lang/String;)V")) {
+                    request.disable();
+                    return methodEvent;
+                }
+            }
         }
     }
+
+    public void execute() throws VMStartException, IllegalConnectorArgumentsException, IOException, InterruptedException {
+        startVirtualMachine();
+        var startEvent = stopOnMain();
+        var mainThread = startEvent.thread();
+
+        vm.resume();
+
+        while (true) {
+            var eventSet = vm.eventQueue().remove();
+            for (var event : eventSet) {
+                System.out.println(event.getClass().getName());
+            }
+        }
+    }
+
+
 }
 
 /**
