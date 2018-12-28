db3d99508ecdc9ae4f2bee03a1415a56d425b064
Pedro Henrique <pedro00dk@gmail.com>
2018-12-28 18:08:42 -0300

    Add EventProcessor class

---
+++ tracerJava/src/core/EventProcessor.java
@@ -0,0 +1,14 @@
+package core;
+
+import com.sun.jdi.event.Event;
+import com.sun.jdi.event.EventSet;
+
+/**
+ * Read events queue waiting for actions, process the frames and write the results in the queue.
+ */
+public class EventProcessor {
+
+    public void trace(Event event, EventSet eventSet) {
+
+    }
+}
