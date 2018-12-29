10439db7c5b2a93994a94b5fb334fab8e1c5afb7
Pedro Henrique <pedro00dk@gmail.com>
2018-12-29 16:08:34 -0300

    Test frames and objects inspection

--- tracerJava/src/app/App.java
+++ tracerJava/src/app/App.java
@@ -7,7 +7,22 @@ public class App {
     public static void main(String[] args) throws Exception {
         new Tracer(
                 "Hello.java",
-                "public class Hello{public static void main(String[] args){System.out.println(\"from hello\");}}"
+                "import java.util.*;\n" +
+                        "" +
+                        "public class Hello {\n" +
+                        "    public static void main(String[] args) {\n" +
+                        "        System.out.println(\"from hello\");\n" +
+                        "        var x = 0;\n" +
+                        "        var y = -123l;\n" +
+                        "        var a = new int[]{1,2,3,4};\n" +
+                        "        var b = new String[]{\"a\",\"b\", \"c\", \"d\"};\n" +
+                        "        var c = new HashSet<String>();\n" +
+                        "        var d = new HashMap<Integer, String>();\n" +
+                        "        //var scan = new Scanner(System.in);\n" +
+                        "        //var input = scan.nextLine();\n" +
+                        "        //System.out.println(input);\n" +
+                        "    }\n" +
+                        "}\n"
         ).run();
     }
 }
