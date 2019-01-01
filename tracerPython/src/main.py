499322f4494119f215b189b1f71bd2268f8669ed
Pedro Henrique <pedro00dk@gmail.com>
2019-01-01 18:21:02 -0300

    Print output as json and remove formatted flag

--- tracerPython/src/main.py
+++ tracerPython/src/main.py
@@ -1,5 +1,5 @@
 import argparse
-import pprint
+import json
 
 import broker
 import message
@@ -9,7 +9,6 @@ def main():
     parser = argparse.ArgumentParser(description='Tracer CLI parser')
     parser.add_argument('--name', default='<script>', help='The script name')
     parser.add_argument('--sandbox', default=False, action='store_true', help='Run in a restricted scope')
-    parser.add_argument('--formatted', default=False, action='store_true', help='Print formatted results')
     parser.add_argument('--uncontrolled', default=False, action='store_true',
                         help='Run without stopping and print all results')
     parser.add_argument('--omit-help', default=False, action='store_true', help='Omit help messages')
@@ -19,29 +18,29 @@ def main():
     tracer_broker = broker.TracerBroker(arguments.name, arguments.script, arguments.sandbox)
 
     if arguments.uncontrolled:
-        run_uncontrolled(tracer_broker, arguments.formatted, arguments.omit_help)
+        run_uncontrolled(tracer_broker, arguments.omit_help)
     else:
-        run_controlled(tracer_broker, arguments.formatted, arguments.omit_help)
+        run_controlled(tracer_broker, arguments.omit_help)
 
 
-def run_uncontrolled(tracer_broker: broker.TracerBroker, formatted: bool, omit_help: bool):
+def run_uncontrolled(tracer_broker: broker.TracerBroker, omit_help: bool):
     if not omit_help:
         print('## Running in uncontrolled mode')
         print('## Output format: <event result>\\n<event value>')
         print()
 
-    print_results(tracer_broker.start(), formatted)
+    print_results(tracer_broker.start())
     while True:
         try:
             results = tracer_broker.step()
-            print_results(results, formatted)
+            print_results(results)
             if results[-1].name == message.Result.LOCKED:
                 tracer_broker.input('')
         except:
             break
 
 
-def run_controlled(tracer_broker: broker.TracerBroker, formatted: bool, omit_help: bool):
+def run_controlled(tracer_broker: broker.TracerBroker, omit_help: bool):
     if not omit_help:
         print('## Running in controlled mode')
         print('## Output format: <event result>\\n<event value>')
@@ -54,16 +53,17 @@ def run_controlled(tracer_broker: broker.TracerBroker, formatted: bool, omit_hel
         print()
 
     while True:
-        action_data = input('>>> ').split()
-        action = action_data[0]
-        value = ' '.join(action_data[1:])
+        action_data = input('>>> ')
+        split_index = action_data.find(' ')
+        action = action_data[:split_index if split_index != -1 else None]
+        value = action_data[split_index + 1: 0 if split_index == -1 else None]
         try:
             if action == 'start':
-                print_results(tracer_broker.start(), formatted)
+                print_results(tracer_broker.start())
             elif action == 'step':
-                print_results(tracer_broker.step(1), formatted)
+                print_results(tracer_broker.step(1))
             elif action == 'eval':
-                print_results(tracer_broker.eval(value), formatted)
+                print_results(tracer_broker.eval(value))
             elif action == 'input':
                 tracer_broker.input(value)
             elif action == 'stop':
@@ -76,14 +76,10 @@ def run_controlled(tracer_broker: broker.TracerBroker, formatted: bool, omit_hel
             print(e)
 
 
-def print_results(results: list, formatted: bool):
-    print_function = print if not formatted else pprint.pprint
+def print_results(results: list):
     for result in results:
         print(result.name)
-        if formatted:
-            pprint.pprint(result.value)
-        else:
-            print(result.value)
+        print(json.dumps(result.value))
         print()
 
 
