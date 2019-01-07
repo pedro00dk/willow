0c48c4c7c448401de783e3b387d8147ca49d26be
Pedro Henrique <pedro00dk@gmail.com>
2019-01-07 18:59:29 -0300

    Create method for transforming buffer observables into lines

--- tracers/src/tracer.ts
+++ tracers/src/tracer.ts
@@ -9,6 +9,8 @@ import { Writable } from 'stream'
  */
 export class TracerClient {
     private command: string
+    private stdout: rx.Observable<string> | undefined
+    private stderr: rx.Observable<string> | undefined
 
     /**
      * Initializes the client with the command to spawn the process.
@@ -23,21 +25,30 @@ export class TracerClient {
     spawn() {
         let instance = cp.spawn(this.command, { shell: true })
 
-        rx.fromEvent(instance.stdout, 'data')
-            .pipe(
-                rxOps.map(obj => obj as Buffer),
-                rxOps.map(buf => buf.toString('utf8'))
-            )
-            .subscribe(str => console.log(str))
+        this.stdout = observableAnyToLines(rx.fromEvent(instance.stdout, 'data'))
+        this.stdout.subscribe(str => console.log(str))
 
-        rx.fromEvent(instance.stderr, 'data')
-            .pipe(
-                rxOps.map(obj => obj as Buffer),
-                rxOps.map(buf => buf.toString('utf8'))
-            )
-            .subscribe(str => console.error(str))
+        this.stderr = observableAnyToLines(rx.fromEvent(instance.stderr, 'data'))
+        this.stderr.subscribe(str => console.error(str))
 
         instance.stdin.write('start\n')
         setInterval(() => instance.stdin.write('step\n'), 1000)
     }
+}
+
+/**
+ * Pipes an rx.Observable<any> containing text split in any form to string lines obtained from the buffers. 
+ */
+function observableAnyToLines(observable: rx.Observable<Buffer>): rx.Observable<string> {
+    return observable
+        .pipe(
+            rxOps.map(obj => obj as Buffer),
+            rxOps.map(buf => buf.toString('utf8')),
+            rxOps.flatMap(str => rx.from(str.split(/(\n)/))),
+            rxOps.filter(str => str.length !== 0),
+            rxOps.map(str => [str]),
+            rxOps.scan((acc, val) => acc[acc.length - 1] === '\n' ? val : [...acc, ...val], ['\n']),
+            rxOps.filter(arr => arr[arr.length - 1] === '\n'),
+            rxOps.map(arr => arr.join('')),
+        )
 }
