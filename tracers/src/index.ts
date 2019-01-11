73a15930c284011bced4aa9bc766a864ecc972e3
Pedro Henrique <pedro00dk@gmail.com>
2019-01-11 15:11:45 -0300

    Create starter CLI parser

--- tracers/src/index.ts
+++ tracers/src/index.ts
@@ -1,1 +1,22 @@
-console.log('hello world!')
+import * as yargs from 'yargs'
+
+
+let parser = yargs
+    .usage('Http API for CLI tracers') // not being used as usage, but description
+    .alias('h', 'help')
+    .hide('version')
+    .option('port', { default: 8000, description: 'Set the server port' })
+    .option(
+        'tracer',
+        {
+            // type: 'array', type: 'string'
+            array: true,
+            string: true,
+            nargs: 2,
+            demandOption: true,
+            description: 'Tracer <name> <spawn-cmd{<script>}>'
+        }
+    )
+
+let arguments_ = parser.argv
+console.log(arguments_)
