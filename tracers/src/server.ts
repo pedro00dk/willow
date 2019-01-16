be2f9fc66df28b56eeadb465187b62776101eb0f
Pedro Henrique <pedro00dk@gmail.com>
2019-01-16 14:41:46 -0300

    Create tracer sessions

--- tracers/src/server.ts
+++ tracers/src/server.ts
@@ -10,6 +10,7 @@ export class TracerServer {
     private server: express.Express
     private port: number
     private suppliers: Map<string, (code: string) => Tracer>
+    private sessions: Map<number, { supplier: string, tracer: Tracer }>
 
     /**
      * Creates the server with the port and the tracer suppliers.
@@ -22,6 +23,32 @@ export class TracerServer {
 
         this.port = port
         this.suppliers = suppliers
+        this.sessions = new Map()
+
+        this.configureRoutes()
+    }
+
+    /**
+     * Configures server routes.
+     */
+    private configureRoutes() {
+        this.server.get('/suppliers', (request, response) => response.send(this.getSuppliers()))
+        this.server.get('/sessions', (request, response) => response.send(this.getSessions()))
+    }
+
+    /**
+     * Lists the available suppliers.
+     */
+    private getSuppliers() {
+        return [...this.suppliers.keys()]
+    }
+
+    /**
+     * Returns the sessions ids and its suppliers.
+     */
+    private getSessions() {
+        return [...this.sessions.entries()]
+            .map(([session, { supplier }]) => ({ session, supplier }))
     }
 
     /**
