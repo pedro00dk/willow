e75a2dfcca3c69bf617cf883f3a3a0aee2ac4077
Pedro Henrique <pedro00dk@gmail.com>
2019-01-16 14:58:24 -0300

    Add create route to server

--- tracers/src/server.ts
+++ tracers/src/server.ts
@@ -1,6 +1,6 @@
 import * as express from 'express'
 
-import { Tracer } from './tracer/tracer'
+import { Tracer, DefaultTracer } from './tracer/tracer'
 
 
 /**
@@ -11,6 +11,7 @@ export class TracerServer {
     private port: number
     private suppliers: Map<string, (code: string) => Tracer>
     private sessions: Map<number, { supplier: string, tracer: Tracer }>
+    private sessionIdGenerator: number
 
     /**
      * Creates the server with the port and the tracer suppliers.
@@ -24,6 +25,7 @@ export class TracerServer {
         this.port = port
         this.suppliers = suppliers
         this.sessions = new Map()
+        this.sessionIdGenerator = 0
 
         this.configureRoutes()
     }
@@ -34,6 +36,15 @@ export class TracerServer {
     private configureRoutes() {
         this.server.get('/suppliers', (request, response) => response.send(this.getSuppliers()))
         this.server.get('/sessions', (request, response) => response.send(this.getSessions()))
+        this.server.post(
+            '/create',
+            (request, response) => {
+                let supplier = request.body['supplier'] as string
+                let code = request.body['code'] as string
+                try { response.send(this.createSession(supplier, code)) }
+                catch (error) { response.status(400).send(error) }
+            }
+        )
     }
 
     /**
@@ -48,7 +59,23 @@ export class TracerServer {
      */
     private getSessions() {
         return [...this.sessions.entries()]
-            .map(([session, { supplier }]) => ({ session, supplier }))
+            .map(([id, { supplier }]) => ({ id, supplier }))
+    }
+
+    /**
+     * Creates a new tracer session with the received supplier and code.
+     */
+    private createSession(supplier: string, code: string) {
+        if (supplier === null) throw 'supplier key not found or wrong type'
+        if (code === null) throw 'code key not found or wrong type'
+        if (!this.suppliers.has(supplier)) throw `supplier ${supplier} not found`
+
+        let id = this.sessionIdGenerator++
+        let tracer = this.suppliers.get(supplier)(code)
+        tracer = tracer instanceof DefaultTracer ? tracer : new DefaultTracer(tracer)
+
+        this.sessions.set(id, { supplier, tracer })
+        return { id, supplier }
     }
 
     /**
