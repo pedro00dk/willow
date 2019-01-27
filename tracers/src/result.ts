59c35b5822f82f7eda3769ae5976232f4c37069e
Pedro Henrique <pedro00dk@gmail.com>
2019-01-27 11:09:16 -0300

    Add query generators fo results events

--- tracers/src/result.ts
+++ tracers/src/result.ts
@@ -1,11 +1,11 @@
 // Result fields
 export type Result = {
-    name: ResultType
-    value: ValueType
+    name: ResultNameType
+    value: ResultValueType
 }
 
-export type ResultType = 'started' | 'error' | 'data' | 'product' | 'print' | 'prompt' | 'locked'
-export type ValueType = null | string | Event
+export type ResultNameType = 'started' | 'error' | 'data' | 'product' | 'print' | 'prompt' | 'locked'
+export type ResultValueType = null | string | Event
 
 
 // Event fields
@@ -34,13 +34,13 @@ export type StackReferencesType = ScopeReferencesType[]
 export type ScopeReferencesType = ScopeReferenceType[]
 export type ScopeReferenceType = [string, AnyType]
 
-export type HeapGraphType = Map<string, HeapObjectType>
+export type HeapGraphType = { [reference: string]: HeapObjectType }
 export type HeapObjectType = { type: string, languageType: string, members: HeapObjectMemberType[] }
 export type GenericType = 'list' | 'set' | 'map' | 'udo' // udo => user defined object
 export type HeapObjectMemberType = [AnyType, AnyType]
 
-export type AnyType = LiteralType | ReferenceType
-export type LiteralType = boolean | number | string
+export type AnyType = ValueType | ReferenceType
+export type ValueType = boolean | number | string // strings are better represented as value types
 export type ReferenceType = [number]
 
 export type UserClassesType = string[]
@@ -62,4 +62,27 @@ export function isErrorResult(result: Result) {
  */
 export function isLastResult(result: Result) {
     return result.name === 'data' && (result.value as Event).finish
+}
+
+/**
+ * Queries through a generator all value type object of the received type string.
+ */
+export function* queryValueTypes(event: Event, ...types: ('boolean' | 'number' | 'string')[]) {
+    const typeSet = new Set<string>(types)
+    for (const scopeReference of event.stackReferences) for (const [, value] of scopeReference)
+        if (typeSet.has(typeof value)) yield value
+
+    for (const { members } of Object.values(event.heapGraph)) for (const [key, value] of members) {
+        if (typeSet.has(typeof key)) yield key
+        if (typeSet.has(typeof value)) yield value
+    }
+}
+
+/**
+ * Queries through a generator all reference type object of the received type string.
+ */
+export function* queryReferenceTypes(event: Event, ...types: GenericType[]) {
+    const typeSet = new Set<string>(types)
+    for (const heapObject of Object.values(event.heapGraph))
+        if (typeSet.has(heapObject.type)) yield heapObject
 }
