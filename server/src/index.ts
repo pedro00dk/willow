0352ccbb255135d2949e94692745936ee052f245
Pedro Henrique <pedro00dk@gmail.com>
2019-01-30 18:36:10 -0300

    Create the CLI parser

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,24 @@
-console.log('hello world!')
+import * as yargs from 'yargs'
+
+
+function main() {
+    const parser = yargs
+        .usage('Willow API') // not being used as usage, but description
+        .alias('h', 'help')
+        .hide('version')
+        .option('port', { default: 8000, description: 'Set the server port' })
+        .option('tracers', { description: 'Address of the tracer server', type: 'string' })
+
+    const argumentS = parser.argv
+
+    const port = argumentS.port
+    const tracerServerAddress = argumentS.tracers
+
+    console.log(port, tracerServerAddress)
+}
+
+
+if (!module.parent) {
+    // called as entry point
+    main()
+}
