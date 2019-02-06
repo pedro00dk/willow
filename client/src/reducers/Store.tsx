67929edd690b7c7b2ae6140c00c0b06c9e9cfe7d
Pedro Henrique <pedro00dk@gmail.com>
2019-02-06 17:25:46 -0300

    Create IO reducer for code input and output interaction

--- client/src/reducers/Store.tsx
+++ client/src/reducers/Store.tsx
@@ -3,15 +3,18 @@ import { Provider } from 'react-redux'
 import * as Redux from 'redux'
 import thunk from 'redux-thunk'
 import { reducer as CodeReducer } from './code'
+import { reducer as IOReducer } from './io'
 
 // export renamed action and state types
 export { Action as CodeAction, State as CodeState } from './code'
+export { Action as IOAction, State as IOState } from './io'
 
 
 const reduxStoreEnhancer = Redux.compose(Redux.applyMiddleware(thunk))
 const reduxStoreCreator = reduxStoreEnhancer(Redux.createStore)
 const reduxStore = reduxStoreCreator(Redux.combineReducers({
-    code: CodeReducer
+    code: CodeReducer,
+    io: IOReducer
 }))
 
 export type StoreState = ReturnType<typeof reduxStore.getState>
---
+++ client/src/reducers/io.ts
@@ -0,0 +1,36 @@
+import { Reducer } from 'redux'
+
+
+export type State = {
+    readonly output: ReadonlyArray<string>
+    readonly input: ReadonlyArray<string>
+}
+export type Action =
+    { type: 'io/reset' } |
+    { type: 'io/appendOutput', payload: { output: string } } |
+    { type: 'io/commitInput' } |
+    { type: 'io/setInputLine', payload: { input: string } }
+
+
+const initialState: State = {
+    output: [''],
+    input: ['']
+}
+
+export const reducer: Reducer<State, Action> = (state = initialState, action) => {
+    if (!action) return state
+    switch (action.type) {
+        case 'io/reset': return { ...initialState }
+        case 'io/appendOutput': {
+            const lines = action.payload.output.split(/\r?\n/)
+            const output = [...state.output.slice(0, -1), state.output.slice(-1)[0] + lines[0], ...lines.slice(1)]
+            return { ...state, output }
+        } case 'io/commitInput': {
+            if (state.input.length < 2) break
+            const input = state.input.slice(1)
+            const output = [...state.output.slice(-1), state.output.slice(-1)[0] + state.input[0]]
+            return { ...state, input, output }
+        } case 'io/setInputLine': return { ...state, input: [...state.input.slice(0, -1), action.payload.input] }
+    }
+    return state
+}
