3fb0dddba3a17c64dcd5c0f64484cffc1e094596
Pedro Henrique <pedro00dk@gmail.com>
2019-02-08 11:34:53 -0300

    Add Redux Thunk type safe dispatches

--- client/src/reducers/Store.tsx
+++ client/src/reducers/Store.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
 import * as Redux from 'redux'
-import thunk from 'redux-thunk'
+import { default as thunk, ThunkDispatch as ReduxThunkThunkDispatch, ThunkMiddleware } from 'redux-thunk'
 import { reducer as CodeReducer } from './code'
 import { reducer as IOReducer } from './io'
 
-// export renamed action and state types
-export { Action as CodeAction, State as CodeState } from './code'
-export { Action as IOAction, State as IOState } from './io'
 
-
-const reduxStoreEnhancer = Redux.compose(Redux.applyMiddleware(thunk))
+const reduxStoreEnhancer = Redux.compose(Redux.applyMiddleware(thunk as ThunkMiddleware))
 const reduxStoreCreator = reduxStoreEnhancer(Redux.createStore)
 const reduxStore = reduxStoreCreator(Redux.combineReducers({
     code: CodeReducer,
@@ -18,6 +14,16 @@ const reduxStore = reduxStoreCreator(Redux.combineReducers({
 }))
 
 export type StoreState = ReturnType<typeof reduxStore.getState>
+
+// export thunk dispatch with resolved generics and its prop form
+// shall be used instead of redux Dispatch and DispatchProp when thunks are dispatched
+export type ThunkDispatch = ReduxThunkThunkDispatch<StoreState, void, Redux.AnyAction>
+export type ThunkDispatchProp = { dispatch: ThunkDispatch }
+
+// export renamed action and state types
+export { Action as CodeAction, State as CodeState } from './code'
+export { Action as IOAction, State as IOState } from './io'
+
 
 export function Store(props: { children?: any }) {
     return <Provider store={reduxStore}>
