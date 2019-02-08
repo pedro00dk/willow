3db778ff26b90470a51fdf3093f4453b37927721
Pedro Henrique <pedro00dk@gmail.com>
2019-02-08 11:38:48 -0300

    Add session fetch action in app container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import cn from 'classnames'
 import { css } from 'emotion'
 import * as React from 'react'
+import { connect } from 'react-redux'
 import logo from '../public/logo.svg'
 import { CodeEditor } from './components/editor/CodeEditor'
 import { IOEditor } from './components/editor/IOEditor'
-import { Store } from './reducers/Store'
+import { fetch } from './reducers/session'
+import { ThunkDispatchProp } from './reducers/Store'
 
 
-export function App() {
+// tslint:disable-next-line:variable-name
+export const App = connect()((props: ThunkDispatchProp) => {
+    React.useEffect(() => { props.dispatch(fetch()) })
     return <div className={cn('d-flex flex-column', css({ width: '100vw', height: '100vh' }))}>
         <div className='d-flex flex-row'>
             <div className='d-block flex-fill'>
@@ -20,7 +24,7 @@ export function App() {
             </div>
         </div>
     </div>
-}
+})
 
 function Header() {
     return <nav className='navbar navbar-expand static-top navbar-dark bg-dark shadow-lg'>
