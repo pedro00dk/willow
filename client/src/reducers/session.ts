ec2d763a3d2dfb31d516aa90c2c6d0a9a0aa4708
Pedro Henrique <pedro00dk@gmail.com>
2019-02-08 11:35:17 -0300

    Create session reducer for testing thunk dispatches

---
+++ client/src/reducers/session.ts
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import { Reducer } from 'redux'
+import { serverAddress } from '../server'
+import { ThunkDispatch } from './Store'
+
+
+export type State = {
+    readonly session: string
+    readonly fetching: boolean
+    readonly failed: boolean
+}
+export type Action = { type: 'session/fetch', payload?: { session: string }, error?: string }
+
+const initialState: State = {
+    session: undefined,
+    fetching: false,
+    failed: false
+}
+
+export const reducer: Reducer<State, Action> = (state = initialState, action) => {
+    if (!action) return state
+    switch (action.type) {
+        case 'session/fetch':
+            return !action.payload && !action.error
+                ? { ...state, fetching: true, error: false }
+                : action.payload
+                    ? { ...state, session: action.payload.session, fetching: false, failed: false }
+                    : { ...state, fetching: false, failed: true }
+    }
+    return state
+}
+
+export function fetch() {
+    return async (dispatch: ThunkDispatch) => {
+        dispatch({ type: 'session/fetch' })
+        try {
+            const session = (await axios.get(`${serverAddress}/session`, { withCredentials: true }))
+            dispatch({ type: 'session/fetch', payload: { session: session.data.session } })
+        } catch (error) {
+            dispatch({ type: 'session/fetch', error: error.message })
+        }
+    }
+}
