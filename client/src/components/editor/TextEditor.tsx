89565881be3edfab8b4afe4625fe4c185fdb499f
Pedro Henrique <pedro00dk@gmail.com>
2019-02-09 16:39:34 -0300

    Fix editor misbehaviour when resized

--- client/src/components/editor/TextEditor.tsx
+++ client/src/components/editor/TextEditor.tsx
@@ -17,7 +17,26 @@ export const TextEditor = React.memo((props: EditorProps) => {
     const editorRef = React.useRef<HTMLDivElement>(undefined)
     const [editor, setEditor] = React.useState<ace.Editor>(undefined)
     React.useEffect(
-        () => editorRef.current ? setEditor(ace.edit(editorRef.current)) : undefined,
+        () => {
+            if (!editorRef.current) return
+            const editor = ace.edit(editorRef.current)
+            setEditor(editor)
+            const parentSize = {
+                width: editorRef.current.parentElement.clientWidth,
+                height: editorRef.current.parentElement.clientHeight
+            }
+            const checkResizeInterval = window.setInterval(
+                () => {
+                    const width = editorRef.current.parentElement.clientWidth
+                    const height = editorRef.current.parentElement.clientHeight
+                    if (parentSize.width !== width || parentSize.height !== height) editor.resize()
+                    parentSize.width = width
+                    parentSize.height = height
+                },
+                2000
+            )
+            return () => window.clearInterval(checkResizeInterval)
+        },
         [editorRef]
     )
     React.useEffect(
