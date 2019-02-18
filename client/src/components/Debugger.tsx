c78f5bb89854cb31849be4b1857c97be10dd06dd
Pedro Henrique <pedro00dk@gmail.com>
2019-02-17 22:44:36 -0300

    Connect Debugger component to debug and code reducers

--- client/src/components/Debugger.tsx
+++ client/src/components/Debugger.tsx
@@ -1,23 +1,32 @@
 import * as React from 'react'
+import { connect } from 'react-redux'
+import { start, stop } from '../reducers/debug'
+import { CodeState, DebugState, StoreState, ThunkDispatchProp } from '../reducers/Store'
 
-import play from '../../public/buttons/play.png'
-import restart from '../../public/buttons/restart.png'
-import stepInto from '../../public/buttons/stepInto.png'
-import stepOut from '../../public/buttons/stepOut.png'
-import stepOver from '../../public/buttons/stepOver.png'
-import stop from '../../public/buttons/stop.png'
+import playImg from '../../public/buttons/play.png'
+import restartImg from '../../public/buttons/restart.png'
+import stepIntoImg from '../../public/buttons/stepInto.png'
+import stepOutImg from '../../public/buttons/stepOut.png'
+import stepOverImg from '../../public/buttons/stepOver.png'
+import stopImg from '../../public/buttons/stop.png'
 
 
-type EditorProps = {}
+type ConnectedDebuggerProps = { code: CodeState, debug: DebugState }
+type DebuggerProps = {}
 // tslint:disable-next-line:variable-name
-export const Debugger = React.memo((props: EditorProps) => {
-
+export const Debugger = connect<ConnectedDebuggerProps, {}, DebuggerProps, StoreState>(
+    state => ({ code: state.code, debug: state.debug })
+)((props: ThunkDispatchProp & ConnectedDebuggerProps & DebuggerProps) => {
     return <>
-        <img src={play} className='h-100' />
-        <img src={stepOver} className='h-100' />
-        <img src={stepInto} className='h-100' />
-        <img src={stepOut} className='h-100' />
-        <img src={restart} className='h-100' />
-        <img src={stop} className='h-100' />
+        <img src={playImg} className='h-100'
+            onClick={() => props.dispatch(start('python', props.code.text.join('\n')))}
+        />
+        <img src={stepOverImg} className='h-100' />
+        <img src={stepIntoImg} className='h-100' />
+        <img src={stepOutImg} className='h-100' />
+        <img src={restartImg} className='h-100' />
+        <img src={stopImg} className='h-100'
+            onClick={() => props.dispatch(stop())}
+        />
     </>
 })
