3bed7ad8c7f78ece78de114720b8e44dd2380086
Pedro Henrique <pedro00dk@gmail.com>
2019-02-17 22:09:22 -0300

    Detect and process errors from tracers client

--- server/src/tracers/client.ts
+++ server/src/tracers/client.ts
@@ -11,7 +11,6 @@ export class Client {
      * Creates the client with the received server address.
      */
     constructor(address: string) {
-
         this.address = address
     }
 
@@ -19,28 +18,49 @@ export class Client {
      * Returns the tracer server suppliers.
      */
     async getSuppliers() {
-        return (await axios.get(`${this.address}/suppliers`)).data as string[]
+        try {
+            const response = await axios.get(`${this.address}/suppliers`)
+            return response.data as string[]
+        } catch (error) {
+            throw new Error(error.response ? error.response.data : error.toString())
+        }
     }
 
     /**
      * Returns the tracer server sessions.
      */
     async getSessions() {
-        return (await axios.get(`${this.address}/sessions`)).data as { id: number, supplier: string, state: string }[]
+        try {
+            const response = await axios.get(`${this.address}/sessions`)
+            return response.data as { id: number, supplier: string, state: string }[]
+        } catch (error) {
+            throw new Error(error.response ? error.response.data : error.toString())
+        }
     }
 
     /**
      * Creates a new tracer in the tracer server and returns its id.
      */
     async create(supplier: string, code: string) {
-        return (await axios.post(`${this.address}/create`, { supplier, code })).data as { id: number, supplier: string }
+        try {
+            const response = await axios.post(`${this.address}/create`, { supplier, code })
+            return response.data as { id: number, supplier: string }
+        } catch (error) {
+            throw new Error(error.response ? error.response.data : error.toString())
+        }
     }
 
     /**
      * Executes a command in the tracer refereed by the received id and returns the result.
      */
     async execute(id: number, action: string, args: unknown[]) {
-        return (await axios.post(`${this.address}/execute`, { id, action, args }, { transformResponse: [] }))
-            .data as string
+        try {
+            const response = await axios.post(
+                `${this.address}/execute`, { id, action, args }, { transformResponse: res => res }
+            )
+            return { data: response.data as string, finished: response.headers.finished === 'true' }
+        } catch (error) {
+            throw new Error(error.response ? error.response.data : error.toString())
+        }
     }
 }
