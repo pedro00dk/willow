d7ff9d5ed353924e102ad5947603238321905508
Pedro Henrique <pedro00dk@gmail.com>
2019-02-25 01:02:23 -0300

    Create run script for easier initialization

---
+++ tracerJava/run.sh
@@ -0,0 +1,2 @@
+javac -cp src/:.lib/* --add-exports jdk.jdi/com.sun.tools.jdi=ALL-UNNAMED -d out/ src/Main.java &> /dev/null
+java -cp out/:lib/* --add-exports jdk.jdi/com.sun.tools.jdi=ALL-UNNAMED Main "$@"
