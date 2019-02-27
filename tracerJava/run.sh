d224fa5753b6a9ee27e50d9bc7a857dda1132bdb
Pedro Henrique <pedro00dk@gmail.com>
2019-02-27 14:14:29 -0300

    Remove compiler 'note' and 'picked up' messages from javac and java stderr

--- tracerJava/run.sh
+++ tracerJava/run.sh
@@ -1,2 +1,2 @@
-javac -cp src/:lib/* --add-exports jdk.jdi/com.sun.tools.jdi=ALL-UNNAMED -d out/ src/Main.java &> /dev/null
-java -cp out/:lib/* --add-exports jdk.jdi/com.sun.tools.jdi=ALL-UNNAMED Main "$@"
+(javac -cp src/:lib/* --add-exports jdk.jdi/com.sun.tools.jdi=ALL-UNNAMED -d out/ src/Main.java 3>&1 1>&2 2>&3 | grep -iv -e 'note' -e 'picked') 3>&1 1>&2 2>&3
+(java -cp out/:lib/* --add-exports jdk.jdi/com.sun.tools.jdi=ALL-UNNAMED Main "$@" 3>&1 1>&2 2>&3 | grep -iv 'picked') 3>&1 1>&2 2>&3
