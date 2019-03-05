ddce388f497d5b349fdba0c78367710814b25798
Pedro Henrique <pedro00dk@gmail.com>
2019-03-05 16:49:08 -0300

    Update inspector results to better match the current protocols

--- tracerPython/src/core/inspector.py
+++ tracerPython/src/core/inspector.py
@@ -1,5 +1,4 @@
 import types
-
 from .util import ExceptionUtil, FrameUtil
 
 
@@ -8,38 +7,19 @@ class Inspector:
     Inspects the received frame, building the stack and heap data and references.
     """
 
-    def __init__(self):
-        """
-        Initializes the Inspector.
-        """
-        pass
-
     def inspect(self, frame: types.FrameType, event: str, args, exec_call_frame: types.FrameType):
-        """
-        Inspects the application state. Analyses the stack and heap, collecting the objects.
-        """
-        stack_frames, stack_lines = self.inspect_stack(frame, exec_call_frame, True)
-        stack_references, heap_graph, user_classes = self.inspect_heap(stack_frames)
-
-        # args -> exception type, exception object, exception traceback (different from exception object __traceback__)
-        args = args if event != 'exception' else ExceptionUtil.dump(args[1], args[2])
-
-        finish = event == 'return' and len(stack_frames) == 1
+        stack, frames = self.inspect_stack(frame, exec_call_frame, True)
+        heap, stack_references = self.inspect_heap(frames)
         return {
             'name': event,
-            'args': args,
-            'line': stack_lines[0]['line'],
-            'stackLines': stack_lines,
+            'finish': event == 'return' and len(frames) == 1,
+            'exception': ExceptionUtil.dump(args[1], args[2]) if event == 'exception' else None,
+            'stack': stack,
             'stackReferences': stack_references,
-            'heapGraph': heap_graph,
-            'userClasses': user_classes,
-            'finish': finish
+            'heap': heap
         }
 
     def inspect_stack(self, frame: types.FrameType, exec_call_frame: types.FrameType, only_frame_file: bool):
-        """
-        Inspects the application stack.
-        """
         current_frame = frame
         frames = []
         while current_frame != exec_call_frame:
@@ -48,89 +28,74 @@ class Inspector:
 
         frame_file = FrameUtil.file(frame)
         frames = [frame for frame in frames if not only_frame_file or FrameUtil.is_file(frame, frame_file)]
-
-        lines = [{'name': FrameUtil.name(frame), 'line': FrameUtil.line(frame)} for frame in frames]
-        return frames, lines
+        stack = [{'name': FrameUtil.name(frame), 'line': FrameUtil.line(frame)} for frame in frames]
+        return stack, frames
 
     def inspect_heap(self, stack_frames: list):
-        """
-        Inspects the application heap by looking every object recursively found from the stack frames.
-        """
-        module_name = FrameUtil.module(stack_frames[-1])
-
+        module = FrameUtil.module(stack_frames[-1])
+        heap = {}
+        classes = set()
         stack_references = []
-        heap_graph = {}
-        user_classes = set()
-
         for frame in stack_frames[::-1]:
-            frame_variables = FrameUtil.locals(frame)
-            frame_references = [
-                (name, self.inspect_object(frame_variables[name], heap_graph, user_classes, module_name))
-                for name, value in frame_variables.items() if not name.startswith('_')
+            scope_variables = FrameUtil.locals(frame)
+            scope_references = [
+                (name, self.inspect_obj(scope_variables[name], heap, classes, module))
+                for name, value in scope_variables.items() if not name.startswith('_')
             ]
-            stack_references.append(frame_references)
+            stack_references.append(scope_references)
 
-        return stack_references, heap_graph, [user_class.__name__ for user_class in user_classes]
+        return heap, stack_references
 
-    def inspect_object(self, obj, heap_graph: dict, user_classes: set, module_name: str):
+    def inspect_obj(self, obj, heap: dict, classes: set, module: str):
         """
-        Inspects the received object.
-        If the object is a const (bool, int, float, None, complex, str), returns its value.
-        If the object is a type (type), returns its type name.
-        Otherwise, returns the object reference (list with a single number inside) recursively, inspecting object
-        members and filling the heap_graph and user_classes
+        Inspects obj.
+        If obj is a const (bool, int, float, None, complex, str), returns its value.
+        If obj is a type (type), returns its type name.
+        Otherwise, returns a obj reference (list with a single number inside) inspecting obj members and filling the
+        heap and classes.
         """
-        # already inspected
         reference = id(obj)
-        if reference in heap_graph:
+        if reference in heap:
             return reference,
 
-        # const type
         if isinstance(obj, (bool, int, float, type(None))):
             return obj
         if isinstance(obj, (complex, str)):
             return repr(obj)
         if isinstance(obj, type):
-            if obj.__module__ == module_name:
-                user_classes.add(obj)
+            if obj.__module__ == module:
+                classes.add(obj)
             return obj.__name__
 
-        # known type
         generic_type = ''
+        members = None
 
-        if isinstance(obj, (list, tuple, set)):
+        if isinstance(obj, (tuple, list, set)):
+            generic_type = 'tuple' if isinstance(obj, tuple) else 'list' if isinstance(obj, list) else 'set'
             members = [*enumerate(obj)]
-            generic_type = 'list' if isinstance(obj, (list, tuple)) else 'set'
         elif isinstance(obj, dict):
-            members = [*obj.items()]
             generic_type = 'map'
-        elif isinstance(obj, (*user_classes,)):
+            members = [*obj.items()]
+        elif isinstance(obj, (*classes,)):
+            generic_type = 'other'
             members = [(name, value) for name, value in vars(obj).items() if not name.startswith('_')]
-            generic_type = 'udo'
+        else:  # unknown type
+            generic_type = 'other'
 
-        # unknown type
-        else:
-            members = None
-
-        # known object type processing
-        if members:
-
+        if members:  # known object type processing
             # add reference to heap graph (has to be added before other objects inspections)
-            heap_graph[reference] = {}
+            heap[reference] = {}
             members_inspections = [
-                (
-                    self.inspect_object(name, heap_graph, user_classes, module_name),
-                    self.inspect_object(value, heap_graph, user_classes, module_name)
-                )
+                (self.inspect_obj(name, heap, classes, module), self.inspect_obj(value, heap, classes, module))
                 for name, value in members
             ]
-
-            # update with all data
-            heap_graph[reference] = {
-                'type': generic_type, 'languageType': type(obj).__name__, 'members': members_inspections
+            heap[reference] = {
+                'type': generic_type,
+                'lType': type(obj).__name__,
+                'userDefined': generic_type == 'other',
+                'members': members_inspections
             }
             return reference,
-
-        # unknown object type processing
-        else:
-            return self.inspect_object(type(obj), heap_graph, user_classes, module_name)
+        else:  # unknown object type processing
+            # instead of inspecting unknown objects, i decided to inspect its type
+            return self.inspect_obj(type(obj), heap, classes, module)
