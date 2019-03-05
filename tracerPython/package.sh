b9fe058d1e4ce1c3906a10d590c6ee0dfe077277
Pedro Henrique <pedro00dk@gmail.com>
2019-03-05 16:40:04 -0300

    Add initial protobuf file generation support to python tracer

--- tracerPython/package.sh
+++ tracerPython/package.sh
@@ -21,17 +21,22 @@ case $command in
             done
         fi
         ;;
+    protobuf)
+        mkdir --parent ./src/protobuf && protoc --python_out=./src/protobuf --proto_path=../protobuf/ ../protobuf/*
+        # fix import paths of other proto modules
+        sed --in-place --regexp-extended "s/(import .+_pb2 as .+_pb2)/from . \1/" -- ./src/protobuf/*_pb2.py
+        ;;
     lint)
         ./.venv/bin/flake8
         ;;
     test)
-        echo 'no tests implemented yet'
+        echo "no tests implemented yet"
         ;;
     start)
         shift
         ./.venv/bin/python ./src/main.py "$@"
         ;;
     *)
-        echo 'command not found'
+        echo "command not found"
         ;;
 esac
