bb8796d9b2efed3dc4d9e2c7095b28ae72c45874
Pedro Henrique <pedro00dk@gmail.com>
2019-03-13 12:57:49 -0300

    Add java tracer protobuf dependencies and generated classes

--- tracerJava/package.sh
+++ tracerJava/package.sh
@@ -1,11 +1,13 @@
 command=$1
 
 case $command in
+    protobuf)
+        mkdir --parent ./src/protobuf && protoc --java_out=./src/protobuf --proto_path=../protobuf/ ../protobuf/*
+        # fix missing packages
+        sed --in-place "1s/^/package protobuf;\n/" -- ./src/protobuf/*.java
+        ;;
     build)
         (javac -cp src/:lib/* --add-exports jdk.jdi/com.sun.tools.jdi=ALL-UNNAMED -d out/ src/Main.java 3>&1 1>&2 2>&3 | grep -iv -E 'note|picked' | cat) 3>&1 1>&2 2>&3
-        ;;
-    test)
-        echo 'no tests implemented yet'
         ;;
     start)
         shift
