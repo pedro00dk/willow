76978545ecb359c6dddb27c158bc2d1337c760a9
Pedro Henrique <pedro00dk@gmail.com>
2019-03-16 11:49:08 -0300

    Create the tracers service class based on the protocol

--- tracers/src/server.ts
+++ tracers/src/server.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
 import * as log from 'npmlog'
+import * as protocol from './protobuf/protocol'
 import { StepConstraints } from './tracer/step-constraints'
 import { Tracer } from './tracer/tracer'
 import { TracerProcess } from './tracer/tracer-process'
@@ -15,7 +16,7 @@ export class Server {
 
     constructor(private mode: 'json' | 'proto', private port: number, private tracers: { [language: string]: string }) {
         this.server = express()
-        this.server.use(express.json())
+        if (this.mode === 'json') this.server.use(express.json())
         this.configureServerRoutes()
     }
 
@@ -80,15 +81,15 @@ export class Server {
         let result: unknown
         try {
             if (action === 'getState') result = tracer.getState()
-            else if (action === 'start') result = await tracer.start(args[0] as string, args[1] as string)
+            //else if (action === 'start') result = await tracer.start(args[0] as string, args[1] as string)
             else if (action === 'stop') result = tracer.stop()
             else if (action === 'step') result = await tracer.step()
             else if (action === 'stepOver') result = await tracer.stepOver()
             else if (action === 'stepOut') result = await tracer.stepOut()
             else if (action === 'continue') result = await tracer.continue()
-            else if (action === 'input') result = tracer.input(args[0] as string[])
+            //else if (action === 'input') result = tracer.input(args[0] as string[])
             else if (action === 'getBreakpoints') result = tracer.getBreakpoints()
-            else if (action === 'setBreakpoints') result = tracer.setBreakpoints(new Set(args[0] as number[]))
+            //else if (action === 'setBreakpoints') result = tracer.setBreakpoints(new Set(args[0] as number[]))
             else throw new Error('action not found')
         } catch (error) {
             throw error
@@ -104,5 +105,88 @@ export class Server {
     listen() {
         log.info(Server.name, 'listen', { port: this.port })
         this.server.listen(this.port)
+    }
+}
+
+class TracersService {
+    private sessions: Map<number, { language: string; tracer: Tracer }> = new Map()
+    private sessionIdGenerator: number = 0
+
+    constructor(private tracers: { [language: string]: string }) {}
+
+    getLanguages(request: protocol.Empty): protocol.Languages {
+        return protocol.Languages.create({ languages: Object.keys(this.tracers) })
+    }
+
+    getSessions(request: protocol.Empty): protocol.Sessions {
+        return protocol.Sessions.create({
+            sessions: [...this.sessions.entries()].map(([id, { language }]) =>
+                protocol.Session.create({ id: protocol.Id.create({ id }), language })
+            )
+        })
+    }
+
+    private checkGetSessionTracer(id: number) {
+        if (!this.sessions.has(id)) throw new Error('session id not found')
+        return this.sessions.get(id).tracer
+    }
+
+    private removeTracerIfStopped(id: number) {
+        if (!this.sessions.has(id) || this.sessions.get(id).tracer.getState() !== 'stopped') return
+        log.info(Server.name, 'execute - tracer stopped', { id })
+        this.sessions.delete(id)
+    }
+
+    async start(request: protocol.StartRequest): Promise<protocol.StartResponse> {
+        if (!this.tracers[request.language]) throw new Error('unexpected language')
+        const id = this.sessionIdGenerator++
+        log.info(Server.name, 'start', { id, language: request.language })
+        const tracer = new TracerWrapper(new TracerProcess(this.tracers[request.language]))
+        tracer.addStepProcessor(new StepConstraints(1000, 50, 50, 20, 100, 10))
+        this.sessions.set(id, { language: request.language, tracer })
+        const response = await tracer.start(request.start)
+        this.removeTracerIfStopped(id)
+        return protocol.StartResponse.create({ id: protocol.Id.create({ id }), response })
+    }
+
+    stop(request: protocol.Id): protocol.Empty {
+        this.checkGetSessionTracer(request.id).stop()
+        this.removeTracerIfStopped(request.id)
+        return protocol.Empty.create()
+    }
+
+    async step(request: protocol.Id): Promise<protocol.TracerResponse> {
+        const response = await this.checkGetSessionTracer(request.id).step()
+        this.removeTracerIfStopped(request.id)
+        return response
+    }
+
+    async stepOver(request: protocol.Id): Promise<protocol.TracerResponses> {
+        const responses = await this.checkGetSessionTracer(request.id).stepOver()
+        this.removeTracerIfStopped(request.id)
+        return responses
+    }
+
+    async stepOut(request: protocol.Id): Promise<protocol.TracerResponses> {
+        const responses = await this.checkGetSessionTracer(request.id).stepOut()
+        this.removeTracerIfStopped(request.id)
+        return responses
+    }
+
+    async continue(request: protocol.Id): Promise<protocol.TracerResponses> {
+        const responses = await this.checkGetSessionTracer(request.id).continue()
+        this.removeTracerIfStopped(request.id)
+        return responses
+    }
+
+    input(request: protocol.InputRequest): protocol.Empty {
+        this.checkGetSessionTracer(request.id.id).input(request.input)
+        return protocol.Empty.create()
+    }
+    getBreakpoints(request: protocol.Id): protocol.Breakpoints {
+        return this.checkGetSessionTracer(request.id).getBreakpoints()
+    }
+    setBreakpoints(request: protocol.BreakpointsRequest): protocol.Empty {
+        return undefined
     }
 }
