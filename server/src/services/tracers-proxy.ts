23989242514025bb8b75a8d42e8ef67e367aec95
Pedro Henrique <pedro00dk@gmail.com>
2019-03-17 18:40:16 -0300

    Implement better encoding/deconding approach in tracer proxy

--- server/src/services/tracers-proxy.ts
+++ server/src/services/tracers-proxy.ts
@@ -1,4 +1,5 @@
-import Axios, * as axios from 'axios'
+import * as axios from 'axios'
+import * as log from 'npmlog'
 import * as protocol from '../protobuf/protocol'
 
 /**
@@ -24,7 +25,7 @@ export class TracersProxy {
                       }
                     : {}
         } catch (error) {
-            throw new Error(error.toString())
+            throw new Error(error.response ? error.response.data : error.toString())
         }
     }
 
@@ -32,149 +33,60 @@ export class TracersProxy {
         api: axios.AxiosInstance,
         method: string,
         requestObject: T,
-        toProto: (request: T) => Uint8Array,
-        toJson: (request: T) => {},
-        decode: (buffer: Buffer) => U,
-        create: (properties: {}) => U
+        requestType: { encode: (response: T) => protobuf.Writer },
+        responseType: { create: (properties: {}) => U; decode: (buffer: Buffer) => U }
     ) {
+        log.info(TracersProxy.name, 'request', method)
         try {
-            const request = this.mode === 'proto' ? toProto(requestObject) : toJson(requestObject)
-            const response = await api.post(method, request, this.requestConfig)
-            return this.mode === 'proto' ? decode(response.data) : create(response.data)
+            const request = this.mode === 'proto' ? requestType.encode(requestObject).finish() : requestObject
+            const data = (await api.post(method, request, this.requestConfig)).data
+            return this.mode === 'proto' ? responseType.decode(data) : responseType.create(data)
         } catch (error) {
-            throw new Error(error.toString())
+            throw new Error(error.response ? error.response.data : error.toString())
         }
     }
 
     getLanguages(request: protocol.Empty): Promise<protocol.Languages> {
-        return this.request(
-            this.api,
-            '/getLanguages',
-            request,
-            request => protocol.Empty.encodeDelimited(request).finish(),
-            request => request,
-            buffer => protocol.Languages.decodeDelimited(buffer),
-            properties => protocol.Languages.create(properties)
-        )
+        return this.request(this.api, '/getLanguages', request, protocol.Empty, protocol.Languages)
     }
 
     getSessions(request: protocol.Empty): Promise<protocol.Sessions> {
-        return this.request(
-            this.api,
-            '/getSessions',
-            request,
-            request => protocol.Empty.encodeDelimited(request).finish(),
-            request => request,
-            buffer => protocol.Sessions.decodeDelimited(buffer),
-            properties => protocol.Sessions.create(properties)
-        )
+        return this.request(this.api, '/getSessions', request, protocol.Empty, protocol.Sessions)
     }
 
-    async start(request: protocol.StartRequest): Promise<protocol.StartResponse> {
-        return this.request(
-            this.api,
-            '/start',
-            request,
-            request => protocol.StartRequest.encodeDelimited(request).finish(),
-            request => request,
-            buffer => protocol.StartResponse.decodeDelimited(buffer),
-            properties => protocol.StartResponse.create(properties)
-        )
+    start(request: protocol.StartRequest): Promise<protocol.StartResponse> {
+        return this.request(this.api, '/start', request, protocol.StartRequest, protocol.StartResponse)
     }
 
     stop(request: protocol.Id): Promise<protocol.Empty> {
-        return this.request(
-            this.api,
-            '/stop',
-            request,
-            request => protocol.Id.encodeDelimited(request).finish(),
-            request => request,
-            buffer => protocol.Empty.decodeDelimited(buffer),
-            properties => protocol.Empty.create(properties)
-        )
+        return this.request(this.api, '/stop', request, protocol.Id, protocol.Empty)
     }
 
     step(request: protocol.Id): Promise<protocol.TracerResponse> {
-        return this.request(
-            this.api,
-            '/step',
-            request,
-            request => protocol.Id.encodeDelimited(request).finish(),
-            request => request,
-            buffer => protocol.TracerResponse.decodeDelimited(buffer),
-            properties => protocol.TracerResponse.create(properties)
-        )
+        return this.request(this.api, '/step', request, protocol.Id, protocol.TracerResponse)
     }
 
-    async stepOver(request: protocol.Id): Promise<protocol.TracerResponses> {
-        return this.request(
-            this.api,
-            '/stepOver',
-            request,
-            request => protocol.Id.encodeDelimited(request).finish(),
-            request => request,
-            buffer => protocol.TracerResponses.decodeDelimited(buffer),
-            properties => protocol.TracerResponses.create(properties)
-        )
+    stepOver(request: protocol.Id): Promise<protocol.TracerResponses> {
+        return this.request(this.api, '/stepOver', request, protocol.Id, protocol.TracerResponses)
     }
 
-    async stepOut(request: protocol.Id): Promise<protocol.TracerResponses> {
-        return this.request(
-            this.api,
-            '/stepOut',
-            request,
-            request => protocol.Id.encodeDelimited(request).finish(),
-            request => request,
-            buffer => protocol.TracerResponses.decodeDelimited(buffer),
-            properties => protocol.TracerResponses.create(properties)
-        )
+    stepOut(request: protocol.Id): Promise<protocol.TracerResponses> {
+        return this.request(this.api, '/stepOut', request, protocol.Id, protocol.TracerResponses)
     }
 
-    async continue(request: protocol.Id): Promise<protocol.TracerResponses> {
-        return this.request(
-            this.api,
-            '/continue',
-            request,
-            request => protocol.Id.encodeDelimited(request).finish(),
-            request => request,
-            buffer => protocol.TracerResponses.decodeDelimited(buffer),
-            properties => protocol.TracerResponses.create(properties)
-        )
+    continue(request: protocol.Id): Promise<protocol.TracerResponses> {
+        return this.request(this.api, '/continue', request, protocol.Id, protocol.TracerResponses)
     }
 
     input(request: protocol.InputRequest): Promise<protocol.Empty> {
-        return this.request(
-            this.api,
-            '/input',
-            request,
-            request => protocol.InputRequest.encodeDelimited(request).finish(),
-            request => request,
-            buffer => protocol.Empty.decodeDelimited(buffer),
-            properties => protocol.Empty.create(properties)
-        )
+        return this.request(this.api, '/input', request, protocol.InputRequest, protocol.Empty)
     }
 
     getBreakpoints(request: protocol.Id): Promise<protocol.Breakpoints> {
-        return this.request(
-            this.api,
-            '/getBreakpoints',
-            request,
-            request => protocol.Id.encodeDelimited(request).finish(),
-            request => request,
-            buffer => protocol.Breakpoints.decodeDelimited(buffer),
-            properties => protocol.Breakpoints.create(properties)
-        )
+        return this.request(this.api, '/getBreakpoints', request, protocol.Id, protocol.Breakpoints)
     }
 
     setBreakpoints(request: protocol.BreakpointsRequest): Promise<protocol.Empty> {
-        return this.request(
-            this.api,
-            '/setBreakpoints',
-            request,
-            request => protocol.BreakpointsRequest.encodeDelimited(request).finish(),
-            request => request,
-            buffer => protocol.Empty.decodeDelimited(buffer),
-            properties => protocol.Empty.create(properties)
-        )
+        return this.request(this.api, '/setBreakpoints', request, protocol.BreakpointsRequest, protocol.Empty)
     }
 }
