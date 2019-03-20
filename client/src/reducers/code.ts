c417e979909b4ad030d843c7d709adc7e5e21be2
Pedro Henrique <pedro00dk@gmail.com>
2019-03-19 23:57:10 -0300

    Remove language features from code reducer

--- client/src/reducers/code.ts
+++ client/src/reducers/code.ts
@@ -1,20 +1,17 @@
 import { Reducer } from 'redux'
 
 type State = {
-    readonly language: string
     readonly text: string[]
     readonly breakpoints: ReadonlySet<number>
     readonly markers: ReadonlySet<{ line: number; type: 'highlight' | 'warning' | 'error' }>
 }
 
 type Action =
-    | { type: 'code/setLanguage'; payload: { language: string } }
     | { type: 'code/setText'; payload: { text: string[] } }
     | { type: 'code/setBreakpoint'; payload: { line: number } }
     | { type: 'code/setMarkers'; payload: { markers: { line: number; type: 'highlight' | 'warning' | 'error' }[] } }
 
 const initialState: State = {
-    language: undefined,
     text: [],
     breakpoints: new Set(),
     markers: new Set()
@@ -22,7 +19,6 @@ const initialState: State = {
 
 export const reducer: Reducer<State, Action> = (state = initialState, action) => {
     switch (action.type) {
-        case 'code/setLanguage':
         case 'code/setText':
             return { ...state, ...action.payload }
         case 'code/setBreakpoint': {
