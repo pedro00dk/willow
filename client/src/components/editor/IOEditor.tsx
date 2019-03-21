b7bde88fa1b986aef418c5935ec9d1ebf699d158
Pedro Henrique <pedro00dk@gmail.com>
2019-03-21 12:29:01 -0300

    Temporary (not working properly) implementation of IOEditor

--- client/src/components/editor/IOEditor.tsx
+++ client/src/components/editor/IOEditor.tsx
@@ -1,7 +1,7 @@
 import * as ace from 'brace'
 import * as React from 'react'
+import { useDispatch, useRedux } from '../../reducers/Store'
 import { TextEditor } from './TextEditor'
-import { useRedux } from '../../reducers/Store'
 
 type EditorExecEvent = {
     [props: string]: unknown
@@ -14,11 +14,8 @@ type EditorExecEvent = {
 
 export function IOEditor() {
     const [editor, setEditor] = React.useState<ace.Editor>(undefined)
+    const dispatch = useDispatch()
     const { io } = useRedux(state => ({ io: state.io }))
-    React.useEffect(() => {
-        if (!editor) return
-        editor.session.doc.setValue(io.content.join('\n'))
-    })
     React.useEffect(() => {
         if (!editor) return
         editor.$blockScrolling = Infinity
@@ -37,8 +34,18 @@ export function IOEditor() {
                 event.stopPropagation()
             }
         }
+        const onChange = (change: ace.EditorChangeEvent) =>
+            dispatch({
+                type: 'io/setInput',
+                payload: { input: editor.session.getLine(editor.session.getLength() - 1) }
+            })
         ;(editor.commands as any).on('exec', onExec)
+        // editor.on('change', onChange)
         return () => (editor.commands as any).off('exec', onExec)
     }, [editor])
+    React.useEffect(() => {
+        if (!editor) return
+        editor.session.doc.setValue(`${io.content.join('')}\n${io.input}`)
+    }, [io])
     return <TextEditor onEditorUpdate={setEditor} />
 }
--- client/src/reducers/io.ts
+++ client/src/reducers/io.ts
@@ -2,7 +2,6 @@ import { Reducer } from 'redux'
 
 type State = {
     readonly content: ReadonlyArray<string>
-    readonly lastLineBreak: boolean
     readonly input: string
 }
 
@@ -10,11 +9,10 @@ type Action =
     | { type: 'io/reset' }
     | { type: 'io/appendOutput'; payload: { output: string } }
     | { type: 'io/commitInput' }
-    | { type: 'io/setInputLine'; payload: { input: string } }
+    | { type: 'io/setInput'; payload: { input: string } }
 
 const initialState: State = {
-    content: [''],
-    lastLineBreak: false,
+    content: [],
     input: ''
 }
 
@@ -22,24 +20,11 @@ export const reducer: Reducer<State, Action> = (state = initialState, action) =>
     switch (action.type) {
         case 'io/reset':
             return { ...initialState }
-        case 'io/appendOutput': {
-            const outputLines = action.payload.output.split(/\r?\n/)
-            const outputEndsInLineBreak = outputLines.length > 1 && outputLines[outputLines.length - 1].length === 0
-            const mergeLine = state.lastLineBreak
-                ? []
-                : [`${state.content[state.content.length - 1]}${outputLines[0]}`]
-            const contentLinesToConcat = mergeLine.length === 0 ? state.content : state.content.slice(0, -1)
-            const outputLinesToConcat = mergeLine.length === 0 ? outputLines : outputLines.slice(1)
-            const content = [...contentLinesToConcat, ...mergeLine, ...outputLinesToConcat]
-            return { ...state, lastLineBreak: outputEndsInLineBreak, content }
-        }
-        case 'io/commitInput': {
-            const content = state.lastLineBreak
-                ? [...state.content, state.input]
-                : [...state.content.slice(0, -1), `${state.content[state.content.length - 1]}${state.input}`]
-            return { ...state, lastLineBreak: true, content }
-        }
-        case 'io/setInputLine':
+        case 'io/appendOutput':
+            return { ...state, content: [...state.content, action.payload.output] }
+        case 'io/commitInput':
+            return { ...state, content: [...state.content, `${state.input}\n`], input: '' }
+        case 'io/setInput':
             return { ...state, input: action.payload.input }
     }
     return state
