4b48030118e39ade408d68674d7e8b4f29607bcc
Pedro Henrique <pedro00dk@gmail.com>
2019-03-30 18:30:26 -0300

    Update parcel to support newer targets

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,6 @@
+// required for parcel to generate target > ES5 builds
+import '@babel/polyfill'
+
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
 import { App } from './App'
