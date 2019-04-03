0e581f53fdd198c30416a89711ef756bdf5bbfcd
Pedro Henrique <pedro00dk@gmail.com>
2019-04-03 14:03:39 -0300

    Implement LanguageSelector reload support

--- client/src/components/LanguageSelector.tsx
+++ client/src/components/LanguageSelector.tsx
@@ -5,8 +5,9 @@ import { actions as languageActions } from '../reducers/language'
 import { useDispatch, useRedux } from '../reducers/Store'
 
 const styles = {
-    select: css({ flex: '0 1 auto !important', width: 'auto !important' }),
-    input: css({ display: 'inline-flex !important', width: 'auto !important' })
+    group: cn('d-flex input-group ml-3', css({ width: 'auto' })),
+    button: cn('d-inline-flex align-items-center btn btn-outline-secondary', css({ width: '8rem' })),
+    select: cn('custom-select', css({ flex: '0 1 auto !important', width: '8rem !important' }))
 }
 
 export function LanguageSelector() {
@@ -19,23 +20,22 @@ export function LanguageSelector() {
     }, [])
 
     return (
-        <div className={cn('input-group ml-3', styles.input)}>
+        <div className={styles.group}>
             <div
                 className='input-group-prepend'
                 onClick={() => dispatch(languageActions.fetch())}
                 onMouseEnter={() => setMouseOver(true)}
                 onMouseLeave={() => setMouseOver(false)}
             >
-                <label className='input-group-text'>
-                    {!mouseOver ? 'Language' : 'Reload'}
-
-                    {language.fetching && (
-                        <span className='spinner-grow spinner-grow-sm' role='status' aria-hidden='true' />
-                    )}
-                </label>
+                <button className={styles.button}>
+                    <span className='flex-fill'>
+                        {!language.fetching ? (!mouseOver ? 'Language' : 'Reload') : 'Loading'}
+                    </span>
+                    {language.fetching && <span className='spinner-grow spinner-grow-sm' />}
+                </button>
             </div>
             <select
-                className={cn('custom-select', styles.select)}
+                className={styles.select}
                 disabled={debug.debugging}
                 defaultValue={language.languages[language.selected]}
                 onChange={event => dispatch(languageActions.select(event.target.selectedIndex))}
