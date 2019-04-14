155ea9222b1203872277f0c126f2b8eb57e3df18
Pedro Henrique <pedro00dk@gmail.com>
2019-04-14 13:30:24 -0300

    Rename Graph component to Heap

--- client/src/components/visualization/Heap.tsx
+++ client/src/components/visualization/Heap.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export function Graph() {
+export function Heap() {
     return (
         <div className='d-flex flex-row align-items-start flex-nowrap overflow-auto w-100'>
             <svg className='w-100 h-100' />
--- client/src/components/visualization/Visualization.tsx
+++ client/src/components/visualization/Visualization.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
 import { SplitPane } from '../SplitPane'
-import { Graph } from './Graph'
+import { Heap } from './Heap'
 import { Stack } from './Stack'
 
 export function Visualization() {
     return (
         <SplitPane className='d-flex position-relative' resizable split='horizontal' base='35%' left={50} right={-50}>
             <Stack />
-            <Graph />
+            <Heap />
         </SplitPane>
     )
 }
