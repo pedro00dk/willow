0480c0f6abc23d7d8de37457d066f852fcdd8a27
Pedro Henrique <pedro00dk@gmail.com>
2019-04-27 14:57:23 -0300

    Move Arrow navigation from Stack to Visualization

--- client/src/components/visualization/Visualization.tsx
+++ client/src/components/visualization/Visualization.tsx
@@ -1,13 +1,54 @@
+import cn from 'classnames'
 import * as React from 'react'
+import * as protocol from '../../protobuf/protocol'
+import { State, useDispatch, useRedux } from '../../reducers/Store'
+import { actions as tracerActions } from '../../reducers/tracer'
 import { SplitPane } from '../SplitPane'
 import { Heap } from './heap/Heap'
-import { Stack } from './stack/Stack'
+import { MemoStack } from './stack/Stack'
+
+const classes = {
+    container: cn('d-flex', 'w-100 h-100')
+}
+
+const computeNextIndex = (event: React.KeyboardEvent, tracer: State['tracer']) => {
+    if (!tracer.available || (event.key !== 'ArrowLeft' && event.key !== 'ArrowRight')) return tracer.index
+    const previousFilter = (index: number) => index < tracer.index
+    const nextFilter = (index: number) => index > tracer.index
+    const indexFilter = event.key === 'ArrowLeft' ? previousFilter : nextFilter
+
+    const currentStep = tracer.steps[tracer.index]
+    const anyFamilyFilter = (step: protocol.IStep) => true
+    const siblingParentFilter = (step: protocol.IStep) =>
+        !!step.snapshot && !!currentStep.snapshot
+            ? step.snapshot.stack.length <= currentStep.snapshot.stack.length
+            : true
+    const familyFilter = !event.ctrlKey ? anyFamilyFilter : siblingParentFilter
+
+    const resultSelector = <T extends {}>(array: T[]) =>
+        array.length > 0 ? (event.key === 'ArrowLeft' ? array[array.length - 1] : array[0]) : tracer.index
+
+    const selectedIndices = tracer.steps
+        .map((step, i) => ({ step, i }))
+        .filter(({ step, i }) => familyFilter(step) && indexFilter(i))
+        .map(({ i }) => i)
+    return resultSelector(selectedIndices)
+}
 
 export function Visualization() {
+    const dispatch = useDispatch()
+    const { tracer } = useRedux(state => ({ tracer: state.tracer, stack: state.visualization.stack }))
+
     return (
-        <SplitPane split='horizontal' base='15%' left={5} right={-5}>
-            <Stack />
-            <Heap />
-        </SplitPane>
+        <div
+            className={classes.container}
+            onKeyDown={event => dispatch(tracerActions.setIndex(computeNextIndex(event, tracer)))}
+            tabIndex={0}
+        >
+            <SplitPane split='horizontal' base='15%' left={5} right={-5}>
+                <MemoStack />
+                <Heap />
+            </SplitPane>
+        </div>
     )
 }
--- client/src/components/visualization/stack/Stack.tsx
+++ client/src/components/visualization/stack/Stack.tsx
@@ -1,43 +1,21 @@
 import cn from 'classnames'
 import * as React from 'react'
-import * as protocol from '../../../protobuf/protocol'
-import { State, useDispatch, useRedux } from '../../../reducers/Store'
-import { actions as tracerActions } from '../../../reducers/tracer'
+import { useRedux } from '../../../reducers/Store'
 import { MemoScopeNode } from './ScopeNode'
 
 const classes = {
     container: cn('d-flex flex-row align-items-start flex-nowrap', 'overflow-auto', 'w-100 h-100')
 }
 
-const computeNextIndex = (event: React.KeyboardEvent, tracer: State['tracer']) => {
-    if (!tracer.available || (event.key !== 'ArrowLeft' && event.key !== 'ArrowRight')) return tracer.index
-    const previousFilter = (index: number) => index < tracer.index
-    const nextFilter = (index: number) => index > tracer.index
-    const indexFilter = event.key === 'ArrowLeft' ? previousFilter : nextFilter
-
-    const currentStep = tracer.steps[tracer.index]
-    const anyFamilyFilter = (step: protocol.IStep) => true
-    const siblingParentFilter = (step: protocol.IStep) =>
-        !!step.snapshot && !!currentStep.snapshot
-            ? step.snapshot.stack.length <= currentStep.snapshot.stack.length
-            : true
-    const familyFilter = !event.ctrlKey ? anyFamilyFilter : siblingParentFilter
-
-    const resultSelector = <T extends {}>(array: T[]) =>
-        array.length > 0 ? (event.key === 'ArrowLeft' ? array[array.length - 1] : array[0]) : tracer.index
-
-    const selectedIndices = tracer.steps
-        .map((step, i) => ({ step, i }))
-        .filter(({ step, i }) => familyFilter(step) && indexFilter(i))
-        .map(({ i }) => i)
-    return resultSelector(selectedIndices)
-}
-
+// tslint:disable-next-line: variable-name
+export const MemoStack = React.memo(Stack)
 export function Stack() {
     const stackRef = React.useRef<HTMLDivElement>()
     const [width, setWidth] = React.useState(0)
-    const dispatch = useDispatch()
-    const { tracer, stack } = useRedux(state => ({ tracer: state.tracer, stack: state.visualization.stack }))
+    const { tracerAvailable, stack } = useRedux(state => ({
+        tracerAvailable: state.tracer.available,
+        stack: state.visualization.stack
+    }))
 
     React.useLayoutEffect(() => {
         const interval = setInterval(() => {
@@ -48,15 +26,8 @@ export function Stack() {
     }, [width])
 
     return (
-        <div
-            ref={stackRef}
-            className={classes.container}
-            onKeyDown={event => dispatch(tracerActions.setIndex(computeNextIndex(event, tracer)))}
-            tabIndex={0}
-        >
-            {tracer.available && (
-                <MemoScopeNode scope={stack.root} depth={0} width={width} />
-            )}
+        <div ref={stackRef} className={classes.container}>
+            {tracerAvailable && <MemoScopeNode scope={stack.root} depth={0} width={width} />}
         </div>
     )
 }
