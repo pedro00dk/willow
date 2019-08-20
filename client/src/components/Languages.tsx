1ceaea56009564d53c18d4063c69081bec880478
Pedro Henrique <pedro00dk@gmail.com>
2019-08-20 14:54:26 -0300

    Fix and refactor Languages component to use new reducer

--- client/src/components/Languages.tsx
+++ client/src/components/Languages.tsx
@@ -2,70 +2,71 @@ import cn from 'classnames'
 import { css } from 'emotion'
 import * as React from 'react'
 import { colors } from '../colors'
-import { actions as languageActions } from '../reducers/language'
+import { actions as programActions } from '../reducers/program'
 import { useDispatch, useRedux } from '../reducers/Store'
 
 const classes = {
-    container: cn('d-flex', 'input-group', 'w-auto', 'ml-3'),
-    reloader: {
-        container: cn('input-group-prepend'),
-        label: cn('flex-fill'),
-        spin: cn('spinner-grow spinner-grow-sm'),
-        button: cn(
-            'd-inline-flex align-items-center',
-            'btn',
-            'p-1',
-            css({
-                background: colors.gray.light,
-                border: `1px solid ${colors.gray.dark}`,
-                color: colors.black,
-                fontSize: '1rem',
-                width: '7rem',
-                ':hover': {
-                    background: colors.gray.main
-                }
-            })
-        )
-    },
+    container: cn('d-flex', 'input-group', 'w-auto ml-3'),
+    reloader: cn('input-group-prepend'),
+    label: cn('flex-fill'),
+    spin: cn('spinner-grow spinner-grow-sm'),
+    button: cn(
+        'd-inline-flex align-items-center',
+        'btn',
+        'p-1',
+        css({
+            background: colors.gray.light,
+            border: `1px solid ${colors.gray.dark}`,
+            color: colors.black,
+            fontSize: '1rem',
+            width: '7rem',
+            ':hover': { background: colors.gray.main }
+        })
+    ),
     select: cn('custom-select', css({ flex: '0 1 auto !important', color: colors.black, width: '6rem !important' }))
 }
 
 export function Languages() {
     const [mouseOver, setMouseOver] = React.useState(false)
     const dispatch = useDispatch()
-    const { language, fetching } = useRedux(state => ({ language: state.language, fetching: state.tracer.fetching }))
+    const { language, languagesSet, programFetching, tracerFetching } = useRedux(state => ({
+        language: state.program.language,
+        languagesSet: state.program.languages,
+        programFetching: state.program.fetching,
+        tracerFetching: state.tracer.fetching
+    }))
+
+    const languages = [...languagesSet]
 
     React.useEffect(() => {
-        dispatch(languageActions.fetch())
+        dispatch(programActions.fetchLanguages())
     }, [])
 
     return (
         <div className={classes.container}>
             <div
-                className={classes.reloader.container}
-                onClick={() => dispatch(languageActions.fetch())}
+                className={classes.reloader}
+                onClick={() => dispatch(programActions.fetchLanguages())}
                 onMouseEnter={() => setMouseOver(true)}
                 onMouseLeave={() => setMouseOver(false)}
             >
-                <button className={classes.reloader.button}>
-                    <span className={classes.reloader.label}>
-                        {!language.fetching ? (!mouseOver ? 'Language' : 'Reload') : 'Loading'}
+                <button className={classes.button}>
+                    <span className={classes.label}>
+                        {!programFetching ? (!mouseOver ? 'Language' : 'Reload') : 'Loading'}
                     </span>
-                    {language.fetching && <span className={classes.reloader.spin} />}
+                    {programFetching && <span className={classes.spin} />}
                 </button>
             </div>
             <select
                 className={classes.select}
-                disabled={fetching}
-                defaultValue={language.languages[language.selected]}
-                onChange={event => dispatch(languageActions.select(event.target.selectedIndex))}
+                disabled={tracerFetching}
+                defaultValue={language}
+                onChange={event => dispatch(programActions.selectLanguage(languages[event.target.selectedIndex]))}
             >
-                {language.languages.map((language, i) => (
+                {languages.length === 0 && <option key={-1} value='text' label={programFetching ? '...' : '!'} />}
+                {languages.map((language, i) => (
                     <option key={i} value={language} label={language} />
                 ))}
-                {language.languages.length === 0 && (
-                    <option key={-1} value='text' label={language.fetching ? '...' : '!'} />
-                )}
             </select>
         </div>
     )
