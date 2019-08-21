28db7ebb63d90d794a4338f163340013d89bdaee
Pedro Henrique <pedro00dk@gmail.com>
2019-08-21 15:39:38 -0300

    Update stack and scope to use div again
    SVG implementation did not bring enough performance compared to div

--- client/src/components/visualization/stack/Scope.tsx
+++ client/src/components/visualization/stack/Scope.tsx
@@ -1,98 +1,90 @@
+import cn from 'classnames'
+import { css } from 'emotion'
 import * as React from 'react'
 import { colors } from '../../../colors'
 import { useDispatch, useRedux } from '../../../reducers/Store'
 import { actions as tracerActions } from '../../../reducers/tracer'
-import { ScopeNode } from '../../../reducers/tracer'
+import { ScopeData } from '../../../reducers/tracer'
 
-const styles = {
-    rectLayout: (x: number, y: number, width: number, height: number, padding: number) => ({
-        x: x + padding,
-        y: y + padding,
-        width: width - padding * 2,
-        height: height - padding * 2
-    }),
-    textLayout: (x: number, y: number, width: number, height: number, padding: number) => ({
-        ...styles.rectLayout(x, y, width, height, padding),
-        y: y + padding + height * 0.7,
-        fontSize: height * 0.8
-    }),
-    triangleLayout: (x: number, y: number, width: number, height: number, padding: number) => ({
-        points: `${x + padding},${y + padding}
-            ${x + width / 2},${y + padding + height / 2}
-            ${x + width - padding},${y + padding}`
-    }),
-
-    color: (width: number, isLeaf: boolean) =>
-        isLeaf ? colors.blue.lighter : width >= 40 ? colors.blue.main : colors.blue.light
+const classes = {
+    container: cn('d-flex flex-column', 'w-100'),
+    scope: cn(
+        'text-truncate',
+        'w-100',
+        css({
+            fontSize: '1rem',
+            backgroundClip: 'content-box !important',
+            cursor: 'default',
+            ':hover': { borderColor: `${colors.gray.dark} !important` }
+        })
+    ),
+    children: cn('d-flex'),
+    child: cn('d-flex')
 }
 
-const computeChildWidth = (parent: ScopeNode, child: ScopeNode, width: number) =>
-    ((child.range[1] - child.range[0] + 1) / (parent.range[1] - parent.range[0] + 1)) * width
+const scopeColors = (depth: number) => {
+    return depth <= 0
+        ? colors.blue.main
+        : depth <= 2
+        ? colors.blue.light
+        : depth <= 4
+        ? colors.blue.lighter
+        : depth <= 6
+        ? colors.yellow.light
+        : depth <= 8
+        ? colors.yellow.lighter
+        : depth <= 10
+        ? colors.red.light
+        : colors.red.lighter
+}
 
-export const Scope = (props: {
-    scope: ScopeNode
-    depth: number
-    x: number
-    y: number
-    width: number
-    height: number
-}) => {
-    const rectRef = React.useRef<SVGRectElement>()
-    const dispatch = useDispatch()
-    useRedux(async state => {
-        if (!rectRef.current) return
-        const selected = state.tracer.index >= props.scope.range[0] && state.tracer.index <= props.scope.range[1]
-        const stroke = rectRef.current.getAttribute('stroke')
-        if (selected && stroke === 'white') rectRef.current.setAttribute('stroke', colors.gray.dark)
-        else if (!selected && stroke !== 'white') rectRef.current.setAttribute('stroke', 'white')
+const styles = {
+    scope: (depth: number, width: number, selected: boolean, isLeaf: boolean) => ({
+        background: scopeColors(depth),
+        opacity: width >= 20 ? 1 : 0.5,
+        border: `1px ${selected ? colors.gray.main : 'transparent'} solid`,
+        height: !isLeaf ? 'auto' : '1rem'
     })
+}
 
-    const isRoot = props.scope.name == undefined && !!props.scope.children
-    const isIntermediary = !isRoot && !!props.scope.children
-    const isLeaf = !props.scope.children
+const computeChildWidth = (parent: ScopeData, child: ScopeData, width: number) => {
+    const proportion = (child.range[1] - child.range[0] + 1) / (parent.range[1] - parent.range[0] + 1)
+    return { width: proportion * width, percent: `${proportion * 100}%` }
+}
 
-    const clipPathId = props.scope.range.toString()
-    const shift = { value: props.x }
-    const rectLayout = styles.rectLayout(props.x, props.y, props.width, props.height, 0.5)
-    const textLayout = styles.textLayout(props.x, props.y, props.width, props.height, 2)
-    const trianglePoints = styles.triangleLayout(props.x, props.y, props.width, props.height, 0)
+export const Scope = (props: { scope: ScopeData; depth: number; width: number }) => {
+    const dispatch = useDispatch()
+    const { selected } = useRedux(state => ({
+        selected: state.tracer.index >= props.scope.range[0] && state.tracer.index <= props.scope.range[1]
+    }))
+
+    const root = props.scope.name == undefined && !!props.scope.children
+    const leaf = props.scope.name == undefined && !props.scope.children
 
     return (
-        <>
-            {!isRoot && (
-                <g onClick={() => dispatch(tracerActions.setIndex(props.scope.range[0]))}>
-                    {isIntermediary && (
-                        <>
-                            <rect ref={rectRef} {...rectLayout} fill={styles.color(props.width, isLeaf)} stroke='white'>
-                                <title>{props.scope.name}</title>
-                            </rect>
-                            <text {...textLayout} fontFamily='monospace'>
-                                {props.width >= 40 && props.scope.name}
-                            </text>
-                        </>
-                    )}
-                    {isLeaf && <polygon {...trianglePoints} fill={styles.color(props.width, isLeaf)} />}
-                </g>
+        <div className={classes.container}>
+            {!root && (
+                <div
+                    className={classes.scope}
+                    style={styles.scope(props.depth - Number(leaf), props.width, selected, leaf)}
+                    title={props.scope.name}
+                    onClick={event => dispatch(tracerActions.setIndex(props.scope.range[event.ctrlKey ? 1 : 0]))}
+                >
+                    {!leaf && props.width >= 20 ? props.scope.name : '\u200b'}
+                </div>
             )}
-            {(isRoot || isIntermediary) &&
-                props.width >= 10 &&
-                props.scope.children.map((child, i) => {
-                    const childWidth = computeChildWidth(props.scope, child, props.width)
-                    const childX = shift.value
-                    const childY = (props.depth + 1) * props.height
-                    shift.value += childWidth
-                    return (
-                        <Scope
-                            key={i}
-                            scope={child}
-                            depth={props.depth + 1}
-                            x={childX}
-                            y={childY}
-                            width={childWidth}
-                            height={props.height}
-                        />
-                    )
-                })}
-        </>
+            {!leaf && props.width >= 10 && (
+                <div className={classes.children}>
+                    {props.scope.children.map((child, i) => {
+                        const { width, percent } = computeChildWidth(props.scope, child, props.width)
+                        return (
+                            <div key={i} className={classes.child} style={{ width: percent }}>
+                                <Scope scope={child} depth={props.depth + 1} width={width} />
+                            </div>
+                        )
+                    })}
+                </div>
+            )}
+        </div>
     )
 }
--- client/src/components/visualization/stack/Stack.tsx
+++ client/src/components/visualization/stack/Stack.tsx
@@ -3,33 +3,25 @@ import * as React from 'react'
 import { useRedux } from '../../../reducers/Store'
 import { Scope } from './Scope'
 
-const classes = {
-    container: cn('d-flex', 'overflow-auto', 'h-100 w-100')
-}
+const classes = { container: cn('d-flex align-items-start flex-nowrap', 'overflow-auto', 'w-100 h-100') }
 
-export const Stack = React.memo(() => {
-    const containerRef = React.useRef<HTMLDivElement>()
+export const Stack = () => {
+    const ref = React.useRef<HTMLDivElement>()
     const [width, setWidth] = React.useState(0)
     const { available, stack } = useRedux(state => ({ available: state.tracer.available, stack: state.tracer.stack }))
-    const scopeHeight = 20
-    const height = stack ? scopeHeight * stack.depth : 0
 
     React.useEffect(() => {
         const interval = setInterval(() => {
-            if (containerRef.current.clientWidth === width) return
-            setWidth(containerRef.current.clientWidth)
+            if (ref.current.clientWidth === width) return
+            setWidth(ref.current.clientWidth)
         }, 1000)
 
         return () => clearInterval(interval)
-    }, [containerRef, width])
+    }, [ref, width])
 
     return (
-        <div ref={containerRef} className={classes.container}>
-            {available && (
-                <svg width='100%' height={`${height}px`}>
-                    <Scope scope={stack.root} depth={-1} x={0} y={0} width={width} height={scopeHeight} />
-                </svg>
-            )}
+        <div ref={ref} className={classes.container}>
+            {available && <Scope scope={stack.root} depth={-1} width={width} />}
         </div>
     )
-})
+}
