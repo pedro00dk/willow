1032e06a9fc4d79ec50de9077c89da48ddb52255
Pedro Henrique <pedro00dk@gmail.com>
2019-09-01 14:22:21 -0300

    Move Utils to the root src and make more use of it

--- client/src/Utils.tsx
+++ client/src/Utils.tsx
@@ -1,8 +1,33 @@
 import * as React from 'react'
 
+export const clamp = (value: number, min: number, max: number) => Math.min(Math.max(value, min), max)
+export const lerp = (ratio: number, from: number, to: number) => from * (1 - ratio) + to * ratio
+export const ilerp = (value: number, from: number, to: number) => (value - from) / (to - from)
+
+export const svgScreenPointTransform = (
+    direction: 'toSvg' | 'toScreen',
+    svgElement: SVGGraphicsElement,
+    ...points: { x: number; y: number }[]
+) => {
+    const svgToScreenTransformMatrix = svgElement.getScreenCTM()
+    const transformMatrix = direction === 'toSvg' ? svgToScreenTransformMatrix.inverse() : svgToScreenTransformMatrix
+    return points.map(point => {
+        const transformedDomPoint = new DOMPoint(point.x, point.y).matrixTransform(transformMatrix)
+        return { x: transformedDomPoint.x, y: transformedDomPoint.y }
+    })
+}
+
+export const svgScreenVectorTransform = (
+    direction: 'toSvg' | 'toScreen',
+    svgElement: SVGGraphicsElement,
+    ...vectors: { x: number; y: number }[]
+) => {
+    const [root, ...shiftedVectors] = svgScreenPointTransform(direction, svgElement, { x: 0, y: 0 }, ...vectors)
+    return shiftedVectors.map(shiftedVector => ({ x: shiftedVector.x - root.x, y: shiftedVector.y - root.y }))
+}
+
 export const Draggable = (props: {
     containerProps: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-    showGhost: boolean
     onDrag: (delta: { x: number; y: number }, event?: React.DragEvent) => void
     children?: React.ReactNode
 }) => {
@@ -14,7 +39,6 @@ export const Draggable = (props: {
             draggable
             onDragStart={event => {
                 anchor.current = { x: event.clientX, y: event.clientY }
-                if (props.showGhost) return
                 const emptyGhostImage = new Image()
                 emptyGhostImage.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs='
                 event.dataTransfer.setDragImage(emptyGhostImage, 0, 0)
