8aeccaa033dc91e702432df0bb41d76f00909371
Pedro Henrique <pedro00dk@gmail.com>
2019-09-01 13:08:19 -0300

    Move computeNextIndex function to inside the Visualization component

--- client/src/components/visualization/Visualization.tsx
+++ client/src/components/visualization/Visualization.tsx
@@ -11,29 +11,29 @@ const classes = {
     container: cn('d-flex', 'w-100 h-100')
 }
 
-const computeNextIndex = (event: React.KeyboardEvent, tracer: State['tracer']) => {
-    if (!tracer.available || (event.key !== 'ArrowLeft' && event.key !== 'ArrowRight')) return tracer.index
-    const step = tracer.steps[tracer.index]
-
-    const indexFilter = (index: number) => (event.key === 'ArrowLeft' ? index < tracer.index : index > tracer.index)
-    const anyFamilyFilter = (step: schema.Step) => true
-    const siblingParentFilter = (step: schema.Step) =>
-        step.snapshot && step.snapshot ? step.snapshot.stack.length <= step.snapshot.stack.length : true
-    const familyFilter = !event.ctrlKey ? anyFamilyFilter : siblingParentFilter
-
-    const resultSelector = <T extends {}>(array: T[]) =>
-        array.length > 0 ? (event.key === 'ArrowLeft' ? array[array.length - 1] : array[0]) : tracer.index
-
-    const selectedIndices = tracer.steps
-        .map((step, i) => ({ step, i }))
-        .filter(({ step, i }) => familyFilter(step) && indexFilter(i))
-        .map(({ i }) => i)
-    return resultSelector(selectedIndices)
-}
-
 export const Visualization = () => {
     const dispatch = useDispatch()
     const { tracer } = useRedux(state => ({ tracer: state.tracer }))
+
+    const computeNextIndex = (event: React.KeyboardEvent, tracer: State['tracer']) => {
+        if (!tracer.available || (event.key !== 'ArrowLeft' && event.key !== 'ArrowRight')) return tracer.index
+        const step = tracer.steps[tracer.index]
+
+        const indexFilter = (index: number) => (event.key === 'ArrowLeft' ? index < tracer.index : index > tracer.index)
+        const anyFamilyFilter = (step: schema.Step) => true
+        const siblingParentFilter = (step: schema.Step) =>
+            step.snapshot && step.snapshot ? step.snapshot.stack.length <= step.snapshot.stack.length : true
+        const familyFilter = !event.ctrlKey ? anyFamilyFilter : siblingParentFilter
+
+        const resultSelector = <T extends {}>(array: T[]) =>
+            array.length > 0 ? (event.key === 'ArrowLeft' ? array[array.length - 1] : array[0]) : tracer.index
+
+        const selectedIndices = tracer.steps
+            .map((step, i) => ({ step, i }))
+            .filter(({ step, i }) => familyFilter(step) && indexFilter(i))
+            .map(({ i }) => i)
+        return resultSelector(selectedIndices)
+    }
 
     return (
         <div
