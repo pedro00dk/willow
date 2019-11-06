c76f0498ce2539c9ceb961e40035456a6bfe288b
Pedro Henrique <pedro00dk@gmail.com>
2019-11-05 23:16:19 -0300

    Remove no more necessary babel import after update typescript target

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,5 @@
-// required for parcel to generate target > ES5 builds
-import '@babel/polyfill'
-
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
-import { App } from './App'
-import { Store } from './reducers/Store'
+import { App } from './components/App'
 
-ReactDom.render(
-    <Store>
-        <App />
-    </Store>,
-    document.getElementById('root')
-)
+ReactDom.render(<App />, document.getElementById('root'))
