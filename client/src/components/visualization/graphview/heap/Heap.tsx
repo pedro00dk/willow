498eddaed4708354b76059c133e6e43e57855b28
Pedro Henrique <pedro00dk@gmail.com>
2019-11-07 14:30:52 -0300

    Create new component to manage only heap objects

---
+++ client/src/components/visualization/graphview/heap/Heap.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { DefaultState } from '../../../../reducers/Store'
+import { GraphController } from '../GraphController'
+import { Wrapper } from '../Wrapper'
+
+export const Heap = (props: {
+    controller: GraphController
+    updateGraph: React.Dispatch<{}>
+    tracer: DefaultState['tracer']
+}) =>
+    Object.values(props.tracer.heapsData[props.tracer.index]).map(objData => (
+        <Wrapper
+            key={objData.id}
+            objData={objData}
+            controller={props.controller}
+            updateGraph={props.updateGraph}
+            tracer={props.tracer}
+        />
+    ))
