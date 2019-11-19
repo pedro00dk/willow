7ce5f38f7f794d28a6a5d1d0be79ec2456cc47ca
Pedro Henrique <pedro00dk@gmail.com>
2019-11-19 14:50:24 -0300

    Extract functions and use a single layout in SvgWrapper

--- client/src/components/visualization/graphview/SvgWrapper.tsx
+++ client/src/components/visualization/graphview/SvgWrapper.tsx
@@ -18,58 +18,55 @@ export const SvgWrapper = (props: {
     const ref = React.useRef<SVGForeignObjectElement>()
     const pathsRef = React.useRef<SVGForeignObjectElement>()
 
+    const lerp = (from: number, to: number, gradient: number) => from * (1 - gradient) + to * gradient
+
+    const computePath = (
+        sourcePosition: { x: number; y: number },
+        delta: { x: number; y: number },
+        targetPosition: { x: number; y: number },
+        targetSize: { x: number; y: number }
+    ) => {
+        const source = { x: sourcePosition.x + delta.x, y: sourcePosition.y + delta.y }
+        const target = {
+            x: Math.min(Math.max(source.x, targetPosition.x), targetPosition.x + targetSize.x),
+            y: Math.min(Math.max(source.y, targetPosition.y), targetPosition.y + targetSize.y)
+        }
+        const center = { x: lerp(source.x, target.x, 0.5), y: lerp(source.y, target.y, 0.5) }
+        const parallelVector = { x: target.x - source.x, y: target.y - source.y }
+        const orthogonalVector = { x: parallelVector.y / 4, y: -parallelVector.x / 4 }
+        const control = { x: center.x + orthogonalVector.x, y: center.y + orthogonalVector.y }
+        return `M ${source.x},${source.y} Q ${control.x},${control.y} ${target.x},${target.y}`
+    }
+
+    const updateRect = (subscriptionCall?: number) => {
+        const position = props.controller.getPosition(props.id, props.controller.getIndex(), { x: 0, y: 0 })
+        const size = props.controller.getSize(props.id, props.controller.getIndex(), { x: 0, y: 0 })
+        ref.current.setAttribute('x', position.x.toString())
+        ref.current.setAttribute('y', position.y.toString())
+        ref.current.setAttribute('width', size.x.toString())
+        ref.current.setAttribute('height', size.y.toString())
+    }
+
+    const updatePaths = (subscriptionCall?: number) => {
+        const targets = props.controller.getTargets(props.id)
+        const paths = pathsRef.current.children
+        targets.forEach(({ target, delta }, i) => {
+            if (i >= paths.length) return
+            const sourcePosition = props.controller.getPosition(props.id, props.controller.getIndex())
+            const targetPosition = props.controller.getPosition(target, props.controller.getIndex())
+            const targetSize = props.controller.getSize(target, props.controller.getIndex())
+            if (!targetPosition) return
+            const pathD = computePath(sourcePosition, delta, targetPosition, targetSize)
+            paths[i].setAttribute('d', pathD)
+            paths[i].setAttribute('class', classes.path)
+            paths[i].setAttribute('marker-end', 'url(#pointer)')
+        })
+    }
+
     React.useEffect(() => {
-        const updateRect = (subscriptionCall?: number) => {
-            const position = props.controller.getPosition(props.id, props.controller.getIndex(), { x: 0, y: 0 })
-            const size = props.controller.getSize(props.id, props.controller.getIndex(), { x: 0, y: 0 })
-            ref.current.setAttribute('x', position.x.toString())
-            ref.current.setAttribute('y', position.y.toString())
-            ref.current.setAttribute('width', size.x.toString())
-            ref.current.setAttribute('height', size.y.toString())
-        }
-
         updateRect()
-        props.controller.subscribe(props.id, updateRect)
-    })
-
-    React.useLayoutEffect(() => {
-        const lerp = (from: number, to: number, gradient: number) => from * (1 - gradient) + to * gradient
-
-        const computePathD = (
-            sourcePosition: { x: number; y: number },
-            delta: { x: number; y: number },
-            targetPosition: { x: number; y: number },
-            targetSize: { x: number; y: number }
-        ) => {
-            const source = { x: sourcePosition.x + delta.x, y: sourcePosition.y + delta.y }
-            const target = {
-                x: Math.min(Math.max(source.x, targetPosition.x), targetPosition.x + targetSize.x),
-                y: Math.min(Math.max(source.y, targetPosition.y), targetPosition.y + targetSize.y)
-            }
-            const center = { x: lerp(source.x, target.x, 0.5), y: lerp(source.y, target.y, 0.5) }
-            const parallelVector = { x: target.x - source.x, y: target.y - source.y }
-            const orthogonalVector = { x: parallelVector.y / 4, y: -parallelVector.x / 4 }
-            const control = { x: center.x + orthogonalVector.x, y: center.y + orthogonalVector.y }
-            return `M ${source.x},${source.y} Q ${control.x},${control.y} ${target.x},${target.y}`
-        }
-
-        const updatePaths = (subscriptionCall?: number) => {
-            const targets = props.controller.getTargets(props.id)
-            const paths = pathsRef.current.children
-            targets.forEach(({ target, delta }, i) => {
-                if (i >= paths.length) return
-                const sourcePosition = props.controller.getPosition(props.id, props.controller.getIndex())
-                const targetPosition = props.controller.getPosition(target, props.controller.getIndex())
-                const targetSize = props.controller.getSize(target, props.controller.getIndex())
-                if (!targetPosition) return
-                const pathD = computePathD(sourcePosition, delta, targetPosition, targetSize)
-                paths[i].setAttribute('d', pathD)
-                paths[i].setAttribute('class', classes.path)
-                paths[i].setAttribute('marker-end', 'url(#pointer)')
-            })
-        }
-
         updatePaths()
+        props.controller.subscribe(props.id, updateRect)
         props.controller.subscribe(props.id, updatePaths)
         props.controller.getTargets(props.id).forEach(({ target }) => props.controller.subscribe(target, updatePaths))
     })
