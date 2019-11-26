98f6009c297b2cfc14d2854a85dfd42de1fabf38
Pedro Henrique <pedro00dk@gmail.com>
2019-11-26 20:52:46 -0300

    Smal refactoring in client's serverApi

--- client/src/server.ts
+++ client/src/server.ts
@@ -3,6 +3,6 @@ import * as axios from 'axios'
 const apiServerAddress = process.env['SERVER']
 const bundleServerIsReverseProxy = process.env['PROXY']
 
-const baseURL = bundleServerIsReverseProxy == undefined ? apiServerAddress : ''
+const baseURL = !bundleServerIsReverseProxy ? apiServerAddress : ''
 
 export const serverApi = axios.default.create({ baseURL, withCredentials: true })
