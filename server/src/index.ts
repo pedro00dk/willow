280f7dfe757ec6c7b6715b61356983dcf869445d
Pedro Henrique <pedro00dk@gmail.com>
2019-11-26 20:38:02 -0300

    Ass better CLI log in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,7 @@ const main = () => {
     const tracers = [...Array(options.tracer ? options.tracer.length / 2 : 0)]
         .map((_, i) => [options.tracer[i * 2], options.tracer[i * 2 + 1]] as const)
         .reduce((acc, [language, command]) => ({ ...acc, [language]: command }), {} as { [language: string]: string })
-    log.info(main.name, 'cli', { port, tracers })
+    log.info(main.name, 'cli', { ...options, tracers })
     new Server(tracers, port, steps, timeout, debug, clients).listen()
 }
 
