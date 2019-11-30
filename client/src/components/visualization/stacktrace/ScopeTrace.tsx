899cb526152d1b9bb6c0611cd8a50bcaa0697a19
Pedro Henrique <pedro00dk@gmail.com>
2019-11-30 17:22:06 -0300

    Memoize the stack data computation in the StackTrace component

--- client/src/components/visualization/stacktrace/ScopeTrace.tsx
+++ client/src/components/visualization/stacktrace/ScopeTrace.tsx
@@ -5,15 +5,14 @@ import { colors } from '../../../colors'
 import { useDispatch, useSelection } from '../../../reducers/Store'
 import { actions as tracerActions } from '../../../reducers/tracer'
 import { ScopeData } from './StackTrace'
-import * as schema from '../../../schema/schema'
 
 const classes = {
     container: 'd-flex flex-column w-100',
     scope: cn(
-        'text-truncate',
-        'w-100',
+        'text-truncate w-100',
         css({
             fontSize: '1rem',
+            background: colors.blue.light,
             backgroundClip: 'content-box !important',
             cursor: 'default',
             ':hover': { borderColor: `${colors.gray.dark} !important` }
@@ -23,46 +22,29 @@ const classes = {
     child: 'd-flex'
 }
 
-const scopeColors = (depth: number) => {
-    return depth <= 0
-        ? colors.blue.main
-        : depth <= 2
-        ? colors.blue.light
-        : depth <= 4
-        ? colors.blue.lighter
-        : depth <= 6
-        ? colors.yellow.light
-        : depth <= 8
-        ? colors.yellow.lighter
-        : depth <= 10
-        ? colors.red.light
-        : colors.red.lighter
-}
-
 const styles = {
-    scope: (depth: number, width: number, selected: boolean) => ({
-        background: scopeColors(depth),
+    scope: (width: number, selected: boolean) => ({
         opacity: width >= 20 ? 1 : 0.5,
         border: `1px ${selected ? colors.gray.main : 'transparent'} solid`
     })
 }
 
-export const ScopeTrace = (props: { scope: ScopeData; depth: number; width: number }) => {
-    if (props.depth === 0) console.log(props.width)
+export const ScopeTrace = (props: { scope: ScopeData; width: number }) => {
     const dispatch = useDispatch()
     const { selected } = useSelection(state => ({
-        selected: state.tracer.index >= props.scope.range[0] && state.tracer.index <= props.scope.range[1]
+        selected: state.tracer.index >= props.scope.start && state.tracer.index < props.scope.start + props.scope.size
     }))
-    const root = props.scope.name == undefined && !!props.scope.children
 
     return (
         <div className={classes.container}>
-            {!root && (
+            {props.scope.name == undefined && (
                 <div
                     className={classes.scope}
-                    style={styles.scope(props.depth, props.width, selected)}
+                    style={styles.scope(props.width, selected)}
                     title={props.scope.name}
-                    onClick={event => dispatch(tracerActions.setIndex(props.scope.range[!event.altKey ? 0 : 1]))}
+                    onClick={event =>
+                        dispatch(tracerActions.setIndex(props.scope.start + (event.altKey && props.scope.size - 1)))
+                    }
                 >
                     {props.width >= 20 ? props.scope.name : '\u200b'}
                 </div>
@@ -70,14 +52,22 @@ export const ScopeTrace = (props: { scope: ScopeData; depth: number; width: numb
             {props.width >= 10 && (
                 <div className={classes.children}>
                     {props.scope.children.map((child, i) => {
-                        const proportion =
-                            (child.range[1] - child.range[0] + 1) / (props.scope.range[1] - props.scope.range[0] + 1)
-                        const pixels = proportion * props.width
-                        const percent = `${proportion * 100}%`
+                        const childWidthProportion = child.size / props.scope.size
+                        const childWidthPercent = `${childWidthProportion * 100}%`
+                        const childWidthPixels = childWidthProportion * props.width
+                        const previousChild = props.scope.children[i - 1]
+                        const childMarginSize =
+                            child.start - ((previousChild?.start ?? 0) + (previousChild?.size ?? 0)) - props.scope.start
+                        const childMarginProportion = childMarginSize / props.scope.size
+                        const childMarginPercent = `${childMarginProportion * 100}%`
 
                         return (
-                            <div key={i} className={classes.child} style={{ width: percent }}>
-                                <ScopeTrace scope={child} depth={props.depth + 1} width={pixels} />
+                            <div
+                                key={i}
+                                className={classes.child}
+                                style={{ width: childWidthPercent, marginLeft: childMarginPercent }}
+                            >
+                                <ScopeTrace scope={child} width={childWidthPixels} />
                             </div>
                         )
                     })}
--- client/src/components/visualization/stacktrace/StackTrace.tsx
+++ client/src/components/visualization/stacktrace/StackTrace.tsx
@@ -4,53 +4,56 @@ import React from 'react'
 import { useSelection } from '../../../reducers/Store'
 import { ScopeTrace } from './ScopeTrace'
 
-export type ScopeData = { name: string; children: ScopeData[]; range: [number, number] }
+export type ScopeData = { name: string; children: ScopeData[]; start: number; size: number; threw: boolean }
 
 const classes = {
-    container: cn('d-flex align-items-start flex-nowrap', 'overflow-auto', 'w-100 h-100', css({ userSelect: 'none' }))
+    container: cn('d-flex align-items-start flex-nowrap overflow-auto w-100 h-100', css({ userSelect: 'none' }))
 }
 
 export const StackTrace = React.memo(() => {
     const ref = React.useRef<HTMLDivElement>()
     const [width, setWidth] = React.useState(0)
-    const { tracer } = useSelection(state => ({ tracer: state.tracer }))
+    const { steps } = useSelection(state => ({ steps: state.tracer.steps }))
 
-    const stackRootData = React.useMemo(() => {
-        if (!tracer.available) return
-        const stackRootData: ScopeData = { name: undefined, children: [], range: [0, 0] }
-        const scopeDataPath: ScopeData[] = []
-        tracer.steps.forEach(({ snapshot, threw }, i) => {
+    const stackData = React.useMemo(() => {
+        if (!steps) return
+        const rootData: ScopeData = { name: undefined, children: [], start: 0, size: steps.length, threw: false }
+        const scopePath: ScopeData[] = []
+        scopePath[-1] = rootData
+        steps.forEach(({ snapshot, threw }, i) => {
             if (threw) {
-                const name = threw.exception?.type ?? threw.cause
-                const threwScopeData: ScopeData = { name, range: [i, i], children: [] }
-                stackRootData.children.push(threwScopeData)
-                scopeDataPath.splice(0, Infinity, threwScopeData)
+                scopePath.splice(0)
+                const name = threw.cause ?? threw.exception.type
+                const threwData: ScopeData = { name, children: [], start: i, size: 1, threw: true }
+                const parentScopeData = scopePath[scopePath.length - 1]
+                parentScopeData.children.push(threwData)
+                scopePath.push(threwData)
                 return
             }
-            ;[...Array(Math.max(snapshot.stack.length, scopeDataPath.length)).keys()].forEach(j => {
+            const biggerScope = Math.max(snapshot.stack.length, scopePath.length)
+            ;[...Array(biggerScope).keys()].forEach(j => {
                 const scope = snapshot.stack[j]
-                const scopeData = scopeDataPath[j]
+                const scopeData = scopePath[j]
                 if (!scope && !scopeData) return
                 else if (!scopeData) {
-                    const newScopeData: ScopeData = { name: scope.name, range: [i, i], children: [] }
-                    const parentScopeData = scopeDataPath[j - 1]
-                    ;(parentScopeData?.children ?? stackRootData.children).push(newScopeData)
-                    scopeDataPath.push(newScopeData)
-                    scopeDataPath.forEach(scopeData => (scopeData.range[1] = i))
+                    const newScopeData: ScopeData = { name: scope.name, start: i, size: 0, children: [], threw: false }
+                    const parentScopeData = scopePath[j - 1]
+                    parentScopeData.children.push(newScopeData)
+                    scopePath.push(newScopeData)
                 } else if (!scope) {
-                    scopeDataPath.splice(j)
+                    scopePath.splice(j)
                 } else if (scope.name !== scopeData.name) {
-                    const newScopeData: ScopeData = { name, range: [i, i], children: [] }
-                    const parentScopeData = scopeDataPath[j - 1]
-                    ;(parentScopeData?.children ?? stackRootData.children).push(newScopeData)
-                    scopeDataPath.pop()
-                    scopeDataPath.push(newScopeData)
-                    scopeDataPath.forEach(scopeData => (scopeData.range[1] = i))
+                    const newScopeData: ScopeData = { name: scope.name, start: i, size: 0, children: [], threw: false }
+                    const parentScopeData = scopePath[j - 1]
+                    parentScopeData.children.push(newScopeData)
+                    scopePath.pop()
+                    scopePath.push(newScopeData)
                 }
             })
+            scopePath.forEach(scopeData => scopeData.size++)
         })
-        return stackRootData
-    }, [tracer.available])
+        return rootData
+    }, [steps])
 
     React.useEffect(() => {
         const interval = setInterval(() => {
@@ -63,7 +66,7 @@ export const StackTrace = React.memo(() => {
 
     return (
         <div ref={ref} className={classes.container}>
-            {tracer.available && <ScopeTrace scope={stackRootData} depth={-1} width={width} />}
+            {steps && <ScopeTrace scope={stackData} width={width} />}
         </div>
     )
 })
