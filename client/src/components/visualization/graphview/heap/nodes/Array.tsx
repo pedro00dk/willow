6341524234d86be0e5c0acf4c07769d94049fe8e
Pedro Henrique <pedro00dk@gmail.com>
2019-12-03 18:11:55 -0300

    Implement wrap parameters in array and use schema types

--- client/src/components/visualization/graphview/heap/nodes/Array.tsx
+++ client/src/components/visualization/graphview/heap/nodes/Array.tsx
@@ -2,20 +2,17 @@ import cn from 'classnames'
 import { css } from 'emotion'
 import * as React from 'react'
 import { colors } from '../../../../../colors'
-import { ObjData } from '../../../../../reducers/tracer'
 import * as schema from '../../../../../schema/schema'
 import { Base, getDisplayValue, valueChanged } from '../../Base'
-import { ComputedParameters, readParameters, UnknownParameters } from '../../GraphController'
-import { Parameters } from '../Parameters'
+import { readParameters, UnknownParameters } from '../../GraphData'
 
 const classes = {
-    container: 'd-flex align-items-center text-nowrap',
+    container: 'd-flex text-nowrap',
     element: cn(
-        'd-inline-flex flex-column',
-        'px-1',
+        'd-inline-flex px-1',
         css({ border: `0.5px solid ${colors.gray.dark}`, cursor: 'default', fontSize: '1rem' })
     ),
-    index: cn('text-truncate', css({ fontSize: '0.5rem' })),
+    index: cn('text-truncate mr-1', css({ fontSize: '0.5rem' })),
     value: cn('text-center text-truncate', css({ fontSize: '0.75rem' }))
 }
 
@@ -23,74 +20,84 @@ const styles = {
     background: (changed: boolean) => (changed ? colors.red.light : colors.blue.light)
 }
 
-const defaultParameters = {
-    'show index': { value: true },
-    'max width': { value: 30, range: [5, 100] as [number, number] }
+export const defaultParameters = {
+    index: { value: true },
+    width: { value: 30, range: [5, 100] as [number, number] },
+    direction: { value: 'row', options: ['row', 'column'] },
+    wrap: { value: 'Infinity', options: [...[...Array(100).keys()].map(i => (i + 1).toString()), Infinity.toString()] },
+    'wrap index': { value: false }
 }
 
-export const defaults: ReadonlySet<schema.Obj['type']> = new Set(['array', 'alist', 'tuple', 'set'])
-export const supported: ReadonlySet<schema.Obj['type']> = new Set(['array', 'alist', 'llist', 'tuple', 'set'])
+export const defaults: ReadonlySet<schema.Obj['gType']> = new Set(['array', 'linked', 'set'])
+export const supported: ReadonlySet<schema.Obj['gType']> = new Set(['array', 'linked', 'set'])
 
 export const Node = (props: {
-    objData: ObjData
+    id: string
+    obj: schema.Obj
     parameters: UnknownParameters
     onTargetRef: (id: string, target: string, ref: HTMLSpanElement) => void
 }) => {
-    const previousMembers = React.useRef<ObjData['members']>([])
+    const currentMembers = React.useRef<schema.Member[]>([])
     const parameters = readParameters(props.parameters, defaultParameters)
+    const wrap = parseFloat(parameters.wrap)
+    const chunkSize = isFinite(wrap) ? wrap : props.obj.members.length
+    const chunks = props.obj.members.reduce((acc, next, i) => {
+        const chunkIndex = Math.floor(i / chunkSize)
+        const chunk = acc[chunkIndex] ?? (acc[chunkIndex] = [])
+        chunk.push(next)
+        return acc
+    }, [] as schema.Member[][])
 
-    React.useEffect(() => {
-        previousMembers.current = props.objData.members
-    })
+    React.useEffect(() => void (currentMembers.current = props.obj.members))
 
     return (
-        <Base title={props.objData.languageType}>
-            <div className={classes.container}>
-                {!supported.has(props.objData.type)
+        <Base title={props.obj.lType}>
+            <div
+                className={classes.container}
+                style={{ flexDirection: parameters.direction === 'row' ? 'column' : 'row' }}
+            >
+                {!supported.has(props.obj.gType)
                     ? 'incompatible'
-                    : props.objData.members.length === 0
+                    : props.obj.members.length === 0
                     ? 'empty'
-                    : props.objData.members.map((member, i) => {
-                          const isPrimitive = typeof member.value !== 'object'
-                          const changed = valueChanged(previousMembers.current[i], member)
-                          const displayValue = getDisplayValue(props.objData, member.value)
+                    : chunks.map((chunk, i) => (
+                          <div className={'d-flex'} style={{ flexDirection: parameters.direction as any }}>
+                              {chunk.map((member, j) => {
+                                  const memberIndex = i * chunkSize + j
+                                  console.log(memberIndex, chunkSize, i, j)
+                                  const showIndex = !parameters['wrap index'] ? memberIndex : j
+                                  const isPrimitive = typeof member.value !== 'object'
+                                  const changed = valueChanged(currentMembers.current[memberIndex], member)
+                                  const displayValue = getDisplayValue(props.id, member.value)
 
-                          return (
-                              <div
-                                  key={i}
-                                  className={classes.element}
-                                  style={{ maxWidth: parameters['max width'], background: styles.background(changed) }}
-                                  title={displayValue}
-                              >
-                                  {parameters['show index'] && <span className={classes.index}>{i}</span>}
-                                  <span
-                                      ref={ref =>
-                                          ref &&
-                                          !isPrimitive &&
-                                          props.onTargetRef(props.objData.id, (member.value as ObjData).id, ref)
-                                      }
-                                      className={classes.value}
-                                  >
-                                      {displayValue}
-                                  </span>
-                              </div>
-                          )
-                      })}
+                                  return (
+                                      <div
+                                          key={memberIndex}
+                                          className={classes.element}
+                                          style={{
+                                              width: parameters.width,
+                                              background: styles.background(changed),
+                                              flexDirection: parameters.direction === 'row' ? 'column' : 'row'
+                                          }}
+                                          title={displayValue}
+                                      >
+                                          {parameters.index && <span className={classes.index}>{showIndex}</span>}
+                                          <span
+                                              ref={ref =>
+                                                  ref &&
+                                                  !isPrimitive &&
+                                                  props.onTargetRef(props.id, (member.value as [string])[0], ref)
+                                              }
+                                              className={classes.value}
+                                          >
+                                              {displayValue}
+                                          </span>
+                                      </div>
+                                  )
+                              })}
+                          </div>
+                      ))}
             </div>
         </Base>
     )
 }
-
-export const NodeParameters = (props: {
-    objData: ObjData
-    withReset: boolean
-    parameters: UnknownParameters
-    onChange: (updatedParameters: ComputedParameters<typeof defaultParameters>) => void
-}) => (
-    <Parameters
-        withReset={props.withReset}
-        parameters={props.parameters}
-        defaults={defaultParameters}
-        onChange={props.onChange}
-    />
-)
