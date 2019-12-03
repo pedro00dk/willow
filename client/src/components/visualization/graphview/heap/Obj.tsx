3705fc206c06aed2c0706da9570d1952b0c6f776
Pedro Henrique <pedro00dk@gmail.com>
2019-12-03 18:15:08 -0300

    Update Obj component to use schema types

--- client/src/components/visualization/graphview/heap/Obj.tsx
+++ client/src/components/visualization/graphview/heap/Obj.tsx
@@ -5,19 +5,21 @@ import 'react-contexify/dist/ReactContexify.min.css'
 import { MenuProvider, Menu, Item, Separator, Submenu } from 'react-contexify'
 import { DefaultState } from '../../../../reducers/Store'
 import { ObjData } from '../../../../reducers/tracer'
-import { GraphController, UnknownParameters } from '../GraphController'
+import * as schema from '../../../../schema/schema'
+import { GraphData, UnknownParameters } from '../GraphData'
 import { Draggable } from '../../../Draggable'
-import { svgScreenTransformVector, svgElementContext } from '../SvgView'
+import { svgScreenTransformVector } from '../svg/SvgView'
 import * as ArrayModule from './nodes/Array'
 import * as BarsModule from './nodes/Bars'
 import * as FieldModule from './nodes/Field'
 import * as MapModule from './nodes/Map'
+import { Parameters } from './Parameters'
 
 const modules = {
-    array: ArrayModule,
-    bars: BarsModule,
-    field: FieldModule,
-    map: MapModule
+    array: ArrayModule
+    // bars: BarsModule,
+    // field: FieldModule,
+    // map: MapModule
 }
 
 const classes = {
@@ -26,58 +28,83 @@ const classes = {
 }
 
 export const Obj = (props: {
-    objData: ObjData
+    id: string
+    obj: schema.Obj
+    graphData: GraphData
+    forceUpdate: React.Dispatch<{}>
     tracer: DefaultState['tracer']
-    controller: GraphController
-    updateGraph: React.Dispatch<{}>
 }) => {
     const ref = React.useRef<HTMLDivElement>()
     const targets = React.useRef<[string, HTMLSpanElement][]>()
     targets.current = []
-    const getSvgElement = React.useContext(svgElementContext)
-    const { id, type, languageType } = props.objData
-    const { index, groupsData } = props.tracer
-    const selector = props.controller.getSelector(id)
-    const parameters =
-        selector === 'id' ? props.controller.getParameters(id) : props.controller.getTypeParameters(languageType)
+    const { id, obj, graphData, forceUpdate, tracer } = props
+    const index = graphData.getIndex()
+    const heap = tracer.steps[index].snapshot.heap
+    const { gType, lType } = obj
+    const selector = props.graphData.getSelector(id)
     const defaultNode = Object.entries(modules)
-        .filter(([, mod]) => mod.defaults.has(type))
+        .filter(([, mod]) => mod.defaults.has(gType))
         .map(([name]) => name)[0]
-    const supportedNodeNames = Object.entries(modules)
-        .filter(([, mod]) => mod.supported.has(type))
+    const supportedNodes = Object.entries(modules)
+        .filter(([, mod]) => mod.supported.has(gType))
         .map(([name]) => name)
-
     const nodeName =
-        selector === 'id'
-            ? props.controller.getNodeName(id, defaultNode)
-            : props.controller.getTypeNodeName(languageType, defaultNode)
-
-    const { Node, NodeParameters } = modules[nodeName as keyof typeof modules]
+        selector === 'id' ? graphData.getNodeName(id, defaultNode) : graphData.getTypeNodeName(lType, defaultNode)
+    const parameters = selector === 'id' ? props.graphData.getParameters(id) : props.graphData.getTypeParameters(lType)
+    const { Node, defaultParameters } = modules[nodeName as keyof typeof modules]
 
     const updatePosition = (delta: { x: number; y: number }, depth: number, update: 'from' | 'all') => {
-        const getLinked = (objData: ObjData, depth = 0, pool = new Set<string>()) => {
-            if (depth < 0 || pool.has(objData.id)) return pool
-            pool.add(objData.id)
-            objData.members
+        const getLinkedObjs = (id: string, obj: schema.Obj, depth = 0, pool = new Set<string>()) => {
+            if (depth < 0 || pool.has(id)) return pool
+            pool.add(id)
+            obj.members
                 .filter(member => typeof member.value === 'object')
-                .forEach(member => getLinked(member.value as ObjData, depth - 1, pool))
+                .forEach(member => {
+                    const memberId = (member.value as [string])[0]
+                    const memberObj = heap[memberId]
+                    getLinkedObjs(memberId, memberObj, depth - 1, pool)
+                })
             return pool
         }
-        getLinked(props.objData, depth).forEach(id => {
-            const position = props.controller.getPosition(id, index)
+        getLinkedObjs(id, obj, depth).forEach(id => {
+            const position = props.graphData.getPosition(id, index)
             const range = [update === 'all' ? 0 : index, props.tracer.heapsData.length] as [number, number]
-            props.controller.setAnimate(false)
-            props.controller.setPositionRange(id, range, { x: position.x + delta.x, y: position.y + delta.y })
-            props.controller.callSubscriptions(id)
+            props.graphData.setAnimate(false)
+            props.graphData.setPositionRange(id, range, { x: position.x + delta.x, y: position.y + delta.y })
+            props.graphData.callSubscriptions(id)
+        })
+    }
+
+    const updateSize = () => {
+        if (!ref.current) return
+        const rect = ref.current.getBoundingClientRect()
+        const screenSize = { x: rect.width, y: rect.height }
+        const svg = ref.current.closest('svg')
+        const [svgSize] = svgScreenTransformVector('toSvg', svg, screenSize)
+        props.graphData.setSizeRange(id, [index, index], svgSize)
+        props.graphData.callSubscriptions(id)
+    }
+
+    const updateTargets = () => {
+        if (!ref.current) return
+        targets.current.forEach(([target, spanRef]) => {
+            const rect = ref.current.getBoundingClientRect()
+            const spanRect = spanRef.getBoundingClientRect()
+            const screenDelta = { x: spanRect.left - rect.left, y: spanRect.top - rect.top }
+            const screenSize = { x: spanRect.width, y: spanRect.height }
+            const svg = ref.current.closest('svg')
+            const [svgDelta, svgSize] = svgScreenTransformVector('toSvg', svg, screenDelta, screenSize)
+            const delta = { x: svgDelta.x + svgSize.x / 2, y: svgDelta.y + svgSize.y / 2 }
+            props.graphData.getTargets(id).push({ target, delta })
         })
     }
 
     const autoLayout = (horizontal: boolean, update: 'from' | 'all') => {
-        const groupData = groupsData[index]
+        const groupData = tracer.groupsData[index]
         const structure = groupData[id]
         if (!structure || structure.type === 'unknown') return
-        const positionAnchor = props.controller.getPosition(id, index)
-        const sizeAnchor = props.controller.getSize(id, index)
+        const positionAnchor = props.graphData.getPosition(id, index)
+        const sizeAnchor = props.graphData.getSize(id, index)
         const increment = { x: sizeAnchor.x * 1.5, y: sizeAnchor.y * 1.5 }
         const range = [update === 'all' ? 0 : index, props.tracer.heapsData.length] as [number, number]
 
@@ -115,36 +142,15 @@ export const Obj = (props: {
         const [positions] = postLayout(props.tracer.heapsData[index][structure.base])
         const basePosition = positions[structure.base]
         const baseDelta = { x: positionAnchor.x - basePosition.x, y: positionAnchor.y - basePosition.y }
-        props.controller.setAnimate(true)
+        props.graphData.setAnimate(true)
         Object.entries(positions).forEach(([id, position]) => {
             position.x += baseDelta.x
             position.y += baseDelta.y
-            props.controller.setPositionRange(id, range, position)
-            props.controller.callSubscriptions(id)
+            props.graphData.setPositionRange(id, range, position)
+            props.graphData.callSubscriptions(id)
         })
     }
 
-    const updateSize = () => {
-        if (!ref.current) return
-        const rect = ref.current.getBoundingClientRect()
-        const screenSize = { x: rect.width, y: rect.height }
-        const [svgSize] = svgScreenTransformVector('toSvg', getSvgElement(), screenSize)
-        props.controller.setSizeRange(id, [index, index], svgSize)
-        props.controller.callSubscriptions(id)
-    }
-
-    const updateTargets = () => {
-        if (!ref.current) return
-        targets.current.forEach(([target, spanRef]) => {
-            const rect = ref.current.getBoundingClientRect()
-            const spanRect = spanRef.getBoundingClientRect()
-            const screenDelta = { x: spanRect.left - rect.left, y: spanRect.top - rect.top }
-            const screenSize = { x: spanRect.width, y: spanRect.height }
-            const [svgDelta, svgSize] = svgScreenTransformVector('toSvg', getSvgElement(), screenDelta, screenSize)
-            const delta = { x: svgDelta.x + svgSize.x / 2, y: svgDelta.y + svgSize.y / 2 }
-            props.controller.getTargets(id).push({ target, delta })
-        })
-    }
     return (
         <Draggable
             props={{
@@ -157,7 +163,8 @@ export const Obj = (props: {
                 }
             }}
             onDrag={(delta, event) => {
-                const [svgDelta] = svgScreenTransformVector('toSvg', getSvgElement(), delta)
+                const svg = ref.current.closest('svg')
+                const [svgDelta] = svgScreenTransformVector('toSvg', svg, delta)
                 const depth = !event.altKey ? 0 : Infinity
                 const update = !event.ctrlKey ? 'from' : 'all'
                 updatePosition(svgDelta, depth, update)
@@ -166,7 +173,8 @@ export const Obj = (props: {
             <MenuProvider id={id} className={classes.menuProvider}>
                 <div className={classes.menuProvider}>
                     <Node
-                        objData={props.objData}
+                        id={id}
+                        obj={obj}
                         parameters={parameters}
                         onTargetRef={(id, target, spanRef) => targets.current.push([target, spanRef])}
                     />
@@ -175,24 +183,24 @@ export const Obj = (props: {
             <Menu id={id}>
                 <Item
                     onClick={args => (
-                        props.controller.setSelector(id, selector === 'id' ? 'type' : 'id'), props.updateGraph({})
+                        props.graphData.setSelector(id, selector === 'id' ? 'type' : 'id'), props.forceUpdate({})
                     )}
                 >
                     {`using ${selector} parameters`}
                 </Item>
                 <Separator />
                 <Submenu label='node'>
-                    <Item onClick={args => (props.controller.setNodeName(id, undefined), props.updateGraph({}))}>
+                    <Item onClick={args => (props.graphData.setNodeName(id, undefined), props.forceUpdate({}))}>
                         reset
                     </Item>
-                    {supportedNodeNames.map((nodeName, i) => (
+                    {supportedNodes.map((nodeName, i) => (
                         <Item
                             key={i}
                             onClick={args => {
                                 selector === 'id'
-                                    ? props.controller.setNodeName(id, nodeName)
-                                    : props.controller.setTypeNodeName(languageType, nodeName)
-                                props.updateGraph({})
+                                    ? props.graphData.setNodeName(id, nodeName)
+                                    : props.graphData.setTypeNodeName(lType, nodeName)
+                                props.forceUpdate({})
                             }}
                         >
                             {nodeName}
@@ -201,15 +209,15 @@ export const Obj = (props: {
                 </Submenu>
                 <Separator />
                 <Submenu label='parameters'>
-                    <NodeParameters
-                        objData={props.objData}
+                    <Parameters
                         withReset
                         parameters={parameters}
+                        defaults={defaultParameters}
                         onChange={(updatedParameters: UnknownParameters) => {
                             selector === 'id'
-                                ? props.controller.setParameters(id, updatedParameters)
-                                : props.controller.setTypeParameters(languageType, updatedParameters)
-                            props.updateGraph({})
+                                ? props.graphData.setParameters(id, updatedParameters)
+                                : props.graphData.setTypeParameters(lType, updatedParameters)
+                            props.forceUpdate({})
                         }}
                     />
                 </Submenu>
