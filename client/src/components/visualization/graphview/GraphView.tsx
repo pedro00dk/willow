2deb68306d49554d97ec982202fc15774b8febe3
Pedro Henrique <pedro00dk@gmail.com>
2019-12-03 16:42:10 -0300

    Update GraphView and Heap compnents to match small changs in GraphData

--- client/src/components/visualization/graphview/GraphView.tsx
+++ client/src/components/visualization/graphview/GraphView.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import { useSelection } from '../../../reducers/Store'
-import { GraphController } from './GraphController'
+import { GraphData } from './GraphData'
 import { Heap } from './heap/Heap'
-import { SvgView } from './SvgView'
+import { SvgView } from './svg/SvgView'
 
 const classes = {
     container: 'd-flex w-100 h-100'
 }
 
 export const GraphView = React.memo(() => {
-    const controller = React.useRef(new GraphController({ x: 1000, y: 1000 }, { x: 20, y: 20 }))
-    const updateGraph = React.useState({})[1]
+    const graphData = React.useRef(new GraphData({ x: 1000, y: 1000 }, { x: 20, y: 20 }))
+    const forceUpdate = React.useState({})[1]
     const { tracer } = useSelection(state => ({ tracer: state.tracer }))
-    controller.current.clearSubscriptions()
-    controller.current.clearTargets()
-    controller.current.setIndex(tracer.index)
-    controller.current.setAnimate(true)
+    graphData.current.setIndex(tracer.index)
+    graphData.current.setAnimate(true)
+    graphData.current.clearTargets()
+    graphData.current.clearSubscriptions()
 
     return (
         <div className={classes.container}>
-            <SvgView size={controller.current.getGraphSize()}>
-                {tracer.available && <Heap controller={controller.current} updateGraph={updateGraph} tracer={tracer} />}
+            <SvgView graphData={graphData.current}>
+                <Heap graphData={graphData.current} forceUpdate={forceUpdate} tracer={tracer} />
             </SvgView>
         </div>
     )
--- client/src/components/visualization/graphview/heap/Heap.tsx
+++ client/src/components/visualization/graphview/heap/Heap.tsx
@@ -1,24 +1,20 @@
 import React from 'react'
 import { DefaultState } from '../../../../reducers/Store'
-import { GraphController } from '../GraphController'
-import { SvgWrapper } from '../SvgWrapper'
+import { GraphData } from '../GraphData'
+import { SvgWrapper } from '../svg/SvgWrapper'
 import { Obj } from './Obj'
 
 export const Heap = (props: {
-    controller: GraphController
-    updateGraph: React.Dispatch<{}>
+    graphData: GraphData
+    forceUpdate: React.Dispatch<{}>
     tracer: DefaultState['tracer']
 }) => (
     <>
-        {Object.values(props.tracer.heapsData[props.tracer.index]).map(objData => (
-            <SvgWrapper
-                key={objData.id}
-                id={objData.id}
-                paths={objData.members.reduce((acc, next) => acc + Number(typeof next.value === 'object'), 0)}
-                {...props}
-            >
-                <Obj objData={objData} {...props} />
-            </SvgWrapper>
-        ))}
+        {props.tracer.steps &&
+            Object.entries(props.tracer.steps[props.tracer.index].snapshot?.heap ?? {}).map(([id, obj]) => (
+                <SvgWrapper key={id} id={id} controller={props.graphData}>
+                    <Obj id={id} obj={obj} {...props} />
+                </SvgWrapper>
+            ))}
     </>
 )
