e098f4d3504f00d8215198ca8801e4584fee409a
Pedro Henrique <pedro00dk@gmail.com>
2019-12-04 14:27:05 -0300

    Implement the GraphView Stack to generate stack references in the graph

---
+++ client/src/components/visualization/graphview/stack/Stack.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { DefaultState } from '../../../../reducers/Store'
+import * as schema from '../../../../schema/schema'
+import { GraphData } from '../GraphData'
+
+export const Stack = (props: {
+    graphData: GraphData
+    forceUpdate: React.Dispatch<{}>
+    tracer: DefaultState['tracer']
+}) => {
+    if (!props.tracer.steps) return <></>
+    const deltaSize = props.graphData.getGraphSize().x * 0.04
+    const stack = props.tracer.steps[props.graphData.getIndex()].snapshot?.stack ?? []
+    const variables = stack
+        .flatMap(scope => scope.variables)
+        .filter(variable => typeof variable.value === 'object')
+        .reduce((acc, next) => {
+            const id = (next.value as [string])[0]
+            if (!acc[id]) acc[id] = []
+            if (acc[id].length > 3) return acc
+            acc[id].push(next.name)
+            return acc
+        }, {} as { [id: string]: string[] })
+
+    console.log(variables)
+    Object.entries(variables).forEach(([id, names]) =>
+        names.forEach((name, i) => {
+            const angle = (i * 20 + 5) * (Math.PI / 180)
+            console.log(angle)
+            const delta = { x: deltaSize * Math.cos(angle), y: deltaSize * -Math.sin(angle) }
+            console.log(delta)
+            props.graphData.getTargets(id).push({ target: id, delta, text: name })
+        })
+    )
+
+    return <></>
+}
