63156700fd31a22f6d3e7976bb69f6658761bf82
Pedro Henrique <pedro00dk@gmail.com>
2020-01-10 18:07:40 -0300

    Update python tracer Dockerfile

--- tracers/python/src/main.py
+++ tracers/python/src/main.py
@@ -5,25 +5,20 @@ import tracer
 
 
 def main():
-    """
-    Entrypoint of the tracer.
-    Build the command line interface, run the tracer and print results.
-    """
+    parser = argparse.ArgumentParser(description='Python tracer CLI', usage=argparse.SUPPRESS)
+    parser.usage = 'tracer [options]\n  stdin: {"source?": "string", "input"?: "string", "steps?": "number"}'
+    parser.add_argument('--pretty', default=False, action='store_true', help='Pretty print output')
+    parser.add_argument('--test', default=False, action='store_true', help='Run the test source')
+    options = parser.parse_args()
 
-    argument_parser = argparse.ArgumentParser(description='Python tracer CLI', usage=argparse.SUPPRESS)
-    argument_parser.usage = 'tracer [options]\n  stdin: {"source?": "string", "input"?: "string", "steps?": "number"}'
-    argument_parser.add_argument('--pretty', default=False, action='store_true', help='Pretty print output')
-    argument_parser.add_argument('--test', default=False, action='store_true', help='Run the test source')
-    options = argument_parser.parse_args()
-
-    trace_data = json.loads(input())
+    raw_trace = json.loads(input())
     trace = {
         'source':
-        trace_data['source'] if not options.test and trace_data.get('source') is not None else
+        raw_trace['source'] if not options.test and raw_trace.get('source') is not None else
         pathlib.Path('./res/main.py').read_text(encoding='utf8') if options.test else
         '',
-        'input': trace_data['input'] if trace_data.get('input') is not None else '',
-        'steps': trace_data['steps'] if trace_data.get('steps') is not None else 2 ** 31 - 1
+        'input': raw_trace['input'] if raw_trace.get('input') is not None else '',
+        'steps': raw_trace['steps'] if raw_trace.get('steps') is not None else 2 ** 31 - 1
     }
 
     result = tracer.Tracer(trace).run()
