130c738003d2c7ea637118ed99c90c9684c4fcb7
Pedro Henrique <pedro00dk@gmail.com>
2020-01-31 13:36:46 -0300

    Simplify Language component (fix firefox not showing names)

--- client/src/components/controls/Language.tsx
+++ client/src/components/controls/Language.tsx
@@ -1,27 +1,14 @@
 import cn from 'classnames'
 import { css } from 'emotion'
 import React from 'react'
-import { colors } from '../../colors'
 import { actions as languageActions } from '../../reducers/language'
 import { useDispatch, useSelection } from '../../reducers/Store'
 
 const classes = {
-    container: 'd-flex input-group w-auto',
-    input: 'input-group-prepend',
-    button: cn(
-        'd-inline-flex align-items-center btn p-1',
-        css({
-            background: colors.gray.light,
-            border: `1px solid ${colors.gray.dark}`,
-            color: colors.black,
-            fontSize: '1rem',
-            width: '7rem',
-            ':hover': { background: colors.gray.main }
-        })
-    ),
-    label: 'flex-fill',
-    spin: 'spinner-grow spinner-grow-sm',
-    select: cn('custom-select', css({ flex: '0 1 auto !important', color: colors.black, width: '6rem !important' }))
+    container: 'input-group w-auto',
+    group: 'input-group-prepend',
+    label: 'input-group-text',
+    select: cn('custom-select', css({ flex: '0 1 auto !important', width: '6rem !important' }))
 }
 
 export const Language = () => {
@@ -35,20 +22,17 @@ export const Language = () => {
 
     return (
         <div className={classes.container}>
-            <div className={classes.input}>
-                <button className={classes.button}>
-                    <span className={classes.label}>Language</span>
-                    {fetching && <span className={classes.spin} />}
-                </button>
+            <div className={classes.group}>
+                <span className={classes.label}>Language</span>
             </div>
             <select
                 className={classes.select}
                 defaultValue={selectedLanguage}
                 onChange={event => dispatch(languageActions.select(event.target.selectedIndex))}
             >
-                {languages.length === 0 && <option key={-1} label={fetching ? '...' : '!'} />}
+                {languages.length === 0 && <option key={-1}>{fetching ? '...' : '!!!'}</option>}
                 {languages.map((language, i) => (
-                    <option key={i} value={language} label={language} />
+                    <option key={i}>{language}</option>
                 ))}
             </select>
         </div>
