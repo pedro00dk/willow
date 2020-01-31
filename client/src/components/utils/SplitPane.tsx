209f120d250c61e609e842f11fa610c4a1e70270
Pedro Henrique <pedro00dk@gmail.com>
2020-01-31 17:20:45 -0300

    Modify Dragger style to make easier to pull

--- client/src/components/utils/SplitPane.tsx
+++ client/src/components/utils/SplitPane.tsx
@@ -1,16 +1,30 @@
+import cn from 'classnames'
 import { css } from 'emotion'
 import React from 'react'
-import { colors } from '../../colors'
 import { Draggable } from './Draggable'
 
 const classes = {
     container: 'd-flex w-100 h-100',
     pane: 'd-flex',
-    dragger: css({ background: colors.gray.light })
+    dragger: {
+        base: cn(
+            'd-flex',
+            css({
+                background: 'padding-box lightgray',
+                boxSizing: 'border-box',
+                border: '0px solid transparent',
+                transition: 'all 0.2s ease',
+                zIndex: 10,
+                ':hover': { background: 'darkgray', border: '4px solid lightgray' }
+            })
+        ),
+        row: css({ width: '10px', borderLeftWidth: 4, borderRightWidth: 4, marginLeft: -4, marginRight: -4 }),
+        column: css({ height: '10px', borderTopWidth: 4, borderBottomWidth: 4, marginTop: -4, marginBottom: -4 })
+    }
 }
 
 const styles = {
-    size: (ratio: number, crop: number) => `calc(${(ratio * 100).toFixed(2)}% - ${crop}px)`,
+    size: (ratio: number) => `calc(${ratio * 100}% - 1px)`,
     cursor: (layout: 'row' | 'column') => (layout === 'row' ? 'ew-resize' : 'ns-resize')
 }
 
@@ -18,44 +32,34 @@ export const SplitPane = (props: {
     orientation?: 'row' | 'column'
     ratio?: number
     range?: [number, number]
-    draggerSize?: number
     children?: [React.ReactNode, React.ReactNode]
 }) => {
-    const { orientation = 'row', ratio: initialRatio = 0.5, range = [0.2, 0.8], draggerSize = 6, children } = props
+    const { orientation = 'row', ratio: initialRatio = 0.5, range = [0.2, 0.8], children } = props
     const container$ = React.useRef<HTMLDivElement>()
     const firstPane$ = React.useRef<HTMLDivElement>()
     const secondPane$ = React.useRef<HTMLDivElement>()
     const ratio = React.useRef(Math.min(Math.max(initialRatio, range[0]), range[1]))
-    const variableSize = orientation === 'row' ? 'width' : 'height'
-    const fixedSize = orientation === 'row' ? 'height' : 'width'
+    const freeDimension = orientation === 'row' ? 'width' : 'height'
 
     return (
         <div ref={container$} className={classes.container} style={{ flexDirection: orientation }}>
-            <div
-                ref={firstPane$}
-                className={classes.pane}
-                style={{ [fixedSize]: '100%', [variableSize]: styles.size(ratio.current, draggerSize / 2) }}
-            >
+            <div ref={firstPane$} className={classes.pane} style={{ [freeDimension]: styles.size(ratio.current) }}>
                 {children[0]}
             </div>
             <Draggable
                 props={{
-                    className: classes.dragger,
-                    style: { [fixedSize]: '100%', [variableSize]: draggerSize, cursor: styles.cursor(orientation) }
+                    className: cn(classes.dragger.base, classes.dragger[orientation]),
+                    style: { cursor: styles.cursor(orientation) }
                 }}
                 onDrag={delta => {
                     const rect = container$.current.getBoundingClientRect()
                     const change = orientation === 'row' ? delta.x / rect.width : delta.y / rect.height
                     ratio.current = Math.min(Math.max(ratio.current + change, range[0]), range[1])
-                    firstPane$.current.style[variableSize] = styles.size(ratio.current, draggerSize / 2)
-                    secondPane$.current.style[variableSize] = styles.size(1 - ratio.current, draggerSize / 2)
+                    firstPane$.current.style[freeDimension] = styles.size(ratio.current)
+                    secondPane$.current.style[freeDimension] = styles.size(1 - ratio.current)
                 }}
             />
-            <div
-                ref={secondPane$}
-                className={classes.pane}
-                style={{ [fixedSize]: '100%', [variableSize]: styles.size(1 - ratio.current, draggerSize / 2) }}
-            >
+            <div ref={secondPane$} className={classes.pane} style={{ [freeDimension]: styles.size(1 - ratio.current) }}>
                 {children[1]}
             </div>
         </div>
