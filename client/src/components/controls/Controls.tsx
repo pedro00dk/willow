c9b3db2773d1036b49765b90e8fe0eda4a3ad292
Pedro Henrique <pedro00dk@gmail.com>
2020-01-31 14:36:03 -0300

    Remove unnecessary alignment in Controls

--- client/src/components/controls/Controls.tsx
+++ client/src/components/controls/Controls.tsx
@@ -3,14 +3,14 @@ import { Language } from './Language'
 import { Toolbar } from './Toolbar'
 
 const classes = {
-    container: 'd-flex align-items-center shadow-sm p-2 mb-1',
-    split: 'px-2'
+    container: 'd-flex shadow-sm p-2 mb-1',
+    spacer: 'mx-2'
 }
 
 export const Controls = () => (
     <div className={classes.container}>
         <Language />
-        <span className={classes.split} />
+        <span className={classes.spacer} />
         <Toolbar />
     </div>
 )
