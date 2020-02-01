541bef53ea590b3d3efac469cc90c324503c75d7
Pedro Henrique <pedro00dk@gmail.com>
2020-02-01 17:05:08 -0300

    StackTrace no more computes its own size to implement overflow

--- client/src/components/visualization/stacktrace/StackTrace.tsx
+++ client/src/components/visualization/stacktrace/StackTrace.tsx
@@ -1,5 +1,3 @@
-import cn from 'classnames'
-import { css } from 'emotion'
 import React from 'react'
 import { useSelection } from '../../../reducers/Store'
 import * as schema from '../../../schema/schema'
@@ -8,40 +6,19 @@ import { ScopeTrace } from './ScopeTrace'
 export type ScopeSlice = { name: string; range: [number, number]; nodes: schema.Scope[]; children: schema.Scope[][] }
 
 const classes = {
-    container: cn('d-flex position-absolute align-items-start flex-nowrap overflow-auto', css({ userSelect: 'none' }))
+    container: 'd-flex  overflow-auto w-100'
 }
 
-export const StackTrace = React.memo(() => {
-    const ref = React.useRef<HTMLDivElement>()
-    const [size, setSize] = React.useState({ x: 0, y: 0 })
+export const StackTrace = () => {
     const { steps } = useSelection(state => ({ steps: state.tracer.steps }))
 
     const scopeSlice = React.useMemo(() => {
         if (!steps) return
-        return {
-            name: undefined,
-            range: [0, steps.length - 1],
-            nodes: [],
-            children: steps.map(
-                ({ snapshot, threw }) =>
-                    snapshot?.stack ?? [{ name: threw.cause ?? threw.exception.type } as schema.Scope]
-            )
-        } as ScopeSlice
+        const children = steps.map(({ snapshot, threw }) => {
+            return snapshot?.stack ?? [{ name: threw.cause ?? threw.exception.type } as schema.Scope]
+        })
+        return { name: undefined, range: [0, steps.length - 1], nodes: [], children } as ScopeSlice
     }, [steps])
 
-    React.useEffect(() => {
-        const interval = setInterval(() => {
-            const parentSize = { x: ref.current.parentElement.clientWidth, y: ref.current.parentElement.clientHeight }
-            if (size.x === parentSize.x && size.y === parentSize.y) return
-            setSize(parentSize)
-        }, 1000)
-
-        return () => clearInterval(interval)
-    }, [ref.current, size])
-
-    return (
-        <div ref={ref} className={classes.container} style={{ width: size.x - 1, height: size.y - 1 }}>
-            {steps && <ScopeTrace scopeSlice={scopeSlice} width={size.x} />}
-        </div>
-    )
-})
+    return <div className={classes.container}>{steps && <ScopeTrace scopeSlice={scopeSlice} />}</div>
+}
