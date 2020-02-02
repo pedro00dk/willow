41d95b215d0494c292778e8147333ba96dd5fcbc
Pedro Henrique <pedro00dk@gmail.com>
2020-02-01 21:20:51 -0300

    Remove wrapper from GraphView since it is not necessary anymore

--- client/src/components/visualization/graphview/GraphView.tsx
+++ client/src/components/visualization/graphview/GraphView.tsx
@@ -5,25 +5,19 @@ import { Heap } from './heap/Heap'
 import { Stack } from './stack/Stack'
 import { SvgView } from './svg/SvgView'
 
-const classes = {
-    container: 'd-flex w-100 h-100'
-}
-
 export const GraphView = React.memo(() => {
-    const graphData = React.useRef(new GraphData({ x: 1000, y: 1000 }, { x: 20, y: 20 }))
     const forceUpdate = React.useState({})[1]
     const { tracer } = useSelection(state => ({ tracer: state.tracer }))
+    const graphData = React.useRef(new GraphData({ width: 1000, height: 1000 }, { x: 20, y: 20 }))
     graphData.current.setIndex(tracer.index)
     graphData.current.setAnimate(true)
     graphData.current.clearTargets()
     graphData.current.clearSubscriptions()
 
     return (
-        <div className={classes.container}>
-            <SvgView graphData={graphData.current}>
-                <Stack graphData={graphData.current} forceUpdate={forceUpdate} tracer={tracer} />
-                <Heap graphData={graphData.current} forceUpdate={forceUpdate} tracer={tracer} />
-            </SvgView>
-        </div>
+        <SvgView size={graphData.current.getGraphSize()}>
+            <Stack graphData={graphData.current} forceUpdate={forceUpdate} tracer={tracer} />
+            <Heap graphData={graphData.current} forceUpdate={forceUpdate} tracer={tracer} />
+        </SvgView>
     )
 })
