57de3464a356e4f91150b212604a2b36eb4d0116
Pedro Henrique <pedro00dk@gmail.com>
2020-02-04 13:34:17 -0300

    Split the Stack components in different files

---
+++ client/src/components/visualization/stack/Scope.tsx
@@ -0,0 +1,56 @@
+import cn from 'classnames'
+import { css } from 'emotion'
+import React from 'react'
+import * as schema from '../../../schema/schema'
+import { memberChanged, getDisplayValue } from '../graphview/Base'
+import { colors } from '../../../colors'
+
+const classes = {
+    container: 'd-flex flex-column table table-sm table-hover border',
+    column: 'd-flex flex-column',
+    row: 'd-flex',
+    headerCell: cn('text-truncate w-100', css({ fontSize: '1rem' })),
+    cell: cn('text-truncate w-50', css({ fontSize: '1rem' }))
+}
+
+const styles = {
+    variable: (changed: boolean) => (changed ? colors.yellow.lighter : undefined)
+}
+
+export const Scope = (props: { scope: schema.Scope }) => {
+    const currentVariables = React.useRef<{ [name: string]: schema.Variable }>({})
+
+    React.useEffect(() => {
+        currentVariables.current = props.scope.variables.reduce(
+            (acc, next) => ((acc[next.name] = next), acc),
+            {} as { [name: string]: schema.Variable }
+        )
+    })
+
+    return (
+        <table className={classes.container}>
+            <thead className={classes.column}>
+                <tr className={classes.row}>
+                    <th className={classes.headerCell}>{props.scope.name}</th>
+                </tr>
+            </thead>
+            <tbody className={classes.column}>
+                {props.scope.variables.map((variable, i) => {
+                    const displayValue = getDisplayValue(undefined, variable.value)
+                    const changed = memberChanged(currentVariables.current[variable.name], variable)
+                    return (
+                        <tr
+                            key={i}
+                            className={classes.row}
+                            style={{ background: styles.variable(changed) }}
+                            title={displayValue}
+                        >
+                            <td className={classes.cell}>{variable.name}</td>
+                            <td className={classes.cell}>{displayValue}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
+}
--- client/src/components/visualization/stack/Stack.tsx
+++ client/src/components/visualization/stack/Stack.tsx
@@ -1,22 +1,9 @@
-import cn from 'classnames'
-import { css } from 'emotion'
 import React from 'react'
-import * as schema from '../../../schema/schema'
 import { useSelection } from '../../../reducers/Store'
-import { memberChanged, getDisplayValue } from '../graphview/Base'
-import { colors } from '../../../colors'
+import { Scope } from './Scope'
 
 const classes = {
-    container: 'd-flex flex-column position-absolute overflow-auto',
-    table: 'd-flex flex-column table table-sm table-hover border',
-    row: 'd-flex',
-    column: 'd-flex flex-column',
-    headerCell: cn('text-truncate w-100', css({ fontSize: '1rem' })),
-    cell: cn('text-truncate w-50', css({ fontSize: '1rem' }))
-}
-
-const styles = {
-    variable: (changed: boolean) => (changed ? colors.yellow.lighter : undefined)
+    container: 'd-flex flex-column position-absolute overflow-auto'
 }
 
 export const Stack = () => {
@@ -41,7 +28,7 @@ export const Stack = () => {
         onResize(undefined)
         globalThis.addEventListener('paneResize', onResize)
         return () => globalThis.removeEventListener('paneResize', onResize)
-    }, [container$])
+    }, [container$.current])
 
     React.useEffect(() => container$.current.scrollBy({ top: Number.MAX_SAFE_INTEGER, behavior: 'smooth' }))
 
@@ -52,43 +39,5 @@ export const Stack = () => {
                 <Scope key={i} scope={scope} />
             ))}
         </div>
-    )
-}
-
-const Scope = (props: { scope: schema.Scope }) => {
-    const currentVariables = React.useRef<{ [name: string]: schema.Variable }>({})
-
-    React.useEffect(() => {
-        currentVariables.current = props.scope.variables.reduce(
-            (acc, next) => ((acc[next.name] = next), acc),
-            {} as { [name: string]: schema.Variable }
-        )
-    })
-
-    return (
-        <table className={classes.table}>
-            <thead className={classes.column}>
-                <tr className={classes.row}>
-                    <th className={classes.headerCell}>{props.scope.name}</th>
-                </tr>
-            </thead>
-            <tbody className={classes.column}>
-                {props.scope.variables.map((variable, i) => {
-                    const displayValue = getDisplayValue(undefined, variable.value)
-                    const changed = memberChanged(currentVariables.current[variable.name], variable)
-                    return (
-                        <tr
-                            key={i}
-                            className={classes.row}
-                            style={{ background: styles.variable(changed) }}
-                            title={displayValue}
-                        >
-                            <td className={classes.cell}>{variable.name}</td>
-                            <td className={classes.cell}>{displayValue}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
     )
 }
