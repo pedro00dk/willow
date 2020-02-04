a0fbce174becde73b5be46531457fd2fe4bde886
Pedro Henrique <pedro00dk@gmail.com>
2020-02-04 17:32:06 -0300

    Update GraphView calls to GraphData

--- client/src/components/visualization/graphview/GraphView.tsx
+++ client/src/components/visualization/graphview/GraphView.tsx
@@ -8,14 +8,14 @@ import { SvgView } from './svg/SvgView'
 export const GraphView = React.memo(() => {
     const forceUpdate = React.useState({})[1]
     const { tracer } = useSelection(state => ({ tracer: state.tracer }))
-    const graphData = React.useRef(new GraphData({ width: 1000, height: 1000 }, { x: 20, y: 20 }))
+    const graphData = React.useRef(new GraphData({ width: 1200, height: 1000 }, { x: 20, y: 20 }))
     graphData.current.setIndex(tracer.index)
     graphData.current.setAnimate(true)
-    graphData.current.clearTargets()
     graphData.current.clearSubscriptions()
+    graphData.current.clearEdges()
 
     return (
-        <SvgView size={graphData.current.getGraphSize()}>
+        <SvgView graphData={graphData.current}>
             <Stack graphData={graphData.current} forceUpdate={forceUpdate} tracer={tracer} />
             <Heap graphData={graphData.current} forceUpdate={forceUpdate} tracer={tracer} />
         </SvgView>
