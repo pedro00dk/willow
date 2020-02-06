1bd80e17b8276d552455b9b00b00b40dfe3a681d
Pedro Henrique <pedro00dk@gmail.com>
2020-02-06 20:06:39 -0300

    Remove GroupData computations from tracer reducer

--- client/src/reducers/tracer.ts
+++ client/src/reducers/tracer.ts
@@ -2,30 +2,15 @@ import { api } from '../api'
 import * as schema from '../schema/schema'
 import { DefaultAsyncAction } from './Store'
 
-export type GroupData = {
-    [id: string]: StructureData
-}
-
-export type StructureData = {
-    base: string
-    depth: number
-    members: Set<string>
-    hasCycleEdge: boolean
-    hasParentEdge: boolean
-    hasCrossEdge: boolean
-    type: 'node' | 'list' | 'tree' | 'unknown'
-}
-
 type State = {
     fetching: boolean
     index?: number
     steps?: schema.Step[]
-    groupsData?: GroupData[]
     error?: string
 }
 
 type Action =
-    | { type: 'tracer/trace'; payload?: { steps: schema.Step[]; groupsData: GroupData[] }; error?: string }
+    | { type: 'tracer/trace'; payload?: { steps: schema.Step[] }; error?: string }
     | { type: 'tracer/setIndex'; payload: number }
 
 const initialState: State = {
@@ -47,66 +32,6 @@ export const reducer = (state: State = initialState, action: Action): State => {
     }
 }
 
-const buildStructureData = (
-    heap: schema.Snapshot['heap'],
-    id: string,
-    parentId: string,
-    depth: number,
-    structureData: StructureData
-) => {
-    structureData.members.add(id)
-    structureData.depth = Math.max(structureData.depth, depth)
-    structureData.type =
-        structureData.members.size === 1
-            ? 'node'
-            : structureData.members.size === structureData.depth
-            ? 'list'
-            : !structureData.hasCrossEdge
-            ? 'tree'
-            : 'unknown'
-    heap[id].members
-        .filter(member => typeof member.value === 'object' && heap[member.value[0]].lType === heap[id].lType)
-        .map(member => (member.value as [string])[0])
-        .forEach(memberId => {
-            if (!structureData.members.has(memberId))
-                return buildStructureData(heap, memberId, id, depth + 1, structureData)
-            structureData.hasCycleEdge = structureData.hasCycleEdge || memberId === id
-            structureData.hasParentEdge = structureData.hasParentEdge || memberId === parentId
-            structureData.hasCrossEdge = structureData.hasCrossEdge || (memberId !== id && memberId !== parentId)
-        })
-    return structureData
-}
-
-const buildGroupsData = (steps: schema.Step[]) => {
-    let previous: GroupData = {}
-    return steps.map(({ snapshot }, i) => {
-        if (!snapshot) return previous
-        const groupData: GroupData = {}
-        const references = snapshot.stack
-            .flatMap(scope => scope.variables)
-            .filter(variable => typeof variable.value === 'object')
-            .map(variable => (variable.value as [string])[0])
-            .map(id => [id, snapshot.heap[id].members] as const)
-            .map(([id, members]) => [id, members.filter(member => typeof member.value === 'object')] as const)
-            .flatMap(([id, members]) => [id, ...members.map(member => (member.value as [string])[0])])
-        const heap = snapshot?.heap ?? {}
-        new Set(references).forEach(id => {
-            if (groupData[id]) return
-            const structureData = buildStructureData(heap, id, undefined, 1, {
-                base: id,
-                depth: 0,
-                members: new Set(),
-                hasCycleEdge: false,
-                hasParentEdge: false,
-                hasCrossEdge: false,
-                type: 'unknown'
-            })
-            structureData.members.forEach(id => (groupData[id] = structureData))
-        })
-        return (previous = groupData)
-    })
-}
-
 const trace = (): DefaultAsyncAction => async (dispatch, getState) => {
     dispatch({ type: 'tracer/trace' })
     try {
@@ -118,9 +43,7 @@ const trace = (): DefaultAsyncAction => async (dispatch, getState) => {
                 input: input.join('\n')
             })
         ).data
-        const steps = result.steps
-        const groupsData = buildGroupsData(steps)
-        dispatch({ type: 'tracer/trace', payload: { steps, groupsData } })
+        dispatch({ type: 'tracer/trace', payload: { steps: result.steps } })
     } catch (error) {
         dispatch({ type: 'tracer/trace', error: error.response ? error.response.data : error.toString() })
     }
