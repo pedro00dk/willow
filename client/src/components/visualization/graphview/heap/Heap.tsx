a32d6e5099a7993afdc27c2d5c349fea9870903f
Pedro Henrique <pedro00dk@gmail.com>
2020-02-07 17:41:06 -0300

    Compute depth of objs from stack variables in Heap

--- client/src/components/visualization/graphview/heap/Heap.tsx
+++ client/src/components/visualization/graphview/heap/Heap.tsx
@@ -1,20 +1,56 @@
-import React from 'react'
+import React, { Children } from 'react'
 import { DefaultState } from '../../../../reducers/Store'
 import { GraphData } from '../GraphData'
 import { SvgNode } from '../svg/SvgNode'
 import { Obj } from './Obj'
+import * as schema from '../../../../schema/schema'
 
 export const Heap = (props: {
     graphData: GraphData
     forceUpdate: React.Dispatch<{}>
     tracer: DefaultState['tracer']
-}) => (
-    <>
-        {props.tracer.steps &&
-            Object.keys(props.tracer.steps[props.graphData.getIndex()].snapshot?.heap ?? {}).map(id => (
-                <SvgNode key={id} id={id} graphData={props.graphData}>
-                    <Obj id={id} {...props} />
-                </SvgNode>
-            ))}
-    </>
-)
+}) => {
+    const depths = React.useMemo(() => {
+        const computeDepths = (
+            ids: string[],
+            heap: { [id: string]: schema.Obj },
+            depths: { [id: string]: number } = {},
+            depth = 0
+        ) => {
+            if (ids.length === 0) return depths
+            const childrenIds = ids.reduce((acc, next, i) => {
+                if (depths[next] !== undefined) return acc
+                depths[next] = depth + i
+                acc.push(
+                    ...heap[next].members
+                        .filter(member => typeof member.value === 'object')
+                        .map(member => (member.value as string[])[0])
+                )
+                return acc
+            }, [] as string[])
+            computeDepths(childrenIds, heap, depths, depth + 1000)
+            return depths
+        }
+
+        return (props.tracer.steps ?? []).reduce((acc, step, i) => {
+            const { stack = [], heap = {} } = step.snapshot ?? {}
+            const stackIds = stack
+                .flatMap(scope => scope.variables)
+                .filter(variable => typeof variable.value === 'object')
+                .map(variable => (variable.value as [string])[0])
+            acc.push(computeDepths(stackIds, heap))
+            return acc
+        }, [] as { [id: string]: number }[])
+    }, [props.tracer.steps])
+
+    return (
+        <>
+            {props.tracer.steps &&
+                Object.entries(depths).map(([id, depth]) => (
+                    <SvgNode key={id} id={id} graphData={props.graphData}>
+                        <Obj id={id} {...props} />
+                    </SvgNode>
+                ))}
+        </>
+    )
+}
