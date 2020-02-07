675e6db648eaa85bc9ad2cea7b689ba85c814b5c
Pedro Henrique <pedro00dk@gmail.com>
2020-02-07 20:14:33 -0300

    Compute depths only when necessary in Heap

--- client/src/components/visualization/graphview/heap/Heap.tsx
+++ client/src/components/visualization/graphview/heap/Heap.tsx
@@ -1,47 +1,39 @@
-import React, { Children } from 'react'
+import React from 'react'
 import { DefaultState } from '../../../../reducers/Store'
 import { GraphData } from '../GraphData'
 import { SvgNode } from '../svg/SvgNode'
 import { Obj } from './Obj'
-import * as schema from '../../../../schema/schema'
 
-export const Heap = (props: {
-    graphData: GraphData
-    forceUpdate: React.Dispatch<{}>
-    tracer: DefaultState['tracer']
-}) => {
-    const depths = React.useMemo(() => {
-        const computeDepths = (
-            ids: string[],
-            heap: { [id: string]: schema.Obj },
-            depths: { [id: string]: number } = {},
-            depth = 0
-        ) => {
-            if (ids.length === 0) return depths
-            const childrenIds = ids.reduce((acc, next, i) => {
-                if (depths[next] !== undefined) return acc
-                depths[next] = depth + i
-                acc.push(
-                    ...heap[next].members
-                        .filter(member => typeof member.value === 'object')
-                        .map(member => (member.value as string[])[0])
-                )
-                return acc
-            }, [] as string[])
-            computeDepths(childrenIds, heap, depths, depth + 1000)
-            return depths
-        }
+export const Heap = (props: { graphData: GraphData; update: React.Dispatch<{}>; tracer: DefaultState['tracer'] }) => {
+    const depths = React.useRef<{ [id: string]: number }[]>([])
+    const index = props.graphData.getIndex()
+    const { stack = [], heap = {} } = props.tracer.steps?.[index].snapshot ?? {}
 
-        return (props.tracer.steps ?? []).reduce((acc, step, i) => {
-            const { stack = [], heap = {} } = step.snapshot ?? {}
-            const stackIds = stack
+    React.useMemo(() => (depths.current = []), [props.tracer.steps])
+
+    const computeDepths = (ids: string[], depths: { [id: string]: number } = {}, depth = 0) => {
+        if (ids.length === 0) return depths
+        const childrenIds = ids.reduce((acc, next, i) => {
+            if (depths[next] !== undefined) return acc
+            depths[next] = depth + i
+            acc.push(
+                ...heap[next].members
+                    .filter(member => typeof member.value === 'object')
+                    .map(member => (member.value as string[])[0])
+            )
+            return acc
+        }, [] as string[])
+        computeDepths(childrenIds, depths, depth + 1000)
+        return depths
+    }
+
+    depths.current[index] ??
+        (depths.current[index] = computeDepths(
+            stack
                 .flatMap(scope => scope.variables)
                 .filter(variable => typeof variable.value === 'object')
                 .map(variable => (variable.value as [string])[0])
-            acc.push(computeDepths(stackIds, heap))
-            return acc
-        }, [] as { [id: string]: number }[])
-    }, [props.tracer.steps])
+        ))
 
     return (
         <>
