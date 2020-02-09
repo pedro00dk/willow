49349d8ddd2b6bf4e25bb4159e871a071926d3e9
Pedro Henrique <pedro00dk@gmail.com>
2020-02-09 15:41:28 -0300

    Disable SvgNode animation when rendered for the first time

--- client/src/components/visualization/graphview/svg/SvgNode.tsx
+++ client/src/components/visualization/graphview/svg/SvgNode.tsx
@@ -13,13 +13,14 @@ const styles = {
 
 export const SvgNode = (props: { id: string; graphData: GraphData; children?: React.ReactNode }) => {
     const container$ = React.useRef<SVGForeignObjectElement>()
+    const firstDraw = React.useRef(true)
     const node = props.graphData.getNode(props.id)
 
     React.useLayoutEffect(() => {
         const updateNode = (callId?: number) => {
             const position = props.graphData.getNodePosition(node)
             const size = props.graphData.getNodeSize(node)
-            container$.current.style.transition = styles.animate(props.graphData.getAnimate())
+            container$.current.style.transition = styles.animate(!firstDraw.current && props.graphData.getAnimate())
             container$.current.setAttribute('x', position.x.toString())
             container$.current.setAttribute('y', position.y.toString())
             container$.current.setAttribute('width', size.x.toString())
@@ -27,8 +28,9 @@ export const SvgNode = (props: { id: string; graphData: GraphData; children?: Re
         }
 
         updateNode()
+        firstDraw.current = false
         props.graphData.subscribe(props.id, updateNode)
-    }, [container$.current, props.id, props.graphData])
+    })
 
     return (
         <>
