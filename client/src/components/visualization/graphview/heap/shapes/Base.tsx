97b2333ed78061afe5cee35778db03c2b38dd71f
Pedro Henrique <pedro00dk@gmail.com>
2020-02-10 22:43:57 -0300

    Mose Base component to shapes folder and remove duplicate functions

---
+++ client/src/components/visualization/graphview/heap/shapes/Base.tsx
@@ -0,0 +1,17 @@
+import cn from 'classnames'
+import { css } from 'emotion'
+import * as React from 'react'
+import { colors } from '../../../../../colors'
+
+const classes = {
+    container: 'd-flex flex-column',
+    title: cn('px-1', css({ fontSize: '0.5rem' })),
+    children: cn('d-flex justify-content-center rounded p-1', css({ background: colors.gray.light }))
+}
+
+export const Base = (props: { title: string; children?: React.ReactNode }) => (
+    <div className={classes.container}>
+        <span className={classes.title}>{props.title}</span>
+        <div className={classes.children}>{props.children}</div>
+    </div>
+)
