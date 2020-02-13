ce5ac56726316b6fb2d85fa2448c72792fd7a0f6
Pedro Henrique <pedro00dk@gmail.com>
2020-02-13 13:24:44 -0300

    Minor rename refactor in SvgNode

--- client/src/components/visualization/graphview/svg/SvgNode.tsx
+++ client/src/components/visualization/graphview/svg/SvgNode.tsx
@@ -13,22 +13,22 @@ const styles = {
 
 export const SvgNode = (props: { id: string; graphData: GraphData; children?: React.ReactNode }) => {
     const container$ = React.useRef<SVGForeignObjectElement>()
-    const firstDraw = React.useRef(true)
+    const firstUpdate = React.useRef(true)
     const node = props.graphData.getNode(props.id)
 
     React.useLayoutEffect(() => {
         const updateNode = (callId?: number) => {
             const position = props.graphData.getNodePosition(node)
             const size = props.graphData.getNodeSize(node)
-            container$.current.style.transition = styles.animate(!firstDraw.current && props.graphData.getAnimate())
+            container$.current.style.transition = styles.animate(!firstUpdate.current && props.graphData.getAnimate())
             container$.current.setAttribute('x', position.x.toString())
             container$.current.setAttribute('y', position.y.toString())
             container$.current.setAttribute('width', size.x.toString())
             container$.current.setAttribute('height', size.y.toString())
+            firstUpdate.current = false
         }
 
         updateNode()
-        firstDraw.current = false
         props.graphData.subscribe(node.id, updateNode)
     })
 
