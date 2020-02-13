550f5163270cbbacdfa7a8290923ee4c95005660
Pedro Henrique <pedro00dk@gmail.com>
2020-02-13 13:44:49 -0300

    Check for new edges individually and use Edge name prop in SvgEdges

--- client/src/components/visualization/graphview/svg/SvgEdges.tsx
+++ client/src/components/visualization/graphview/svg/SvgEdges.tsx
@@ -3,17 +3,18 @@ import { GraphData } from '../GraphData'
 
 export const SvgEdges = (props: { id: string; graphData: GraphData }) => {
     const container$ = React.useRef<SVGGElement>()
-    const firstDraw = React.useRef(true)
     const [edgesLength, setEdgesLength] = React.useState(0)
     const node = props.graphData.getNode(props.id)
     const edges = props.graphData.getEdges(node.id)
+    const sourceEdges = [...edges.children, ...edges.loose]
 
     React.useLayoutEffect(() => {
-        const localEdges = [...edges.children, ...edges.loose]
-        if (edgesLength !== localEdges.length) return setEdgesLength(localEdges.length)
+        const sourceEdges = [...edges.children, ...edges.loose]
+        if (edgesLength !== sourceEdges.length) return setEdgesLength(sourceEdges.length)
 
-        const updateEdges = (callId?: number) => {
-            localEdges.forEach((edge, i) => {
+        const updateEdges = (callId?: number) =>
+            sourceEdges.forEach((edge, i) => {
+                console.log(edge.color)
                 const group = container$.current.children.item(i)
                 const marker = group.children.item(0) as SVGMarkerElement
                 const path = group.children.item(1) as SVGPathElement
@@ -23,8 +24,8 @@ export const SvgEdges = (props: { id: string; graphData: GraphData }) => {
                 const textPath = text.firstElementChild as SVGTextPathElement
                 const data = props.graphData.computeEdgePathData(edge)
                 const currentData = animate.getAttribute('to')
-                if (data === currentData) return
-                animate.setAttribute('dur', !firstDraw.current && props.graphData.getAnimate() ? '0.4s' : '0.001s')
+                const newEdge = currentData == undefined
+                animate.setAttribute('dur', !newEdge && props.graphData.getAnimate() ? '0.4s' : '0.001s')
                 animate.setAttribute('from', currentData)
                 animate.setAttribute('to', data)
                 path.setAttribute('stroke', edge.color)
@@ -34,36 +35,38 @@ export const SvgEdges = (props: { id: string; graphData: GraphData }) => {
                 textPath.textContent = edge.text
                 ;(animate as any).beginElement()
             })
-        }
 
         updateEdges()
-        firstDraw.current = false
         Object.keys(props.graphData.getNodeChildren(node, 1)).forEach(id => props.graphData.subscribe(id, updateEdges))
     })
 
     return (
         <g ref={container$}>
-            {[...Array(edgesLength).keys()].map(i => (
-                <g key={i}>
-                    <marker
-                        id={`${props.id}-${i}-marker`}
-                        markerWidth={10}
-                        markerHeight={6}
-                        refX={10}
-                        refY={3}
-                        orient='auto'
-                        markerUnits='userSpaceOnUse'
-                    >
-                        <polyline fill='none' points='0 0, 10 3, 0 6' />
-                    </marker>
-                    <path id={`${props.id}-${i}`} markerEnd={`url(#${props.id}-${i}-marker)`} fill='none'>
-                        <animate attributeName='d' begin='indefinite' fill='freeze' />
-                    </path>
-                    <text style={{ userSelect: 'none' }} fontSize='0.75rem'>
-                        <textPath xlinkHref={`#${props.id}-${i}`} startOffset='50%' textAnchor='middle' />
-                    </text>
-                </g>
-            ))}
+            {sourceEdges.map(edge => {
+                const pathId = `${edge.id}-${edge.name}`
+                const markerId = `${pathId}-marker`
+                return (
+                    <g key={edge.name}>
+                        <marker
+                            id={markerId}
+                            markerWidth={10}
+                            markerHeight={6}
+                            refX={10}
+                            refY={3}
+                            orient='auto'
+                            markerUnits='userSpaceOnUse'
+                        >
+                            <polyline fill='none' points='0 0, 10 3, 0 6' />
+                        </marker>
+                        <path id={pathId} markerEnd={`url(#${markerId}`} fill='none'>
+                            <animate attributeName='d' begin='indefinite' fill='freeze' />
+                        </path>
+                        <text style={{ userSelect: 'none' }} fontSize='0.75rem'>
+                            <textPath xlinkHref={`#${pathId}`} startOffset='50%' textAnchor='middle' />
+                        </text>
+                    </g>
+                )
+            })}
         </g>
     )
 }
