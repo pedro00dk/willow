41d34823ec0d374286ee436a7c8a8369bda1de4c
Pedro Henrique <pedro00dk@gmail.com>
2020-02-16 18:12:40 -0300

    Create the options reducer to store trace and visualization options

---
+++ client/src/reducers/options.ts
@@ -0,0 +1,36 @@
+/**
+ * Input reducer updates some tracing and visualization options.
+ */
+type State = { liveProgramming: boolean; preserveLayout: boolean }
+
+type Action =
+    | { type: 'options/setLiveProgramming'; payload: boolean }
+    | { type: 'options/setPreserveLayout'; payload: boolean }
+
+const initialState: State = {
+    liveProgramming: false,
+    preserveLayout: false
+}
+
+export const reducer = (state: State = initialState, action: Action): State => {
+    switch (action.type) {
+        case 'options/setLiveProgramming':
+            return { ...state, liveProgramming: action.payload }
+        case 'options/setPreserveLayout':
+            return { ...state, preserveLayout: action.payload }
+        default:
+            return state
+    }
+}
+
+const setLiveProgramming = (liveProgramming: boolean): Action => ({
+    type: 'options/setLiveProgramming',
+    payload: liveProgramming
+})
+
+const setPreserveLayout = (preserveLayout: boolean): Action => ({
+    type: 'options/setPreserveLayout',
+    payload: preserveLayout
+})
+
+export const actions = { setLiveProgramming, setPreserveLayout }
