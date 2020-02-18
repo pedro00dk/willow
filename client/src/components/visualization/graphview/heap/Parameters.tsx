d90b30d8094529af677a2a5e1e90c8fc4d1b3740
Pedro Henrique <pedro00dk@gmail.com>
2020-02-18 19:51:16 -0300

    Change Parameters reset label

--- client/src/components/visualization/graphview/heap/Parameters.tsx
+++ client/src/components/visualization/graphview/heap/Parameters.tsx
@@ -56,7 +56,7 @@ export const Parameters = <T extends UnknownParameters, U extends DefaultParamet
         <>
             {props.withReset && (
                 <Item onClick={args => props.onChange(readParameters(undefined, props.defaults))}>
-                    <span>{'reset parameters'}</span>
+                    <span>{'reset'}</span>
                 </Item>
             )}
             {Object.entries(parameters).map(([name, value]) =>
@@ -84,7 +84,7 @@ export const Parameters = <T extends UnknownParameters, U extends DefaultParamet
                         onChange={value => props.onChange({ ...parameters, [name]: value })}
                     />
                 ) : (
-                    <span>{'unknown property'}</span>
+                    <span>{'unknown  parameter type'}</span>
                 )
             )}
         </>
