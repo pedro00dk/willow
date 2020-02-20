40c214d99df28448c0147787c456d0df9a8cb1ad
Pedro Henrique <pedro00dk@gmail.com>
2020-02-20 18:13:33 -0300

    Refactor and move node initialization task to Heap

--- client/src/components/visualization/graphview/heap/Heap.tsx
+++ client/src/components/visualization/graphview/heap/Heap.tsx
@@ -1,47 +1,49 @@
 import React from 'react'
 import { DefaultState } from '../../../../reducers/Store'
 import { GraphData } from '../GraphData'
+import { isValueObject } from '../SchemaUtils'
 import { SvgNode } from '../svg/SvgNode'
 import { Obj } from './Obj'
 
-export const Heap = (props: { graphData: GraphData; update: React.Dispatch<{}>; tracer: DefaultState['tracer'] }) => {
+export const Heap = (props: { tracer: DefaultState['tracer']; graphData: GraphData; update: React.Dispatch<{}> }) => {
     const depths = React.useRef<{ [id: string]: number }[]>([])
-    const index = props.graphData.getIndex()
-    const { stack = [], heap = {} } = props.tracer.steps?.[index].snapshot ?? {}
+    const available = props.tracer.available
+    const index = props.tracer.index
+    const { stack = [], heap = {} } = (available && props.tracer.steps[index].snapshot) || {}
 
-    React.useMemo(() => (depths.current = []), [props.tracer.steps])
+    React.useMemo(() => (depths.current = []), [available])
 
-    const computeDepths = (ids: string[], depths: { [id: string]: number } = {}, depth = 0) => {
+    const computeDepths = (ids: string[], depth = 0, depths: { [id: string]: number } = {}): typeof depths => {
         if (ids.length === 0) return depths
-        const childrenIds = ids.reduce((acc, next, i) => {
-            if (depths[next] != undefined) return acc
-            depths[next] = depth + i
-            acc.push(
-                ...heap[next].members
-                    .filter(member => typeof member.value === 'object')
-                    .map(member => (member.value as string[])[0])
-            )
+        const childrenIds = ids.reduce((acc, id, i) => {
+            if (depths[id] != undefined) return acc
+            depths[id] = depth + i
+            heap[id].members.forEach(member => isValueObject(member.value) && acc.push(member.value[0]))
             return acc
         }, [] as string[])
-        computeDepths(childrenIds, depths, depth + 1000)
-        return depths
+        return computeDepths(childrenIds, depth + 1000, depths)
     }
 
-    depths.current[index] ??
-        (depths.current[index] = computeDepths(
-            stack
-                .flatMap(scope => scope.variables)
-                .filter(variable => typeof variable.value === 'object')
-                .map(variable => (variable.value as [string])[0])
-        ))
+    if (!depths.current[index]) {
+        const ids = stack
+            .flatMap(scope => scope.variables)
+            .filter(variable => isValueObject(variable.value))
+            .map(variable => (variable.value as [string])[0])
+        depths.current[index] = computeDepths(ids)
+    }
 
     return (
         <>
-            {Object.keys(heap).map(id => (
-                <SvgNode key={id} id={id} graphData={props.graphData}>
-                    <Obj id={id} depth={depths.current[index][id]} {...props} />
-                </SvgNode>
-            ))}
+            {Object.entries(heap).map(([id, obj]) => {
+                const node = props.graphData.getNode(id)
+                node.type = obj.lType
+                node.depth = depths.current[index][id]
+                return (
+                    <SvgNode key={id} id={id} graphData={props.graphData}>
+                        <Obj id={id} obj={obj} node={node} graphData={props.graphData} update={props.update} />
+                    </SvgNode>
+                )
+            })}
         </>
     )
 }
