ca8bfd118bdc3b02b75b8f21b1fa6e47c6b7a4c5
Pedro Henrique <pedro00dk@gmail.com>
2020-02-20 22:35:00 -0300

    Major refactor and modifications in Obj component

--- client/src/components/visualization/graphview/heap/Obj.tsx
+++ client/src/components/visualization/graphview/heap/Obj.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import 'react-contexify/dist/ReactContexify.min.css'
 import { Item, Menu, MenuProvider, Separator, Submenu } from 'react-contexify'
-import { DefaultState } from '../../../../reducers/Store'
 import * as schema from '../../../../schema/schema'
 import { Draggable } from '../../../utils/Draggable'
-import { Edge, GraphData, layoutParameters, svgScreenTransformVector, UnknownParameters } from '../GraphData'
+import { Edge, GraphData, layoutParameters, Node, svgScreenTransformVector } from '../GraphData'
 import { getMemberName } from '../SchemaUtils'
+import { Parameters } from './Parameters'
 import * as ArrayModule from './shapes/Array'
 import * as ColumnModule from './shapes/Column'
 import * as FieldModule from './shapes/Field'
 import * as MapModule from './shapes/Map'
-import { Parameters } from './Parameters'
+
+const classes = {
+    container: 'd-flex position-absolute',
+    menuProvider: 'd-flex'
+}
 
 const modules = {
     array: ArrayModule,
@@ -19,35 +23,27 @@ const modules = {
     map: MapModule
 }
 
-const classes = {
-    container: 'd-flex position-absolute',
-    menuProvider: 'd-flex'
-}
-
 export const Obj = (props: {
     id: string
-    depth: number
+    obj: schema.Obj
+    node: Node
     graphData: GraphData
     update: React.Dispatch<{}>
-    tracer: DefaultState['tracer']
 }) => {
     const container$ = React.useRef<HTMLDivElement>()
+    const references = React.useRef<{ id: string; name: string; ref$: HTMLSpanElement; edge: Partial<Edge> }[]>()
+    references.current = []
     const previousIndex = React.useRef(undefined)
     const previousMembers = React.useRef<{ [id: string]: schema.Member }>({})
-    const links = React.useRef<({ id: string; name: string; ref$: HTMLSpanElement } & Partial<Edge>)[]>()
-    links.current = []
 
-    const id = props.id
-    const index = props.graphData.getIndex()
-    const obj = props.tracer.steps[index].snapshot.heap[id]
-    const node = props.graphData.getNode(id)
-    node.depth = props.depth
-    node.type = obj.lType
-    const defaultShape = Object.entries(modules)
-        .filter(([, mod]) => mod.defaults.has(obj.gType))
-        .map(([name]) => name)[0]
-    const shape = props.graphData.getNodeShape(node) ?? props.graphData.setNodeShape(node, defaultShape)
-    const parameters = props.graphData.getNodeParameters(node)
+    const defaultShape = React.useMemo(() => {
+        return Object.entries(modules).reduce(
+            (acc, [name, mod]) => acc ?? (mod.defaults.has(props.obj.gType) ? name : acc),
+            undefined as string
+        )
+    }, [props.obj.gType])
+    const shape = props.graphData.getNodeShape(props.node) ?? defaultShape
+    const parameters = props.graphData.getNodeParameters(props.node)
 
     const { Shape } = modules[shape as keyof typeof modules]
 
@@ -56,34 +52,28 @@ export const Obj = (props: {
         const rect = container$.current.getBoundingClientRect()
         const screenSize = { x: rect.width, y: rect.height }
         const [svgSize] = svgScreenTransformVector('toSvg', svg, true, screenSize)
-        node.size = svgSize
+        props.node.size = svgSize
     })
 
     React.useLayoutEffect(() => {
         const svg = container$.current.closest('svg')
         const rect = container$.current.getBoundingClientRect()
-        links.current.forEach(({ id: target, name, ref$, ...data }) => {
+        references.current.forEach(({ id: target, name, ref$, edge }) => {
             const refRect = ref$.getBoundingClientRect()
             const screenDelta = { x: refRect.left - rect.left, y: refRect.top - rect.top }
             const screenSize = { x: refRect.width, y: refRect.height }
             const [svgDelta, svgSize] = svgScreenTransformVector('toSvg', svg, true, screenDelta, screenSize)
             const delta = { x: svgDelta.x + svgSize.x / 2, y: svgDelta.y + svgSize.y / 2 }
-            props.graphData.pushEdge(id, name, { ...data, self: true, from: { delta }, target, to: { mode: 'near' } })
+            props.graphData.pushEdge(props.node.id, name, { ...edge, from: { delta }, target, to: { mode: 'near' } })
         })
     })
 
-    React.useLayoutEffect(() => props.graphData.callSubscriptions(id))
+    React.useLayoutEffect(() => props.graphData.callSubscriptions(props.node.id))
 
     React.useEffect(() => {
-        // TODO check this implementation
-        // console.log('check members', previousIndex.current, previousMembers.current)
-        if (previousIndex.current === index) return
-        previousIndex.current = index
-        previousMembers.current = obj.members.reduce((acc, member) => {
-            acc[getMemberName(member)] = member
-            return acc
-        }, {} as { [name: string]: schema.Member })
-        // console.log('update members', previousIndex.current, previousMembers.current)
+        if (props.graphData.getIndex() === previousIndex.current) return
+        previousIndex.current = props.graphData.getIndex()
+        previousMembers.current = Object.fromEntries(props.obj.members.map(member => [getMemberName(member), member]))
     })
 
     return (
@@ -92,31 +82,38 @@ export const Obj = (props: {
                 ref: container$,
                 className: classes.container,
                 onDoubleClick: event => {
-                    const dir = !event.altKey ? 'horizontal' : 'vertical'
+                    const direction = !event.altKey ? 'horizontal' : 'vertical'
                     const mode = !event.ctrlKey ? 'available' : !event.shiftKey ? 'override' : 'all'
-                    const structure = props.graphData.applyStructureLayout(node, dir, undefined, true, index, mode)
+                    const structure = props.graphData.applyStructureLayout(
+                        props.node,
+                        direction,
+                        undefined,
+                        undefined,
+                        undefined,
+                        mode
+                    )
                     props.graphData.setAnimate(true)
-                    Object.keys(structure.members).forEach(id => props.graphData.callSubscriptions(id))
+                    Object.values(structure.members).forEach(node => props.graphData.callSubscriptions(node.id))
                 }
             }}
-            onDrag={(delta, event) => {
+            onDrag={(event, delta) => {
                 const svg = container$.current.closest('svg')
                 const [svgDelta] = svgScreenTransformVector('toSvg', svg, false, delta)
                 const depth = event.altKey ? Infinity : 0
                 const mode = !event.ctrlKey ? 'available' : !event.shiftKey ? 'override' : 'all'
-                const movedNodes = props.graphData.moveNodePositions(node, depth, svgDelta, index, mode)
+                const movedNodes = props.graphData.moveNodePositions(props.node, depth, svgDelta, undefined, mode)
                 props.graphData.setAnimate(false)
-                Object.keys(movedNodes).forEach(id => props.graphData.callSubscriptions(id))
+                Object.values(movedNodes).forEach(node => props.graphData.callSubscriptions(node.id))
             }}
         >
-            <MenuProvider id={id} className={classes.menuProvider}>
+            <MenuProvider id={props.node.id} className={classes.menuProvider}>
                 <div className={classes.menuProvider}>
                     <Shape
-                        id={id}
-                        obj={obj}
-                        previousMembers={previousMembers.current}
+                        id={props.node.id}
+                        obj={props.obj}
                         parameters={parameters}
-                        onLink={link => links.current.push(link)}
+                        previousMembers={previousMembers.current}
+                        onReference={reference => references.current.push(reference)}
                     />
                 </div>
             </MenuProvider>
@@ -127,56 +124,65 @@ export const Obj = (props: {
 
 const ObjMenu = (props: {
     id: string
+    obj: schema.Obj
+    node: Node
     graphData: GraphData
     update: React.Dispatch<{}>
-    tracer: DefaultState['tracer']
 }) => {
-    const id = props.id
-    const index = props.graphData.getIndex()
-    const obj = props.tracer.steps[index].snapshot.heap[id]
-    const node = props.graphData.getNode(id)
-    const supportedShapes = Object.entries(modules)
-        .filter(([, mod]) => mod.supported.has(obj.gType))
-        .map(([name]) => name)
-    const shape = props.graphData.getNodeShape(node)
-    const parameters = props.graphData.getNodeParameters(node)
+    const [defaultShape, supportedShapes] = React.useMemo(() => {
+        return [
+            Object.entries(modules).reduce(
+                (acc, [name, mod]) => acc ?? (mod.defaults.has(props.obj.gType) ? name : acc),
+                undefined as string
+            ),
+            Object.entries(modules).reduce(
+                (acc, [name, mod]) => (mod.supported.has(props.obj.gType) && acc.push(name), acc),
+                [] as string[]
+            )
+        ]
+    }, [props.obj.gType])
+    const shape = props.graphData.getNodeShape(props.node) ?? defaultShape
+    const parameters = props.graphData.getNodeParameters(props.node)
 
-    const { ShapeParameters } = modules[shape as keyof typeof modules]
+    const { defaultParameters } = modules[shape as keyof typeof modules]
 
     return (
-        <Menu id={id}>
-            <Item onClick={args => ((node.mode = node.mode === 'own' ? 'type' : 'own'), props.update({}))}>
-                <span title='click to change'>{`using ${node.mode} parameters`}</span>
+        <Menu id={props.node.id}>
+            <Item onClick={args => ((props.node.mode = props.node.mode === 'own' ? 'type' : 'own'), props.update({}))}>
+                <span title='click to change'>{`using ${props.node.mode} parameters`}</span>
             </Item>
             <Separator />
             <Submenu label='shape'>
-                <Item onClick={args => (props.graphData.setNodeShape(node, ''), props.update({}))}>{'reset'}</Item>
+                <Item onClick={args => (props.graphData.setNodeShape(props.node, undefined), props.update({}))}>
+                    {'reset'}
+                </Item>
                 {supportedShapes.map((shape, i) => (
-                    <Item key={i} onClick={args => (props.graphData.setNodeShape(node, shape), props.update({}))}>
+                    <Item key={i} onClick={args => (props.graphData.setNodeShape(props.node, shape), props.update({}))}>
                         {shape}
                     </Item>
                 ))}
             </Submenu>
             <Separator />
             <Submenu label='parameters'>
-                <ShapeParameters
-                    id={id}
-                    obj={obj}
+                <Parameters
                     withReset
                     parameters={parameters}
-                    onChange={(parameters: UnknownParameters) => (
-                        props.graphData.setNodeParameters(node, parameters), props.update({})
+                    defaults={defaultParameters}
+                    obj={props.obj}
+                    onChange={parameters => (
+                        props.graphData.setNodeParameters(props.node, parameters), props.update({})
                     )}
                 />
             </Submenu>
             <Separator />
             <Submenu label='layout'>
                 <Parameters
-                    defaults={layoutParameters}
-                    parameters={node.layout}
                     withReset
-                    onChange={parameters => ((node.layout = parameters), props.update)}
-                ></Parameters>
+                    parameters={props.node.layout}
+                    defaults={layoutParameters}
+                    obj={props.obj}
+                    onChange={parameters => ((props.node.layout = parameters), props.update({}))}
+                />
             </Submenu>
         </Menu>
     )
