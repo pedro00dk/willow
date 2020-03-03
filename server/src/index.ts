2d67d5ee2e4bae90812f2611f2ccbe9b403edbe6
Pedro Henrique <pedro00dk@gmail.com>
2020-03-03 20:11:23 -0300

    Add new cli parameters in index.js

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,57 @@
 import yargs from 'yargs'
-import { Server } from './server'
+import { createServer } from './server'
 
 const main = () => {
     const parser = yargs
         .usage('Willow API for CLI Tracers')
         .alias('h', 'help')
         .hide('version')
-        .option('tracer', { array: true, description: 'Tracer <language> <command>', nargs: 2, string: true })
-        .option('steps', { default: 1000, description: 'Maximum number of allowed steps of a program' })
-        .option('timeout', { default: 8000, description: 'Maximum tracer run time (milliseconds)' })
-        .option('client', { default: '*', description: 'Client origin (enable CORS)' })
         .option('port', { default: 8000, description: 'Set the server port' })
-        .option('verbose', { type: 'boolean', description: 'Log traces calls and results' })
+        .option('tracer-command', { array: true, string: true, nargs: 2, description: 'Tracer <language> <command>' })
+        .option('tracer-steps', { default: 1000, description: 'Maximum number of allowed steps of a program' })
+        .option('tracer-timeout', { default: 8000, description: 'Maximum tracer run time (milliseconds)' })
+        .option('signin-steps', { type: 'number', description: 'Override --tracer-steps for signed users' })
+        .option('signin-timeout', { type: 'number', description: 'Override --tracer-timeout for signed users' })
+        .option('auth-enable', { default: false, description: 'Enable OAuth authentication routes' })
+        .option('auth-google-client-id', { type: 'string', description: 'Google OAuth client id' })
+        .option('auth-google-client-secret', { type: 'string', description: 'Google Oauth client secret' })
+        .option('auth-google-callback-uri', { type: 'string', description: 'Google Oauth client secret' })
+        .option('auth-cookie-key', { default: 'cookie-key', description: 'Key to encrypt authorization cookie' })
+        .option('cors-client', { type: 'string', description: 'CORS for client (set exact address to allow auth)' })
+        .option('verbose', { default: false, description: 'Log traces calls and results' })
 
     const options = parser.argv
-    const tracers = [...Array(options.tracer ? options.tracer.length / 2 : 0)]
-        .map((_, i) => [options.tracer[i * 2], options.tracer[i * 2 + 1]] as const)
-        .reduce((acc, [language, command]) => ({ ...acc, [language]: command }), {} as { [language: string]: string })
 
-    console.log(main.name, 'cli', { ...options, tracers })
-    new Server(tracers, options.steps, options.timeout, options.client, options.port, options.verbose).listen()
+    const port = options.port
+    const commands: { [language: string]: string } = Object.fromEntries(
+        options['tracer-command'].reduce((acc, languageOrCommand, i) => {
+            if (i % 2 === 0) acc.push([languageOrCommand, undefined])
+            else acc[acc.length - 1][1] = languageOrCommand
+            return acc
+        }, [] as [string, string][])
+    )
+    const tracers = {
+        commands,
+        steps: options['tracer-steps'],
+        timeout: options['tracer-timeout']
+    }
+    const signed = {
+        steps: options['signin-steps'],
+        timeout: options['signin-timeout']
+    }
+    const credentials = options['auth-enable'] && {
+        clientID: options['auth-google-client-id'],
+        clientSecret: options['auth-google-client-secret'],
+        callbackURL: options['auth-google-callback-uri']
+    }
+    const cookieKey = options['auth-cookie-key']
+    const corsClient = options['cors-client']
+    const verbose = options.verbose
+
+    console.log({ tracers, signed, credentials, cookieKey, corsClient, verbose })
+    const server = createServer(tracers, signed, credentials, cookieKey, corsClient, verbose)
+    console.log('Server running at', port)
+    server.listen(port)
 }
 
 main()
