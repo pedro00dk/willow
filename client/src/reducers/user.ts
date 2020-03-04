09be1e49fe88661c0f8e918a3e832ab8a66b0907
Pedro Henrique <pedro00dk@gmail.com>
2020-03-04 19:05:26 -0300

    Impelement first client user actions

--- client/src/reducers/user.ts
+++ client/src/reducers/user.ts
@@ -1,3 +1,5 @@
+import { api, apiUrl } from '../api'
+import { DefaultAsyncAction } from './Store'
 /**
  * User reducer updates the user information and collect user actions.
  */
@@ -16,9 +18,10 @@ type State = {
 }
 
 type Action =
-    | { type: 'user/signIn'; payload: string }
-    | { type: 'user/signOut' }
-    | { type: 'user/action'; payload: State['actions'][0] }
+    | { type: 'user/signin' }
+    | { type: 'user/signout' }
+    | { type: 'user/fetch'; payload?: string; error?: string }
+    | { type: 'user/action'; payload?: State['actions'][0]; error?: string }
 
 const initialState: State = {
     fetching: false,
@@ -29,13 +32,37 @@ const initialState: State = {
 
 export const reducer = (state: State = initialState, action: Action): State => {
     switch (action.type) {
-        case 'user/signIn':
-        case 'user/signOut':
+        case 'user/signin':
+        case 'user/signout':
+            return state
+        case 'user/fetch':
+            return action.payload
+                ? { ...state, fetching: false, email: action.payload }
+                : action.error != undefined
+                ? { ...initialState, error: action.error }
+                : { ...initialState, fetching: true }
         case 'user/action':
-
         default:
             return state
     }
 }
 
-export const actions = {}
+const signin = (): DefaultAsyncAction => async () => {
+    window.location.href = `${apiUrl}/api/auth/signin`
+}
+
+const signout = (): DefaultAsyncAction => async () => {
+    await api.get('/api/auth/signout')
+}
+
+const fetch = (): DefaultAsyncAction => async dispatch => {
+    dispatch({ type: 'user/fetch' })
+    try {
+        const user = (await api.get('/api/auth/user')).data as { email: string }
+        dispatch({ type: 'user/fetch', payload: user.email })
+    } catch (error) {
+        dispatch({ type: 'user/fetch', error: error?.response?.data ?? error.toString() })
+    }
+}
+
+export const actions = { signin, fetch }
