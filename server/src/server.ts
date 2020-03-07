8b046844dec6a613f0af143d2a646287491a0d8f
Pedro Henrique <pedro00dk@gmail.com>
2020-03-07 15:46:54 -0300

    Reflect previous modifications of database and authentication in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,38 +1,48 @@
 import cors from 'cors'
 import express from 'express'
-import { connectDatabase, getUser } from './database'
-import { createHandlers as createAuthHandlers } from './routes/auth'
+import { Profile } from 'passport'
+import { connectDatabaseClient, getUser } from './database'
+import { createHandlers as createAuthenticationHandlers } from './routes/authentication'
 import { createHandlers as createTracerHandlers } from './routes/tracer'
+import { User } from './user'
 
 /**
- * Creates the server instance and all available handlers and routers, then return the server listen function.
+ * Creates the server instance and all available handlers and routers, then return the server.
  *
- * @param tracers.commands dictionary with languages and commands to spawn their tracers
+ * @param tracers.commands languages and commands to spawn their tracers
  * @param tracers.steps maximum number of steps a tracer is allowed to run
  * @param tracers.timeout maximum time in milliseconds a tracer is allowed to run
- * @param signed.steps override steps for signed users
- * @param signed.timeout override timeout for signed users
- * @param credentials.clientID google oauth credential client id
- * @param credentials.clientSecret google oauth credential client secret
- * @param credentials.callbackURL one of the authorized redirect uris enabled in the google oauth credential
- *                                (it can be relative if server_address + router_path + callbackURL matches the one of
- *                                the redirect uris and the /success route provided by this router)
- * @param credentials.databaseConnection connection string to a mongo database
- * @param cookieKey key for session cookie encryption
- * @param corsWhitelist set of client addresses to allow cors ('*' allow any address)
- * @param verbose enable verbose output (prints traces and results)
+ * @param signed.steps override tracers.steps for signed users
+ * @param signed.timeout override tracers.timeout for signed users
+ * @param authentication if set, enable google oauth routes for user signin
+ * @param authentication.clientId google oauth client id
+ * @param authentication.clientSecret google oauth client secret
+ * @param database if set, enable user storage (requires authentication)
+ * @param database.uri connection uri to mongo database
+ * @param database.name the mongo database name
+ * @param corsWhitelist allow cors clients ('*' all clients)
+ * @param verbose increase log output
  */
 export const createServer = async (
     tracers: { commands: { [language: string]: string }; steps: number; timeout: number },
     signed: { steps: number; timeout: number },
-    credentials: { clientID: string; clientSecret: string; callbackURL: string; databaseConnection: string },
-    cookieKey: string,
+    authentication: { clientId: string; clientSecret: string },
+    database: { uri: string; name: string },
     corsWhitelist: Set<string>,
     verbose: boolean
 ) => {
     const server = express()
     server.use(express.json())
-    if (corsWhitelist != undefined)
+    // server.use((req, res, next) => {
+    //     const origin = req.headers.origin as string
+    //     if (corsWhitelist.size === 0 || origin == undefined) return next()
+    //     const corsOrigin = corsWhitelist.has(origin) ? origin : corsWhitelist.has('*') ? '*' : ''
+    //     res.setHeader('Control-Access-Allow-Origin', corsOrigin)
+    //     res.setHeader('Control-Access-Allow-Credentials', 'true')
+    //     if (req.method !== 'OPTIONS') return next()
+    //     // preflight request
+    // })
+    if (corsWhitelist.size > 0)
         server.use(
             cors({
                 origin: (origin, callback) => {
@@ -44,26 +54,29 @@ export const createServer = async (
         )
 
     const apiRouter = express.Router()
+    if (authentication) {
+        if (database) await connectDatabaseClient(database.uri, database.name)
 
-    if (credentials) {
-        await connectDatabase(credentials.databaseConnection)
+        const getUserFromProfile = async (profile: Profile) => {
+            const user = { id: profile.id, name: profile.displayName, email: profile.emails[0].value }
+            return database ? await getUser(user, true) : user
+        }
+        const serializeUser = async (user: User) => (database ? (await getUser(user)).id : JSON.stringify(user))
+        const deserializeUser = async (id: string) => (database ? await getUser(id) : JSON.parse(id))
 
-        const { handlers: authHandlers, router: authRouter } = createAuthHandlers(
-            credentials,
-            cookieKey,
-            async profile => await getUser(profile.id, profile.displayName, profile.emails[0].value),
-            async user => (await getUser(user.id)).id,
-            async id => await getUser(id)
+        const { handlers: authenticationHandlers, router: authenticationRouter } = createAuthenticationHandlers(
+            authentication,
+            '/api/authentication/callback',
+            getUserFromProfile,
+            serializeUser,
+            deserializeUser
         )
-        authHandlers.forEach(handler => server.use(handler))
-        apiRouter.use('/auth', authRouter)
+        authenticationHandlers.forEach(handler => server.use(handler))
+        apiRouter.use('/authentication', authenticationRouter)
     }
-
     const { handlers: tracerHandlers, router: tracerRouter } = createTracerHandlers(tracers, signed, verbose)
     tracerHandlers.forEach(handler => server.use(handler))
     apiRouter.use('/tracer', tracerRouter)
-
     server.use('/api', apiRouter)
-
     return server
 }
