3377ef1251bac5a6412b6e1500b357e7b07ebdb2
Pedro Henrique <pedro00dk@gmail.com>
2020-03-07 15:28:23 -0300

    Use the mongodb driver directly rather than mongoose

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,24 +1,39 @@
-import mongoose from 'mongoose'
+import mongodb from 'mongodb'
+import { User } from './user'
 
-const actionSchema = new mongoose.Schema({
-    date: Date,
-    name: String,
-    payload: String
-})
+/**
+ * Mongo client and database.
+ */
+const mongo = {
+    client: undefined as mongodb.MongoClient,
+    db: undefined as mongodb.Db
+}
 
-const userSchema = new mongoose.Schema(
-    {
-        id: { type: String },
-        name: { type: String },
-        email: { type: String },
-        actions: [actionSchema]
-    },
-    { id: false }
-)
+/**
+ * Connect to a mongo database.
+ * Check parameters documentation in server.ts.
+ */
+export const connectDatabaseClient = async (uri: string, name: string) => {
+    if (mongo.client?.isConnected()) {
+        console.log('database', 'closing previous connection')
+        await mongo.client.close()
+    }
+    console.log('database', `connecting to ${uri}`, `db ${name}`)
+    mongo.client = await mongodb.MongoClient.connect(uri, { useUnifiedTopology: true })
+    mongo.db = mongo.client.db(name)
+}
 
-export const User = mongoose.model('user', userSchema)
-
-export const connectDatabase = (database: string) => mongoose.connect(database)
-
-export const getUser = async (id: string, name?: string, email?: string) =>
-    (await User.findOne({ id: id })) ?? (await new User({ id, name, email, actions: [] }).save())
+/**
+ * Get the user from the database which has the same id. If it does not exist, create a new one and return it.
+ * @param user user object
+ */
+export const getUser = async (user: User, create = false) => {
+    if (!mongo.client.isConnected()) throw Error('Mongo database is not connected')
+    const usersCollection = mongo.db.collection<User>('users')
+    let databaseUser = await usersCollection.findOne({ id: user.id })
+    if (!databaseUser && create) {
+        await usersCollection.insertOne(user)
+        databaseUser = await usersCollection.findOne({ id: user.id })
+    }
+    return databaseUser
+}
