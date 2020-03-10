3418c17b219a9b7b16f7a3268b58a07d8cdeece6
Pedro Henrique <pedro00dk@gmail.com>
2020-03-09 22:01:13 -0300

    Use synthetic import in api module

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,9 +1,9 @@
 /**
  * Save the api uri to make requests.
  */
-import * as axios from 'axios'
+import axios from 'axios'
 
 // environment variables captured when building by parcel using dotenv
-const apiUrl = process.env['API']
+const apiUri = process.env['API']
 
-export const api = axios.default.create({ baseURL: apiUrl, withCredentials: true })
+export const api = axios.create({ baseURL: apiUri, withCredentials: true })
