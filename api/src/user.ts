63cfefd766ffa4bd30557089b2cd06e7cdbe79a9
Pedro Henrique <pedro00dk@gmail.com>
2020-03-11 12:53:31 -0300

    Extract action type from user Actions

--- api/src/user.ts
+++ api/src/user.ts
@@ -8,13 +8,18 @@ export type User = {
 }
 
 /**
+ * Any relevant action executed by the user.
+ */
+export type Action = {
+    name: string
+    date: Date
+    payload: any
+}
+
+/**
  * List of actions executed by the user.
  */
 export type Actions = {
     id: User['id']
-    actions: {
-        date: Date
-        name: string
-        payload: any
-    }[]
+    actions: Action[]
 }
