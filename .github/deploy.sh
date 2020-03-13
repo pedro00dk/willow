2a4dec62c80f8a9a9f5d280cadda1f105ad92f06
Pedro Henrique <pedro00dk@gmail.com>
2020-03-13 14:30:17 -0300

    Update deploy scripts

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -3,11 +3,10 @@
 USERNAME=${1}
 TOKEN=${2}
 REPOSITORY=${3}
-AUTHENTICATION_CLIENT_ID=${4}
-AUTHENTICATION_CLIENT_SECRET=${5}
+AUTH_CLIENT_ID=${4}
+AUTH_CLIENT_SECRET=${5}
 API_URL=${6}
-CLIENT_URL=${7}
-DATABASE_URL=${8}
+DB_URL=${7}
 
 set -v
 
@@ -15,7 +14,6 @@ set -v
 docker container ls --all
 docker container stop $(docker container ls --quiet) || true
 docker container rm $(docker container ls --all --quiet) || true
-
 
 # pull github build images
 docker login --username ${USERNAME} --password ${TOKEN} docker.pkg.github.com
@@ -29,25 +27,23 @@ docker image prune --force
 docker network create willow-network || true
 
 # commands for starting the tracers in containers
-JAVA_TRACER_COMMAND="docker run --rm -i docker.pkg.github.com/${REPOSITORY}/willow-tracer-java --silent"
-PYTHON_TRACER_COMMAND="docker run --rm -i docker.pkg.github.com/${REPOSITORY}/willow-tracer-python --silent"
+JAVA_TRACER="docker run --rm -i docker.pkg.github.com/${REPOSITORY}/willow-tracer-java --silent"
+PYTHON_TRACER="docker run --rm -i docker.pkg.github.com/${REPOSITORY}/willow-tracer-python --silent"
 
 # start api server
-sudo docker container run --name willow-api \
+sudo docker container run \
     --rm --detach --network willow-network --publish 443:8000 \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     docker.pkg.github.com/${REPOSITORY}/willow-api \
     -- \
-    --port 8000 \
-    --tracer-command python "${PYTHON_TRACER_COMMAND}" \ # --tracer-command java "${JAVA_TRACER_COMMAND}" \
-    --tracer-steps 500 \
-    --signed-steps 1000 \
-    --authentication-enable \
-    --authentication-client-id "${AUTHENTICATION_CLIENT_ID}" \
-    --authentication-client-secret "${AUTHENTICATION_CLIENT_SECRET}" \
-    --database-enable \
-    --database-url "${DATABASE_URL}" \
-    --database-name 'willow'
+    --tracer python "${PYTHON_TRACER}" \ # --tracer java "${JAVA_TRACER}" \
+    --auth \
+    --auth-client-id "${AUTH_CLIENT_ID}" \
+    --auth-client-secret "${AUTH_CLIENT_SECRET}" \
+    --db \
+    --db-url "${DATABASE_URL}" \
+    --db-name 'willow' \
+    --port 8000
 
 sudo docker container run --name willow-client \
     --rm --detach --network willow-network --publish 80:8000 \
