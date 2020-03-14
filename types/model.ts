80d5acc2181ed465b7765346369a584666915e9d
Pedro Henrique <pedro00dk@gmail.com>
2020-03-14 17:14:26 -0300

    Create the new types folder containing project shared types

---
+++ types/model.ts
@@ -0,0 +1,54 @@
+import * as tracer from './tracer'
+
+/**
+ * User entity.
+ */
+export type User = {
+    id: string
+    name: string
+    email: string
+    admin: boolean
+}
+
+/**
+ * List of actions executed by a user.
+ */
+export type Actions = {
+    id: User['id']
+    actions: Action[]
+}
+
+/**
+ * Any relevant action executed by a user.
+ */
+export type Action = {
+    name: string
+    date: Date
+    payload: any
+}
+
+/**
+ * List of programs saved by a user.
+ */
+export type Programs = {
+    id: User['id']
+    programs: Program[]
+}
+
+/**
+ * Program data saved by a user.
+ */
+export type Program = {
+    name: string
+    language: string
+    request: Pick<tracer.Request, 'source' | 'input'>
+}
+
+/**
+ * Example program that can be fetched by the user.
+ */
+export type Example = {
+    name: string
+    language: string
+    request: Pick<tracer.Request, 'source' | 'input'>
+}
