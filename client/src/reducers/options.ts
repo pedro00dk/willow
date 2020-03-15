6da76e533e30115250dcc9b187293913e81c1cc7
Pedro Henrique <pedro00dk@gmail.com>
2020-03-15 15:00:45 -0300

    Implement enableVisualization option in option reducer

--- client/src/reducers/options.ts
+++ client/src/reducers/options.ts
@@ -2,33 +2,38 @@
  * Input reducer updates some tracing and visualization options.
  */
 type State = {
-    liveProgramming: boolean
+    enableVisualization: boolean
     preserveLayout: boolean
+    liveProgramming: boolean
 }
 
 type Action =
-    | { type: 'options/setLiveProgramming'; payload: boolean }
+    | { type: 'options/setEnableVisualization'; payload: boolean }
     | { type: 'options/setPreserveLayout'; payload: boolean }
+    | { type: 'options/setLiveProgramming'; payload: boolean }
 
 const initialState: State = {
-    liveProgramming: true, // TODO Implement
-    preserveLayout: true // TODO Write requirements in schema (from 0 ordered obj references)
+    enableVisualization: true,
+    preserveLayout: true,
+    liveProgramming: true // TODO Implement
 }
 
 export const reducer = (state: State = initialState, action: Action): State => {
     switch (action.type) {
-        case 'options/setLiveProgramming':
-            return { ...state, liveProgramming: action.payload }
+        case 'options/setEnableVisualization':
+            return { ...state, enableVisualization: action.payload }
         case 'options/setPreserveLayout':
             return { ...state, preserveLayout: action.payload }
+        case 'options/setLiveProgramming':
+            return { ...state, liveProgramming: action.payload }
         default:
             return state
     }
 }
 
-const setLiveProgramming = (liveProgramming: boolean): Action => ({
-    type: 'options/setLiveProgramming',
-    payload: liveProgramming
+const setEnableVisualization = (enableVisualization: boolean): Action => ({
+    type: 'options/setPreserveLayout',
+    payload: enableVisualization
 })
 
 const setPreserveLayout = (preserveLayout: boolean): Action => ({
@@ -36,4 +41,9 @@ const setPreserveLayout = (preserveLayout: boolean): Action => ({
     payload: preserveLayout
 })
 
-export const actions = { setLiveProgramming, setPreserveLayout }
+const setLiveProgramming = (liveProgramming: boolean): Action => ({
+    type: 'options/setLiveProgramming',
+    payload: liveProgramming
+})
+
+export const actions = { setEnableVisualization, setPreserveLayout, setLiveProgramming }
