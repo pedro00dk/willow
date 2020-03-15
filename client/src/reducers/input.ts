eaebb2b72837ce276333e4ee5367eeabdd661d26
Pedro Henrique <pedro00dk@gmail.com>
2020-03-15 14:59:40 -0300

    Put input and source tracer states in container to allow mutation

--- client/src/reducers/input.ts
+++ client/src/reducers/input.ts
@@ -1,14 +1,15 @@
 /**
  * Input reducer updates the piece of state that stores program inputs written by the user.
+ * This reducer state allows mutation of its contents, due to the fact that input is not observed in components.
  */
-type State = string[]
+type State = { content: string[] }
 
 type Action = { type: 'input/set'; payload: string[] }
 
-const initialState: State = []
+const initialState: State = { content: [] }
 
 export const reducer = (state: State = initialState, action: Action): State =>
-    action.type === 'input/set' ? action.payload : state
+    action.type === 'input/set' ? { content: action.payload } : state
 
 const set = (input: string[]): Action => ({ type: 'input/set', payload: input })
 
--- client/src/reducers/source.ts
+++ client/src/reducers/source.ts
@@ -1,14 +1,15 @@
 /**
  * Source reducer updates the state that stores the program source code.
+ * This reducer state allows mutation of its contents, due to the fact that source is not observed in components.
  */
-type State = string[]
+type State = { content: string[] }
 
 type Action = { type: 'source/set'; payload: string[] }
 
-const initialState: State = []
+const initialState: State = { content: [] }
 
 export const reducer = (state: State = initialState, action: Action): State =>
-    action.type === 'source/set' ? action.payload : state
+    action.type === 'source/set' ? { content: action.payload } : state
 
 const set = (source: string[]): Action => ({ type: 'source/set', payload: source })
 
