7e450a450d0f7569fef6addd28808cbd60ed7c03
Pedro Henrique <pedro00dk@gmail.com>
2020-03-15 15:01:39 -0300

    Implement fetch programs and send actions in user reducer

--- client/src/reducers/user.ts
+++ client/src/reducers/user.ts
@@ -1,33 +1,29 @@
 import { api, apiUrl } from '../api'
+import { Action as UserAction, Program, User } from '../types/model'
 import { DefaultAsyncAction } from './Store'
 /**
  * User reducer updates the user information and collect user actions.
  */
 type State = {
     fetching: boolean
-    signed: boolean
-    email: string
-    actions: ({ time: Date } & (
-        | { action: 'login' }
-        | { action: 'trace'; steps: string; code: string; compilationFail: boolean; executionFail: boolean }
-        | { action: 'loadWillowExample'; exampleId: string }
-        | { action: 'loadOwnExample'; exampleId: string }
-        | { action: 'saveOwnExample'; exampleId: string }
-    ))[]
+    fetchingPrograms: boolean
+    user: User
+    programs: Program[]
     error?: string
 }
 
 type Action =
     | { type: 'user/signin' }
     | { type: 'user/signout' }
-    | { type: 'user/fetch'; payload?: string; error?: string }
-    | { type: 'user/action'; payload?: State['actions'][0]; error?: string }
+    | { type: 'user/fetch'; payload?: User; error?: string }
+    | { type: 'user/fetchPrograms'; payload?: Program[]; error?: string }
+    | { type: 'user/sendAction'; error?: string }
 
 const initialState: State = {
     fetching: false,
-    signed: false,
-    email: undefined,
-    actions: []
+    fetchingPrograms: false,
+    user: undefined,
+    programs: []
 }
 
 export const reducer = (state: State = initialState, action: Action): State => {
@@ -37,11 +33,17 @@ export const reducer = (state: State = initialState, action: Action): State => {
             return state
         case 'user/fetch':
             return action.payload
-                ? { ...state, fetching: false, signed: true, email: action.payload }
+                ? { ...initialState, fetching: false, user: action.payload }
                 : action.error != undefined
                 ? { ...initialState, error: action.error }
                 : { ...initialState, fetching: true }
-        case 'user/action':
+        case 'user/fetchPrograms':
+            return action.payload
+                ? { ...state, fetchingPrograms: false, programs: action.payload }
+                : action.error != undefined
+                ? { ...state, fetchingPrograms: false, programs: [], error: action.error }
+                : { ...state, fetchingPrograms: true, programs: [] }
+        case 'user/sendAction':
         default:
             return state
     }
@@ -58,11 +60,31 @@ const signout = (): DefaultAsyncAction => async () => {
 const fetch = (): DefaultAsyncAction => async dispatch => {
     dispatch({ type: 'user/fetch' })
     try {
-        const user = (await api.get('/api/auth/user')).data as { email: string }
-        dispatch({ type: 'user/fetch', payload: user.email })
+        const user = (await api.get<User>('/api/auth/user')).data
+        dispatch({ type: 'user/fetch', payload: user })
     } catch (error) {
         dispatch({ type: 'user/fetch', error: error?.response?.data ?? error.toString() })
     }
 }
 
-export const actions = { signin, signout, fetch }
+const fetchPrograms = (): DefaultAsyncAction => async dispatch => {
+    dispatch({ type: 'user/fetchPrograms' })
+    try {
+        const programs = (await api.get<User>('/api/program')).data
+        dispatch({ type: 'user/fetchPrograms', payload: programs })
+    } catch (error) {
+        dispatch({ type: 'user/fetchPrograms', error: error?.response?.data ?? error.toString() })
+    }
+}
+
+const sendAction = (action: UserAction): DefaultAsyncAction => async dispatch => {
+    dispatch({ type: 'user/sendAction' })
+    try {
+        await api.post('/api/action', action)
+        dispatch({ type: 'user/sendAction' })
+    } catch (error) {
+        dispatch({ type: 'user/sendAction', error: error?.response?.data ?? error.toString() })
+    }
+}
+
+export const actions = { signin, signout, fetch, fetchPrograms, sendAction }
