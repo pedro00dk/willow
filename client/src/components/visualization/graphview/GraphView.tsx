ba775d7e5215fdcd1f7b0046385a58a0d8a93264
Pedro Henrique <pedro00dk@gmail.com>
2020-03-17 15:05:20 -0300

    Put graphdata in an context to be preserved between rerenders

--- client/src/components/visualization/graphview/GraphView.tsx
+++ client/src/components/visualization/graphview/GraphView.tsx
@@ -5,20 +5,22 @@ import { Heap } from './heap/Heap'
 import { Stack } from './stack/Stack'
 import { SvgView } from './svg/SvgView'
 
+const graphDataContext = React.createContext(new GraphData({ width: 1200, height: 1000 }, { x: 20, y: 20 }))
+
 export const GraphView = () => {
     const update = React.useState({})[1]
-    const graphData = React.useRef(new GraphData({ width: 1200, height: 1000 }, { x: 20, y: 20 }))
+    const graphData = React.useContext(graphDataContext)
     const [zoom, setZoom] = React.useState(devicePixelRatio)
     const { preserveLayout, tracer } = useSelection(state => ({
         preserveLayout: state.options.preserveLayout,
         tracer: state.tracer
     }))
-    if (!tracer.available && !preserveLayout) graphData.current.clearNodes()
-    graphData.current.setSize(tracer.available ? tracer.steps.length : 0)
-    graphData.current.setIndex(tracer.available ? tracer.index : 0)
-    graphData.current.setAnimate(true)
-    graphData.current.clearSubscriptions()
-    graphData.current.clearEdges()
+    if (!tracer.available && !preserveLayout) graphData.clearNodes()
+    graphData.setSize(tracer.available ? tracer.steps.length : 0)
+    graphData.setIndex(tracer.available ? tracer.index : 0)
+    graphData.setAnimate(true)
+    graphData.clearSubscriptions()
+    graphData.clearEdges()
 
     React.useLayoutEffect(() => {
         const onResize = (event: Event) => zoom !== devicePixelRatio && setZoom(devicePixelRatio)
@@ -28,9 +30,9 @@ export const GraphView = () => {
     }, [zoom])
 
     return (
-        <SvgView graphData={graphData.current}>
-            <Stack tracer={tracer} graphData={graphData.current} update={update} />
-            <Heap tracer={tracer} graphData={graphData.current} update={update} />
+        <SvgView graphData={graphData}>
+            <Stack tracer={tracer} graphData={graphData} update={update} />
+            <Heap tracer={tracer} graphData={graphData} update={update} />
         </SvgView>
     )
 }
