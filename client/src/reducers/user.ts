914cb7db21a54ed9a39fbfcd9916b7d2cd3b3f52
Pedro Henrique <pedro00dk@gmail.com>
2020-03-17 15:01:14 -0300

    Fix user payload when the user is undefined

--- client/src/reducers/user.ts
+++ client/src/reducers/user.ts
@@ -15,7 +15,7 @@ type State = {
 type Action =
     | { type: 'user/signin' }
     | { type: 'user/signout' }
-    | { type: 'user/fetch'; payload?: User; error?: string }
+    | { type: 'user/fetch'; payload?: { user: User }; error?: string }
     | { type: 'user/fetchPrograms'; payload?: Program[]; error?: string }
     | { type: 'user/sendAction'; error?: string }
 
@@ -33,7 +33,7 @@ export const reducer = (state: State = initialState, action: Action): State => {
             return state
         case 'user/fetch':
             return action.payload
-                ? { ...initialState, fetching: false, user: action.payload }
+                ? { ...initialState, fetching: false, user: action.payload.user }
                 : action.error != undefined
                 ? { ...initialState, error: action.error }
                 : { ...initialState, fetching: true }
@@ -61,7 +61,7 @@ const fetch = (): DefaultAsyncAction => async dispatch => {
     dispatch({ type: 'user/fetch' })
     try {
         const user = (await api.get<User>('/api/auth/user')).data
-        dispatch({ type: 'user/fetch', payload: user })
+        dispatch({ type: 'user/fetch', payload: { user } })
     } catch (error) {
         dispatch({ type: 'user/fetch', error: error?.response?.data ?? error.toString() })
     }
