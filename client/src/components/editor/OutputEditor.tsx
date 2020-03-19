84299bac30747e3fb30e872e14882e7f71818d25
Pedro Henrique <pedro00dk@gmail.com>
2020-03-18 22:09:21 -0300

    Update the output editor to use the output reducer

--- client/src/components/editor/OutputEditor.tsx
+++ client/src/components/editor/OutputEditor.tsx
@@ -5,33 +5,15 @@ import { TextEditor } from './TextEditor'
 
 export const OutputEditor = () => {
     const editor = React.useRef<ace.Editor>()
-    const output = React.useRef<string[]>([])
 
     React.useLayoutEffect(() => {
         editor.current.renderer.setShowGutter(false)
     }, [editor.current])
 
-    useSelection(async (state, previousState) => {
-        const tracer = state.tracer
-        const previousTracer = previousState.tracer
-        if (
-            !tracer.available ||
-            (tracer.response === previousTracer?.response && output.current.length === tracer.steps.length)
-        )
-            return
-        output.current = tracer.steps.reduce((acc, step) => {
-            const previousContent = acc[acc.length - 1] ?? ''
-            const prints = step.print ?? ''
-            const error = step.error?.exception?.traceback ?? step.error?.cause ?? ''
-            acc.push(`${previousContent}${prints}${error}`)
-            return acc
-        }, [] as string[])
-    })
-
     useSelection(async state => {
         const tracer = state.tracer
         if (!editor.current || !tracer.available) return
-        editor.current.session.doc.setValue(output.current[state.tracer.index])
+        editor.current.session.doc.setValue(state.output[state.tracer.index])
         editor.current.scrollToLine(editor.current.session.getLength(), true, true, undefined)
     })
 
