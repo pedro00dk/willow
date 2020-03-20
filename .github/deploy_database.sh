5d9c6a866b5aaa1fe346dfe056fc480f552e28bc
Pedro Henrique <pedro00dk@gmail.com>
2020-03-20 16:31:08 -0300

    Create script to deploy mongodb with authentication enabled

---
+++ .github/deploy_database.sh
@@ -0,0 +1,38 @@
+DATABASE_USER=${1}
+DATABASE_PASSWORD=${2}
+DATABASE_DIRECTORY=${3}
+DATABASE_PORT=${4}
+
+set -v
+
+docker pull mongo
+docker stop mongo-database || true
+docker rm mongo-database || true
+
+mkdir --parents "${DATABASE_DIRECTORY}"
+echo "
+db = db.getSiblingDB('admin')
+db.createUser({ user: '${DATABASE_USER}', pwd: '${DATABASE_PASSWORD}', roles: ['root'] })
+" |
+sudo tee "${DATABASE_DIRECTORY}/createDatabaseAdmin.js"
+
+docker container run \
+    --name mongo-noauth \
+    --rm --detach --publish ${DATABASE_PORT}:27017 --volume ${DATABASE_DIRECTORY}:/data/db/ \
+    mongo \
+    mongod
+
+sleep 5
+
+docker container exec \
+    --interactive --tty \
+    mongo-noauth \
+    mongo /data/db/createDatabaseAdmin.js
+
+docker stop mongo-noauth
+
+docker container run \
+    --name mongo-database \
+    --rm --detach --publish ${DATABASE_PORT}:27017 --volume ${DATABASE_DIRECTORY}:/data/db/ \
+    mongo \
+    mongod --auth
