9b70dd44b6e25361a385233c5959a27356ef4fe9
Pedro Henrique <pedro00dk@gmail.com>
2020-04-01 17:00:54 -0300

    Update color palette

--- client/src/colors.ts
+++ client/src/colors.ts
@@ -4,31 +4,38 @@
  */
 export const colors = {
     blue: {
-        main: 'rgba(185,201,237,1)',
-        light: 'rgba(208,219,245,1)',
-        lighter: 'rgba(229,236,251,1)',
-        dark: 'rgba(161,181,226,1)',
-        darker: 'rgba(138,160,213,1)'
+        main: 'rgba( 88,148,194,1)',
+        lighter: 'rgba(178,212,239,1)',
+        light: 'rgba(128,179,218,1)',
+        dark: 'rgba( 57,119,167,1)',
+        darker: 'rgba( 32, 99,150,1)'
     },
-    red: {
-        main: 'rgba(255,199,192,1)',
-        light: 'rgba(255,216,211,1)',
-        lighter: 'rgba(255,233,230,1)',
-        dark: 'rgba(255,183,173,1)',
-        darker: 'rgba(255,166,154,1)'
+    green: {
+        main: 'rgba( 84,201,151,1)',
+        lighter: 'rgba(175,241,213,1)',
+        light: 'rgba(124,222,180,1)',
+        dark: 'rgba( 53,177,124,1)',
+        darker: 'rgba( 25,160,102,1)'
     },
     yellow: {
-        main: 'rgba(255,253,192,1)',
-        light: 'rgba(255,254,211,1)',
-        lighter: 'rgba(255,254,230,1)',
-        dark: 'rgba(255,252,173,1)',
-        darker: 'rgba(153,153,0,1)'
+        main: 'rgba(246,234,155,1)',
+        lighter: 'rgba(255,249,211,1)',
+        light: 'rgba(255,246,188,1)',
+        dark: 'rgba(218,203,109,1)',
+        darker: 'rgba(187,170, 69,1)'
+    },
+    red: {
+        main: 'rgba(255,149,107,1)',
+        lighter: 'rgba(255,206,186,1)',
+        light: 'rgba(255,175,143,1)',
+        dark: 'rgba(255,127, 76,1)',
+        darker: 'rgba(233, 93, 37,1)'
     },
     gray: {
-        main: 'darkgray',
-        light: 'lightgray',
-        lighter: 'gainsboro',
-        dark: 'gray',
-        darker: 'rgba(48,48,48,1)'
+        main: 'rgba(170,170,170,1)',
+        lighter: 'rgba(230,230,230,1)',
+        light: 'rgba(210,210,210,1)',
+        dark: 'rgba(127,127,127,1)',
+        darker: 'rgba( 76, 75, 75,1)'
     }
 }
