90a3c3c95dab270ea60adf931b051882697897f7
Pedro Henrique <pedro00dk@gmail.com>
2020-04-24 19:55:55 -0300

    Refactor graph stack implementation

--- client/src/components/visualization/graphview/stack/Stack.tsx
+++ client/src/components/visualization/graphview/stack/Stack.tsx
@@ -4,64 +4,54 @@ import { DefaultState } from '../../../../reducers/Store'
 import * as tracer from '../../../../types/tracer'
 import { Graph } from '../Graph'
 import { SvgNode } from '../svg/SvgNode'
-import { isSameMember, isValueObject, getMemberName, getDisplayValue, getValueString } from '../TracerUtils'
+import { getValueString, isSameMember, isValueReference } from '../TracerUtils'
 
 const styles = {
-    edge: (changed: boolean) => (changed ? colors.yellow.darker : colors.gray.dark),
-    width: (variableIndex: number, variableDepth: number, stackDepth: number) =>
-        variableDepth === stackDepth - 1 ? 2.5 : variableIndex < 2 ? 1 : 0.5
+    edgeColor: (changed: boolean) => (changed ? colors.yellow.darker : colors.gray.dark),
+    edgeWidth: (index: number, scopeDepth: number, stackLength: number) =>
+        scopeDepth === stackLength - 1 ? 2.5 : index < 2 ? 1 : 0.5
 }
 
 export const Stack = (props: { tracer: DefaultState['tracer']; graph: Graph; update: React.Dispatch<{}> }) => {
-    const previousStack = React.useRef<tracer.Scope[]>()
-    const previousMembers = React.useRef<{ [scope: number]: { [name: string]: tracer.Member } }>({})
     const stack = props.tracer.steps[props.graph.index].snapshot?.stack || []
-    const scopesDepths = stack.map((scope, i) => ({ scope, depth: i }))
-    const membersDepths = scopesDepths
-        .flatMap(({ scope, depth }) => scope.members.map(member => ({ member, depth })))
-        .filter(({ member }) => isValueObject(member.value))
-    if (stack !== previousStack.current)
-        previousMembers.current = membersDepths.reduce((acc, { member, depth }) => {
-            if (!acc[depth]) acc[depth] = {}
-            acc[depth][getMemberName(member)] = member
-            return acc
-        }, {} as { [depth: number]: { [name: string]: tracer.Member } })
-    previousStack.current = stack
-
-    const deltas = [
-        { x: -65, y: -25 },
-        { x: -75, y: 0 },
-        { x: -65, y: 25 }
-    ]
-
-    const idMembers = membersDepths.reduce((acc, { member, depth }, i) => {
-        const id = getValueString(member.value)
-        if (!acc[id]) acc[id] = []
-        acc[id].push({ member, depth })
+    const referenceMembers = stack.reduce((acc, scope, i) => {
+        scope.members.forEach(member => {
+            if (!isValueReference(member.value)) return
+            const id = getValueString(member.value)
+            const key = getValueString(member.key)
+            if (!acc[id]) acc[id] = {}
+            if (!acc[id][i]) acc[id][i] = {}
+            acc[id][i][key] = member
+        })
         return acc
-    }, {} as { [id: string]: { member: tracer.Member; depth: number }[] })
+    }, {} as { [id: string]: { [scope: string]: { [key: string]: tracer.Member } } })
+
+    const previousReferenceMembers = React.useRef<typeof referenceMembers>({})
+    React.useEffect(() => {
+        previousReferenceMembers.current = referenceMembers
+    }, [stack])
+
+    const edgeDelta = (size: number, angle: number) => ({ x: -size * Math.cos(angle), y: -size * Math.sin(angle) })
 
     const node = props.graph.getNode('stack')
-    Object.entries(idMembers).forEach(([id, variablesDepths]) =>
-        variablesDepths
+    Object.entries(referenceMembers).forEach(([id, members]) => {
+        const displayMembers = Object.entries(members)
+            .flatMap(([scope, keys]) => Object.entries(keys).flatMap(([key, member]) => ({ scope, key, member })))
             .slice(-3)
-            .reverse()
-            .forEach(({ member, depth }, i) => {
-                const memberName = getMemberName(member)
-                const changed = !isSameMember(member, previousMembers.current[depth]?.[memberName])
-                const displayKey = getDisplayValue(member.key)
-                props.graph.pushEdge(node.id, `${depth}-${memberName}`, {
-                    self: true,
-                    target: id,
-                    from: { delta: deltas[i], source: 'target' },
-                    to: { delta: { x: 0, y: 0 }, source: 'target' },
-                    draw: 'line',
-                    color: styles.edge(changed),
-                    width: styles.width(i, depth, stack.length),
-                    text: displayKey
-                })
+        displayMembers.forEach(({ scope, key, member }, i) => {
+            const edgeId = `${scope}-${key}`
+            const changed = !isSameMember(member, previousReferenceMembers.current[id]?.[scope]?.[key])
+            props.graph.pushEdge(node.id, edgeId, {
+                target: id,
+                from: { delta: edgeDelta(80, i * 15 * (Math.PI / 180)), source: 'target' },
+                to: { delta: { x: 0, y: 0 }, source: 'target' },
+                shape: 'line',
+                color: styles.edgeColor(changed),
+                width: styles.edgeWidth(i, parseInt(scope), stack.length),
+                text: key
             })
-    )
+        })
+    })
 
     return <SvgNode id={node.id} graph={props.graph} />
 }
