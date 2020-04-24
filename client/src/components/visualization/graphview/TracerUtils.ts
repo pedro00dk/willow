0e5d420878c7df405e90ff8cb30359b753c61996
Pedro Henrique <pedro00dk@gmail.com>
2020-04-24 18:00:54 -0300

    Update tracer utility functions

--- client/src/components/visualization/graphview/TracerUtils.ts
+++ client/src/components/visualization/graphview/TracerUtils.ts
@@ -1,26 +1,23 @@
 import * as tracer from '../../../types/tracer'
 
-export const isValueObject = (value: tracer.Value): value is [string] => typeof value === 'object'
-
-export const getValueString = (value: tracer.Value) => (isValueObject(value) ? value[0] : value.toString())
-
-export const getDisplayValue = (value: tracer.Value, containerId?: string) =>
-    isValueObject(value) ? (value[0] === containerId ? ':#:' : '::') : value.toString()
+export const isValueReference = (value: tracer.Value): value is [string] => typeof value === 'object'
 
 export const isSameValue = (valueA: tracer.Value, valueB: tracer.Value) => {
-    const aIsObject = isValueObject(valueA)
-    const bIsObject = isValueObject(valueB)
+    const aIsObject = isValueReference(valueA)
+    const bIsObject = isValueReference(valueB)
     return (
         (!aIsObject && !bIsObject && valueA === valueB) ||
         (aIsObject && bIsObject && (valueA as [string])[0] === (valueB as [string])[0])
     )
 }
 
-export const getMemberName = (member: tracer.Member) =>
-    typeof member.key !== 'object' ? member.key.toString() : `[${member.key[0].toString()}]`
-
 export const isSameMember = (memberA: tracer.Member, memberB: tracer.Member) => {
     if ((!memberA && memberB) || (!memberB && memberA)) return false
     if (!memberA && !memberB) return true
-    return getMemberName(memberA) === getMemberName(memberB) && isSameValue(memberA.value, memberB.value)
+    return getValueString(memberA.key) === getValueString(memberB.key) && isSameValue(memberA.value, memberB.value)
 }
+
+export const getValueString = (value: tracer.Value) => (isValueReference(value) ? value[0] : value).toString()
+
+export const getValueDisplay = (value: tracer.Value, containerId?: string) =>
+    isValueReference(value) ? (value[0] === containerId ? '::#' : '::') : value.toString()
