326dd97145b50964d8bc9e99aed9420747385263
Pedro Henrique <pedro00dk@gmail.com>
2020-04-24 22:32:07 -0300

    Multiple changes in Obj to support easier layout usage

--- client/src/components/visualization/graphview/heap/Obj.tsx
+++ client/src/components/visualization/graphview/heap/Obj.tsx
@@ -1,11 +1,13 @@
+import { css } from 'emotion'
 import React from 'react'
-import 'react-contexify/dist/ReactContexify.min.css'
 import { Item, Menu, MenuProvider, Separator, Submenu } from 'react-contexify'
+import 'react-contexify/dist/ReactContexify.min.css'
+import { colors } from '../../../../colors'
 import { actions, useDispatch } from '../../../../reducers/Store'
 import * as tracer from '../../../../types/tracer'
 import { Draggable } from '../../../utils/Draggable'
 import { Edge, Graph, Node } from '../Graph'
-import { getMemberName, getValueString, isValueObject } from '../TracerUtils'
+import { getValueString } from '../TracerUtils'
 import { Parameters } from './Parameters'
 import * as ArrayModule from './shapes/Array'
 import * as ColumnModule from './shapes/Column'
@@ -14,9 +16,14 @@ import * as MapModule from './shapes/Map'
 
 const classes = {
     container: 'd-flex position-absolute',
-    menuProvider: 'd-flex'
+    menuProvider: 'd-flex',
+    wrapper: 'd-flex flex-column rounded',
+    title: `px-1 ${css({ fontSize: '0.5rem' })}`,
+    children: 'd-flex justify-content-center rounded p-1'
 }
-
+const styles = {
+    layoutHighlight: (enabled: boolean) => (enabled ? colors.gray.light : colors.gray.lighter)
+}
 const shapes = {
     array: ArrayModule,
     column: ColumnModule,
@@ -26,16 +33,10 @@ const shapes = {
 
 export const Obj = (props: { id: string; obj: tracer.Obj; node: Node; graph: Graph; update: React.Dispatch<{}> }) => {
     const container$ = React.useRef<HTMLDivElement>()
-    const references = React.useRef<{ id: string; name: string; ref$: HTMLSpanElement; edge: Partial<Edge> }[]>()
-    references.current = []
-    const previousObj = React.useRef<tracer.Obj>()
-    const previousMembers = React.useRef<{ [id: string]: tracer.Member }>({})
-    const members = Object.fromEntries(props.obj.members.map(member => [getMemberName(member), member]))
-    if (props.obj !== previousObj.current)
-        previousMembers.current = Object.fromEntries(
-            previousObj.current?.members.map(member => [getMemberName(member), member]) ?? []
-        )
-    previousObj.current = props.obj
+    const wrapper$ = React.useRef<HTMLDivElement>()
+    const members = Object.fromEntries(props.obj.members.map(member => [getValueString(member.key), member]))
+    const previousMembers = React.useRef<typeof members>({})
+    const references = React.useRef<{ key: string; target: string; ref$: HTMLSpanElement; edge: Partial<Edge> }[]>([])
     const dispatch = useDispatch()
 
     const defaultShape = React.useMemo(
@@ -43,10 +44,9 @@ export const Obj = (props: { id: string; obj: tracer.Obj; node: Node; graph: Gra
             Object.entries(shapes).reduce(
                 (acc, [name, mod]) => acc ?? (mod.defaults.has(props.obj.category) ? name : acc),
                 undefined
-            ) as keyof typeof shapes,
+            ),
         [props.obj.category]
-    )
-
+    ) as keyof typeof shapes
     const shape = (props.node.getShape() ?? props.node.setShape(defaultShape)) as keyof typeof shapes
     const parameters = props.node.getParameters().get(shape, shapes[shape].defaultParameters)
     const Shape = shapes[shape].Shape
@@ -56,49 +56,47 @@ export const Obj = (props: { id: string; obj: tracer.Obj; node: Node; graph: Gra
         const rect = container$.current.getBoundingClientRect()
         const screenSize = { x: rect.width, y: rect.height }
         const [svgSize] = props.graph.view.transformVector('toSvg', svg, true, screenSize)
-        props.node.size.width = svgSize.x
-        props.node.size.height = svgSize.y
-    })
-
-    React.useLayoutEffect(() => {
-        const svg = container$.current.closest('svg')
-        const rect = container$.current.getBoundingClientRect()
-        references.current.forEach(({ id: target, name, ref$, edge }) => {
+        props.node.size = svgSize
+        references.current.forEach(({ key, target, ref$, edge }) => {
             const refRect = ref$.getBoundingClientRect()
             const screenDelta = { x: refRect.left - rect.left, y: refRect.top - rect.top }
             const screenSize = { x: refRect.width, y: refRect.height }
             const [svgDelta, svgSize] = props.graph.view.transformVector('toSvg', svg, true, screenDelta, screenSize)
             const delta = { x: svgDelta.x + svgSize.x / 2, y: svgDelta.y + svgSize.y / 2 }
-            props.graph.pushEdge(props.node.id, name, {
+            props.graph.pushEdge(props.id, key, {
                 ...edge,
-                self: true,
                 target,
                 from: { delta, source: 'self' },
                 to: { delta: { x: 0, y: 0 }, source: 'target-near' }
             })
         })
+        references.current = []
+        props.graph.subscriptions.call(props.id)
     })
-
-    React.useLayoutEffect(() => props.graph.subscriptions.call(props.id))
 
     React.useEffect(() => {
         if (!props.node.layout.enabled) return
         const structure = props.node.findStructure()
-        props.node.layout.enabled = false
-        structure.base.layout.enabled = true
-        structure.base.layout.horizontal = props.node.layout.horizontal
+        const horizontal = (structure.base.layout.horizontal = props.node.layout.horizontal)
         const position = props.node.getPosition()
-        console.log(props.node.id, position)
-        const layout = structure.applyLayout(
-            { breadth: 1.5, depth: 1.5 },
-            structure.base.layout.horizontal,
-            position,
-            'avl'
-        )
+        const layout = structure.applyLayout({ breadth: 1.5, depth: 1.5 }, horizontal, position, 'ovr')
+        Object.values(structure.members).forEach(node => (node.layout.enabled = false))
+        structure.base.layout.enabled = true
         props.graph.animate = true
         Object.keys(layout).forEach(id => props.graph.subscriptions.call(id))
         dispatch(actions.user.action({ name: 'layout', payload: 'automatic' }))
     })
+
+    React.useEffect(() => {
+        ;(async () => {
+            await undefined
+            wrapper$.current.style.background = styles.layoutHighlight(props.node.layout.enabled)
+        })()
+    })
+
+    React.useEffect(() => {
+        previousMembers.current = members
+    }, [props.obj])
 
     return (
         <Draggable
@@ -106,21 +104,17 @@ export const Obj = (props: { id: string; obj: tracer.Obj; node: Node; graph: Gra
                 ref: container$,
                 className: classes.container,
                 onDoubleClick: event => {
-                    const horizontal = !event.altKey
-                    const mode = !event.ctrlKey ? 'avl' : !event.shiftKey ? 'ovr' : 'all'
+                    const mode = !event.ctrlKey ? 'ovr' : !event.shiftKey ? 'avl' : 'all'
                     const structure = props.node.findStructure()
-                    console.log(props.node.getPosition())
-                    const layout = structure.applyLayout(
-                        { breadth: 1.5, depth: 1.5 },
-                        horizontal,
-                        props.node.getPosition(),
-                        mode
-                    )
-                    props.graph.animate = true
-                    Object.keys(layout).forEach(id => props.graph.subscriptions.call(id))
+                    const horizontal = !event.altKey
+                    const position = props.node.getPosition()
+                    const layout = structure.applyLayout({ breadth: 1.5, depth: 1.5 }, horizontal, position, mode)
                     Object.values(structure.members).forEach(node => (node.layout.enabled = false))
                     structure.base.layout.enabled = props.graph.reapplyLayout
                     structure.base.layout.horizontal = horizontal
+                    props.graph.animate = true
+                    Object.keys(layout).forEach(id => props.graph.subscriptions.call(id))
+                    props.update({})
                     dispatch(actions.user.action({ name: 'layout', payload: 'manual' }))
                 }
             }}
@@ -128,21 +122,29 @@ export const Obj = (props: { id: string; obj: tracer.Obj; node: Node; graph: Gra
                 const svg = container$.current.closest('svg')
                 const [svgDelta] = props.graph.view.transformVector('toSvg', svg, false, delta)
                 const depth = event.altKey ? Infinity : 0
-                const mode = !event.ctrlKey ? 'avl' : !event.shiftKey ? 'ovr' : 'all'
+                const mode = !event.ctrlKey ? 'ovr' : !event.shiftKey ? 'avl' : 'all'
                 const movedNodes = props.node.move(svgDelta, depth, mode)
+                props.node.layout.enabled = false
                 props.graph.animate = false
                 Object.values(movedNodes).forEach(node => props.graph.subscriptions.call(node.id))
             }}
+            onDragEnd={() => wrapper$.current.style.background = styles.layoutHighlight(props.node.layout.enabled)}
         >
-            <MenuProvider id={props.node.id} className={classes.menuProvider}>
+            <MenuProvider id={props.id} className={classes.menuProvider}>
                 <div className={classes.menuProvider}>
-                    <Shape
-                        id={props.node.id}
-                        obj={props.obj}
-                        parameters={parameters as any}
-                        previousMembers={previousMembers.current}
-                        onReference={reference => references.current.push(reference)}
-                    />
+                    <div ref={wrapper$} className={classes.wrapper}>
+                        <span className={classes.title}>{props.node.type}</span>
+                        <div className={classes.children}>
+                            <Shape
+                                id={props.id}
+                                obj={props.obj}
+                                node={props.node}
+                                parameters={parameters as any}
+                                previousMembers={previousMembers.current}
+                                onReference={reference => references.current.push(reference)}
+                            />
+                        </div>
+                    </div>
                 </div>
             </MenuProvider>
             <ObjMenu {...props} />
@@ -175,7 +177,7 @@ const ObjMenu = (props: { id: string; obj: tracer.Obj; node: Node; graph: Graph;
     const parameters = props.node.getParameters().get(shape, shapes[shape].defaultParameters)
 
     return (
-        <Menu id={props.node.id}>
+        <Menu id={props.id}>
             <Item
                 onClick={() => {
                     props.node.mode = props.node.mode === 'local' ? 'type' : 'local'
