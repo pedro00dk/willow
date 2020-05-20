a7a267637684b9fa24de09b1511d8ef9f3ee83a8
Pedro Henrique <pedro00dk@gmail.com>
2020-05-20 17:36:59 -0300

    Remove client api module

--- client/src/api.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Save the api url to make requests.
- */
-import axios from 'axios'
-
-// environment variables captured when building by parcel using dotenv
-export const apiUrl = process.env['API']
-
-export const api = axios.create({ baseURL: apiUrl, withCredentials: true })
