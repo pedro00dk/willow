a77aea418f09f470a5bb4b56cafad8229f8702bf
Pedro Henrique <pedro00dk@gmail.com>
2020-05-20 16:24:44 -0300

    Add firebase initialization in index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,11 @@
 import '@babel/polyfill' // regeneratorRuntime
+import firebase from 'firebase'
 import React from 'react'
 import ReactDom from 'react-dom'
 import { App } from './components/App'
+
+const firebaseConfig = JSON.parse(process.env['FIREBASE_CONFIG'])
+firebase.initializeApp(firebaseConfig)
+firebase.firestore().collection('languages').get().then(x => console.log(x.docs.map(a => [a.id, a.data()])))
 
 ReactDom.render(<App />, document.getElementById('root'))
