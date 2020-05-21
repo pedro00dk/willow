27044879e8334d43b01b4e82e55d56e7ed5a0426
Pedro Henrique <pedro00dk@gmail.com>
2020-05-21 13:59:44 -0300

    Add new cloud function to set available language tracers

--- functions/index.js
+++ functions/index.js
@@ -6,20 +6,45 @@ const firestore = admin.firestore()
 
 const region = process.env['REGION'] || 'us-east1'
 
-const cors = response => {
+/**
+ * @param {Parameters<Parameters<functions.https.onRequest>[0]>[0]} request
+ * @param {Parameters<Parameters<functions.https.onRequest>[0]>[1]} response
+ * @param {string[]} methods
+ */
+const cors = (request, response, methods) => {
     response.setHeader('Access-Control-Allow-Origin', '*')
-    response.setHeader('Access-Control-Allow-Methods', 'POST')
+    response.setHeader('Access-Control-Allow-Methods', methods.join(','))
     response.setHeader('Access-Control-Allow-Headers', 'Content-Type')
     response.setHeader('Access-Control-Max-Age', '3600')
     response.setHeader('Content-Type', 'application/json')
+    if (request.method === 'OPTIONS') {
+        response.status(204).send()
+        return false
+    } else if (!methods.includes(request.method)) {
+        response.status(405).send()
+        return false
+    }
+    return true
 }
 
-exports.listTracers = functions.region(region).https.onRequest(async (request, response) => {
-    cors(response)
+exports.listLanguages = functions.region(region).https.onRequest(async (request, response) => {
+    if (!cors(request, response, ['GET'])) return
     const tracers = (await firestore.collection('languages').get()).docs
         .map(document => document)
         .reduce((acc, next) => ((acc[next.id] = next.data()['tracer']), acc), {})
     response.status(200).json(tracers)
+})
+
+exports.setLanguages = functions.region(region).https.onRequest(async (request, response) => {
+    if (!cors(request, response, ['POST'])) return
+    if (request.get('Content-Type') != 'application/json') response.status(412).send()
+    const batch = firestore.batch()
+    const languages = firestore.collection('languages')
+    const documents = await languages.listDocuments()
+    documents.forEach(document => batch.delete(document))
+    Object.entries(request.body).forEach(([language, tracer]) => batch.create(languages.doc(language), {tracer}))
+    await batch.commit()
+    response.status(200).send()
 })
 
 exports.createUser = functions
