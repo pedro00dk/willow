5677c27f2e11b5696f099eb574a1d494e6538034
Pedro Henrique <pedro00dk@gmail.com>
2020-05-21 17:04:55 -0300

    Add local firebase testing support to the client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,10 @@ import ReactDom from 'react-dom'
 import { App } from './components/App'
 
 const firebaseConfigEnv = process.env['FIREBASE_CONFIG']
+const firebaseTestingEnv = process.env['FIREBASE_TESTING']
+
 const firebaseConfig = JSON.parse(firebaseConfigEnv)
 firebase.initializeApp(firebaseConfig)
+if (firebaseTestingEnv !== 'false') firebase.firestore().settings({ host: firebaseTestingEnv, ssl: false })
 
 ReactDom.render(<App />, document.getElementById('root'))
